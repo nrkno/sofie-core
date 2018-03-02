55e8889dfd0e8e85a0ad50bf4ed7a7dc41646027
Jan Starzak <jan.starzak@gmail.com>
2018-03-02 10:11:39 +0100

    Feat: implement routing with nymansPlayground and tasksSample

--- meteor/client/ui/TaskItem.js
+++ meteor/client/ui/TaskItem.js
@@ -3,16 +3,13 @@ import React, { Component } from 'react';
 //import {EditAttribute} from 'meteor/superfly:mypackage'; // import from packages
 
 // Task component - represents a single todo item
-export default class Task extends Component {
+export default class TaskItem extends Component {
 	render() {
 		return (
 			<li>
 				<div>
 					{this.props.task.text}
-
-				<div>
-
-				</div>
+					<div></div>
 				</div>
 			</li>
 		);
