f3fd4b9eb02120075255b785c831e03efd1bfe62
Jan Starzak <jan.starzak@gmail.com>
2018-03-06 08:31:15 +0100

    Bug: move Origo UI from it's own folder into public When it's in it's own folder and symlinked into public, meteor is unable to serve it on Linux.

---
+++ meteor/client/ui/Dashboard.tsx
@@ -0,0 +1,19 @@
+import { Meteor }           from 'meteor/meteor';
+import * as React           from 'react';
+import * as ReactDOM        from 'react-dom';
+import {withTracker}        from '../lib/ReactMeteorData/react-meteor-data';
+
+interface IPropsHeader {
+
+}
+export default class Dashboard extends React.Component<IPropsHeader> {
+	render() {
+		return (
+			<div>
+				<div className="mvl">
+					<h1>Welcome to YASS</h1>
+				</div>
+			</div>
+		);
+	}
+}
