80c763ae922edebbd3531d0e825cfcb6e9dcdf01
Jan Starzak <jan.starzak@gmail.com>
2018-03-26 10:12:15 +0200

    Feat: store collections in window for easy development

--- meteor/client/lib/dev.ts
+++ meteor/client/lib/dev.ts
@@ -1,5 +1,11 @@
 import { PeripheralDevices } 	from "../../lib/collections/PeripheralDevices";
 import { Tasks } 				from "../../lib/collections/Tasks";
+import { RunningOrders } from "../../lib/collections/RunningOrders";
+import { SegmentLineItems } from "../../lib/collections/SegmentLineItems";
+import { SegmentLines } from "../../lib/collections/SegmentLines";
+import { Segments } from "../../lib/collections/Segments";
+import { ShowStyles } from "../../lib/collections/ShowStyles";
+import { StudioInstallations } from "../../lib/collections/StudioInstallations";
 
 
 // Note: These things are convenience functions to be used during development
@@ -7,4 +13,9 @@ import { Tasks } 				from "../../lib/collections/Tasks";
 
 window['Tasks'] = Tasks;
 window['PeripheralDevices'] = PeripheralDevices;
-
+window['RunningOrders'] = RunningOrders;
+window['SegmentLineItmes'] = SegmentLineItems;
+window['SegmentLines'] = SegmentLines;
+window['Segments'] = Segments;
+window['ShowsStyles'] = ShowStyles;
+window['StudioInstallations'] = StudioInstallations;
