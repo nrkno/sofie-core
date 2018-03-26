bda000bf959c5e087342848548387651606653b0
Jan Starzak <jan.starzak@gmail.com>
2018-03-26 10:11:13 +0200

    Feat: rename rundowns to running orders

--- meteor/lib/collections/RunningOrders.ts
+++ meteor/lib/collections/RunningOrders.ts
@@ -4,8 +4,10 @@ import {Time}                from '../../lib/lib';
 
 
 /** This is a very uncomplete mock-up of the Rundown object */
-export interface Rundown {
+export interface RunningOrder {
 	_id: String,
+	/** ID of the object in MOS */
+	mosId: String,
   studioInstallationId: String,
   showStyleId: String,
 	name: String,
@@ -15,4 +17,4 @@ export interface Rundown {
   nextSegmentLineId: String
 }
 
-export const Rundowns = new Mongo.Collection<Rundown>('rundowns');
+export const RunningOrders = new Mongo.Collection<RunningOrder>('rundowns');
