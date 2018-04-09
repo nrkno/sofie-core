39b6a98b1d70018e46e5709084f2c7512ae30db9
Johan Nyman <johan@nytamin.se>
2018-04-09 14:42:12 +0200

    chore: changed deploy target

--- deploy_to_nrk_supersuite.sh
+++ deploy_to_nrk_supersuite.sh
@@ -4,8 +4,8 @@ DEPLOY_HOSTNAME="eu-west-1.galaxy-deploy.meteor.com"
 
 
 
-echo Deploying to deploy nrk.supersuite.tv...
-meteor deploy nrk.supersuite.tv --settings ../settings.json
+echo Deploying to deploy nrkseff.supersuite.tv...
+meteor deploy nrkseff.supersuite.tv --settings ../settings.json
 
 
 echo Deployment complete.
