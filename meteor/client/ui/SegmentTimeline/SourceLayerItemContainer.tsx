47777af0458c148d148ab8d130e6783035452011
Jan Starzak <jan.starzak@gmail.com>
2018-04-12 21:24:18 +0200

    Feat: fetch data from Timeline if item is in the liveline

--- meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
@@ -3,6 +3,10 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as _ from 'underscore'
 import { withTracker } from '../../lib/ReactMeteorData/react-meteor-data'
+import { TriggerType } from 'superfly-timeline'
+
+import { RunningOrder } from '../../../lib/collections/RunningOrders'
+import { Timeline } from '../../../lib/collections/Timeline'
 
 import { SourceLayerItem } from './SourceLayerItem'
 
@@ -28,7 +32,25 @@ interface IPropsHeader {
 export const SourceLayerItemContainer = withTracker((props) => {
 	if (props.isLiveLine) {
 		// Check in Timeline collection for any changes to the related object
-		return {}
+		let timelineObj = Timeline.findOne({_id: props.segmentLineItem._id})
+
+		if (timelineObj) {
+			let segmentCopy = (_.clone(props.segmentLineItem) as SegmentLineItemUi)
+
+			segmentCopy.trigger = timelineObj.trigger
+			if (timelineObj.trigger.type === TriggerType.TIME_ABSOLUTE) {
+				// if the TIME_ABSOLUTE is, the value is certainly a number
+				segmentCopy.renderedInPoint = (timelineObj.trigger.value as number)
+			}
+			segmentCopy.renderedDuration = timelineObj.duration
+
+			return {
+				segmentLineItem: segmentCopy
+			}
+		} else {
+			// object not found in timeline, don't override any values
+			return {}
+		}
 	} else {
 		// Don't expect any changes
 		return {}
