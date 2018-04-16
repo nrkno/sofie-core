34cab17021ec1e63f0015eae9ea23a04acabe324
Jan Starzak <jan.starzak@gmail.com>
2018-04-16 14:35:20 +0200

    Chore: Update Roboto font

--- meteor/client/ui/SegmentTimeline/TimelineGrid.tsx
+++ meteor/client/ui/SegmentTimeline/TimelineGrid.tsx
@@ -8,7 +8,7 @@ import { RundownUtils } from '../../lib/rundown'
 
 import { Settings } from '../../../lib/Settings'
 
-const GRID_FONT_URL = 'url("/origo-ui/fonts/roboto-bold-webfont.woff")'
+const GRID_FONT_URL = 'url("/fonts/Bold/Roboto-Bold.woff")'
 const TIMELINE_GRID_LABEL_COLOR = 'rgb(175,175,175)'
 const INNER_STEP_GRID_COLOR = 'rgb(112,112,112)'
 const LARGE_STEP_GRID_COLOR = 'rgb(112,112,112)'
