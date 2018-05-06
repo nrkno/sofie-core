092f5b2663bd68e2568122a5140e0d1997f95da7
Johan Nyman <johan@nytamin.se>
2018-05-06 16:52:47 +0200

    Feat: Implemment http-endpoint for external health-checkers to use

--- meteor/server/systemStatus.ts
+++ meteor/server/systemStatus.ts
@@ -1,8 +1,9 @@
 import * as _ from 'underscore'
+import { ServerResponse, IncomingMessage } from 'http'
+// @ts-ignore Meteor package
+import { Picker } from 'meteor/meteorhacks:picker'
 
 // This data structure is to be used to determine the system-wide status of the Core instance
-
-// Ref: https://github.com/nrkno/blaabok-mu/blob/master/Standarder/RFC-MU-2-Helsesjekk.md
 
 export enum StatusCode {
 
@@ -50,4 +51,27 @@ Meteor.methods({
 	'systemStatus.getSystemStatus': () => {
 		return getSystemStatus()
 	}
+})
+// Server route
+// according to spec at https://github.com/nrkno/blaabok-mu/blob/master/Standarder/RFC-MU-2-Helsesjekk.md
+Picker.route('/health', (params, req: IncomingMessage, res: ServerResponse, next) => {
+	let status = getSystemStatus()
+	res.setHeader('Content-Type', 'application/json')
+	let content = ''
+	if (
+		status.statusCode === StatusCode.GOOD ||
+		status.statusCode === StatusCode.WARNING_MINOR
+	) {
+		res.statusCode = 200
+		content = JSON.stringify({status: 'OK'})
+	} else {
+		res.statusCode = 500
+		content = JSON.stringify({
+			status: StatusCode[status.statusCode],
+			statusCode: status.statusCode,
+			messages: status.messages || []
+		})
+	}
+
+	res.end(content)
 })
