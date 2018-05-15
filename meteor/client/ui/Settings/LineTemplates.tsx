77f3a00237ca901757471fd9fb70eae2a5dc933f
Jan Starzak <jan.starzak@gmail.com>
2018-05-15 11:23:26 +0200

    Feat(code editor): implement monaco code editor For now, it's statically placed in /public, but I hope to have a meteor build plugin to just copy it over from node_modules

--- meteor/client/ui/Settings/LineTemplates.tsx
+++ meteor/client/ui/Settings/LineTemplates.tsx
@@ -8,7 +8,7 @@ import { EditAttribute } from '../../lib/EditAttribute'
 import { ModalDialog } from '../../lib/ModalDialog'
 import { withTracker } from '../../lib/ReactMeteorData/react-meteor-data'
 import { Spinner } from '../../lib/Spinner'
-import { literal } from '../../../lib/lib'
+import { literal, partial } from '../../../lib/lib'
 import { Random } from 'meteor/random'
 import * as faTrash from '@fortawesome/fontawesome-free-solid/faTrash'
 import * as faPencilAlt from '@fortawesome/fontawesome-free-solid/faPencilAlt'
@@ -26,20 +26,85 @@ interface IStateHeader {
 	code: string
 }
 
+interface IMonacoPropsHeader {
+	runtimeFunction: RuntimeFunction
+}
+
+class MonacoWrapper extends React.Component<IMonacoPropsHeader> {
+	static _monacoRequire: any
+	static _requireBuffer: any
+	static _monacoRef: any
+
+	_container: HTMLDivElement
+	_editor: monaco.editor.IStandaloneCodeEditor
+	_codeId: string
+
+	constructor (props) {
+		super(props)
+	}
+
+	attachEditor = () => {
+		this._editor = monaco.editor.create(document.getElementById('monaco-container'), {
+			value: this.props.runtimeFunction.code,
+			language: 'javascript'
+		})
+		this._editor.onDidChangeModelContent((e: monaco.editor.IModelContentChangedEvent) => {
+			RuntimeFunctions.update(this.props.runtimeFunction._id, {
+				$set: {
+					code: this._editor.getModel().getValue()
+				}
+			})
+		})
+	}
+
+	componentDidUpdate () {
+		if (this.props.runtimeFunction._id !== this._codeId) {
+			this._codeId = this.props.runtimeFunction._id
+			this._editor.setModel(monaco.editor.createModel(
+				this.props.runtimeFunction.code,
+				'javascript'
+			))
+		}
+	}
+
+	setRef = (el: HTMLDivElement) => {
+		if (el) {
+			if (!MonacoWrapper._monacoRef) {
+				let that = this
+				this._container = el
+				MonacoWrapper._requireBuffer = window['require']
+				window['require'] = undefined
+				let newScript = document.createElement('script')
+				newScript.addEventListener('load', () => {
+					MonacoWrapper._monacoRequire = MonacoWrapper._monacoRequire || window['require']
+					window['require'] = MonacoWrapper._requireBuffer
+					MonacoWrapper._monacoRequire.config({ paths: { 'vs': '/monaco-editor/min/vs' } })
+					MonacoWrapper._monacoRequire(['vs/editor/editor.main'], function () {
+						MonacoWrapper._monacoRef = monaco
+						that.attachEditor()
+					})
+				})
+				newScript.src = '/monaco-editor/min/vs/loader.js'
+				el.appendChild(newScript)
+			} else {
+				this.attachEditor()
+			}
+		}
+	}
+
+	render () {
+		return <div ref={this.setRef}>
+					<div id='monaco-container' className='runtime-function-edit__editor'></div>
+			   </div>
+	}
+}
+
 class LineTemplates extends React.Component<IPropsHeader & InjectedTranslateProps, IStateHeader> {
 	renderEditForm () {
 		const { t } = this.props
 
 		return (
-				<div className='runtime-function-edit mod mhl mvs'>
-					<EditAttribute
-						modifiedClassName='bghl'
-						attribute={'_code'}
-						obj={this.props.lineTemplate}
-						type='multiline'
-						collection={RuntimeFunctions}
-						className='runtime-function-edit__editor input text-input input-l'></EditAttribute>
-				</div>
+			<MonacoWrapper runtimeFunction={this.props.lineTemplate} />
 		)
 	}
 
