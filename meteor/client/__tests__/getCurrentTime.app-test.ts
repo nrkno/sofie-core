3cd1383a10461bc076ef94bd95a61494bb749f57
Jan Starzak <jan.starzak@gmail.com>
2018-05-21 08:41:08 +0200

    Chore: add test case for getCurrentTime

---
+++ meteor/client/__tests__/getCurrentTime.app-test.ts
@@ -0,0 +1,18 @@
+import * as chai from 'chai'
+import { Random } from 'meteor/random'
+import { } from 'mocha'
+
+import { getCurrentTime } from '../../lib/lib'
+
+const expect = chai.expect
+
+describe('getCurrentTime', function () {
+	it('check that getCurrentTime returns the same value in the Client and in the Server', function (done) {
+		Meteor.call('debug__printTime', (err, res) => {
+			if (err) {
+				done()
+			}
+			expect(res).to.equal(getCurrentTime())
+		})
+	})
+})
