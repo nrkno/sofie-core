53b2f22b102b5871804a7665dc15e33bfd664ff5
Johan Nyman <johan@nytamin.se>
2018-06-20 17:29:06 +0200

    fix: prevent default action for f1 & f3

---
+++ meteor/client/lib/keyboardShortcuts.ts
@@ -0,0 +1,14 @@
+import * as mousetrap from 'mousetrap'
+
+Meteor.startup(() => {
+
+	// Prevent opening the Help page:
+	mousetrap.bind('f1', (e: KeyboardEvent) => {
+		e.preventDefault()
+	}, 'keydown')
+
+	// Prevent Search from opening, thus grabbing focus:
+	mousetrap.bind('f3', (e: KeyboardEvent) => {
+		e.preventDefault()
+	}, 'keydown')
+})
