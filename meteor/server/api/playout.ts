dea4d09ed58f0aa4b65581fc715a9d5b7045afdd
Julian Waller <git@julusian.co.uk>
2018-07-05 14:33:55 +0100

    Revert "fix: use unique id for baseline items cache entries"
    This reverts commit cf89409a948ea77abae0303e1958ab2b6cfca453.

--- meteor/server/api/playout.ts
+++ meteor/server/api/playout.ts
@@ -131,7 +131,7 @@ export namespace ServerPlayoutAPI {
 				Body: [],
 				ID: new MosString128(''),
 
-			}, 'baseline_' + runningOrder._id)
+			}, 'baseline')
 
 			if (result.baselineItems) {
 				logger.info(`... got ${result.baselineItems.length} items from template.`)
