5a86a47100ef7ff8d9f200c17394e9e1c59345bf
Jan Starzak <jan.starzak@gmail.com>
2018-07-13 11:01:46 +0200

    Fix: Remove some logging from SourceLayerItem

--- meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
@@ -218,7 +218,7 @@ export class SourceLayerItem extends React.Component<ISourceLayerItemProps, ISou
 		let itemDuration = Math.min(segmentLineItem.duration || segmentLineItem.expectedDuration || segmentLineItem.renderedDuration || 0, this.props.segmentLineDuration - (segmentLineItem.renderedInPoint || 0))
 		if (segmentLineItem.infiniteMode !== undefined && segmentLineItem.infiniteMode !== SegmentLineItemLifespan.Normal) {
 			itemDuration = this.props.segmentLineDuration - (segmentLineItem.renderedInPoint || 0)
-			console.log(segmentLineItem.infiniteMode + ', ' + segmentLineItem.infiniteId)
+			// console.log(segmentLineItem.infiniteMode + ', ' + segmentLineItem.infiniteId)
 		}
 
 		// if (itemDuration === 0 && segmentLineItem.renderedInPoint !== null && segmentLineItem.renderedInPoint !== undefined) {
