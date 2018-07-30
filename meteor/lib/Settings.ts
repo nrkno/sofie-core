32e84bfff082c2d705798a0fabc4060afea63c24
Jan Starzak <jan.starzak@gmail.com>
2018-07-30 10:17:55 +0200

    Chore: allow env variables to set settings All defaultSettings.json property keys are converted to screaming snake case (FRAME_RATE and MEDIA_PREVIEW_SERVICE), etc.

--- meteor/lib/Settings.ts
+++ meteor/lib/Settings.ts
@@ -1,11 +1,24 @@
 import * as _ from 'underscore'
 
 const defaultSettings = require('./defaultSettings.json')
-let localSettings
+let localSettings = {}
 try {
 	localSettings = require('../settings.json')
 } catch (e) {
 	localSettings = {}
 }
 
-export const Settings = _.extend({}, defaultSettings, localSettings)
+let localSettingsEnv = {}
+function screamSnakeCase (input: string) {
+	return input.replace(/([A-Z])/g, '_$1').toUpperCase()
+}
+
+try {
+	_.keys(defaultSettings).forEach((setting) => {
+		localSettingsEnv[setting] = process.env[screamSnakeCase(setting)]
+	})
+} catch (e) {
+	localSettingsEnv = {}
+}
+
+export const Settings = _.extend({}, defaultSettings, localSettings, localSettingsEnv)
