d4fcc3f472e8d482bf0976adfb6e7889e99d0a3b
Johan Nyman <johan@nytamin.se>
2018-08-08 07:56:40 +0200

    feat: refactored initDB into 2 separate methods, so we can easilly update just the layers and not the infrastructure

--- meteor/server/api/initiateDatabase.ts
+++ meteor/server/api/initiateDatabase.ts
@@ -52,6 +52,130 @@ Meteor.methods({
 			return 'Do you really want to do this? You chould only do it when initializing a new database. Confirm with initDB(true).'
 		}
 		logger.info('initDB')
+		Meteor.call('initDB_layers', really)
+
+		PeripheralDevices.upsert('initDBPlayoutDeviceParent', {$set: literal<PeripheralDevice>({
+			_id: 'initDBPlayoutDeviceParent',
+			name: 'initDBPlayoutDeviceParent',
+			type: PeripheralDeviceAPI.DeviceType.PLAYOUT,
+			studioInstallationId: 'studio0',
+			created: getCurrentTime(),
+			status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
+			lastSeen: getCurrentTime(),
+			connected: false,
+			connectionId: null,
+			token: '',
+			settings: {
+				devices: {},
+				mediaScanner: {
+					host: '',
+					port: 8000
+				},
+				casparcgLauncher: {
+					host: '',
+					port: 8005
+				}
+			}
+		})})
+
+		PeripheralDevices.find({
+			type: PeripheralDeviceAPI.DeviceType.PLAYOUT
+		}).forEach((pd) => {
+			PeripheralDevices.update(pd._id, {$set: {
+				'settings.devices.casparcg0': ((pd['settings'] || {})['devices'] || {})['casparcg0'] || {
+					type: PlayoutDeviceType.CASPARCG,
+					options: {
+						host: '160.67.87.50',
+						port: 5250
+					}
+				},
+				'settings.devices.atem0': ((pd['settings'] || {})['devices'] || {})['atem0'] || {
+					type: PlayoutDeviceType.ATEM,
+					options: {
+						host: '160.67.87.51',
+						port: 9910
+					}
+				},
+				'settings.devices.lawo0': ((pd['settings'] || {})['devices'] || {})['lawo0'] || {
+					type: PlayoutDeviceType.LAWO,
+					options: {
+						host: '160.67.96.51',
+						port: 9000,
+						sourcesPath: 'Sapphire.Sources',
+						rampMotorFunctionPath: '1.5.2'
+					}
+				},
+				'settings.devices.abstract0': ((pd['settings'] || {})['devices'] || {})['abstract0'] || {
+					type: PlayoutDeviceType.ABSTRACT,
+					options: {
+					}
+				},
+				'settings.devices.http0': ((pd['settings'] || {})['devices'] || {})['http0'] || {
+					type: PlayoutDeviceType.HTTPSEND,
+					options: {
+					}
+				}
+			}})
+			// PeripheralDevices.update(pd._id, {$set: {
+			// 	mappings: mappings
+			// }})
+		})
+		_.each(((PeripheralDevices.findOne('initDBPlayoutDeviceParent') || {})['settings'] || {}).devices, (device, key) => {
+			PeripheralDevices.upsert('initDBPlayoutDevice' + key, {$set: literal<PeripheralDevice>({
+				_id: 'initDBPlayoutDevice' + key,
+				name: 'initDBPlayoutDevice' + key,
+				type: PeripheralDeviceAPI.DeviceType.OTHER,
+				studioInstallationId: 'studio0',
+				parentDeviceId: 'initDBPlayoutDeviceParent',
+				created: getCurrentTime(),
+				status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
+				lastSeen: getCurrentTime(),
+				connected: false,
+				connectionId: null,
+				token: ''
+			})})
+		})
+
+		PeripheralDevices.upsert('initDBMosDeviceParent', {$set: literal<PeripheralDevice>({
+			_id: 'initDBMosDeviceParent',
+			name: 'initDBMosDeviceParent',
+			type: PeripheralDeviceAPI.DeviceType.MOSDEVICE,
+			studioInstallationId: 'studio0',
+			created: getCurrentTime(),
+			status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
+			lastSeen: getCurrentTime(),
+			connected: false,
+			connectionId: null,
+			token: ''
+		})})
+
+		PeripheralDevices.find({
+			type: PeripheralDeviceAPI.DeviceType.MOSDEVICE
+		}).forEach((pd) => {
+			PeripheralDevices.update(pd._id, {$set: {
+				'settings.mosId': 'SOFIE1.XPRO.MOS',
+				'settings.devices.enps0': ((pd['settings'] || {})['devices'] || {})['enps0'] || {
+					primary: {
+						id: 'MAENPSTEST14',
+						host: '160.67.149.155'
+					},
+					secondary: {
+						id: 'MAENPSTEST15',
+						host: '160.67.149.156'
+					}
+				},
+			}})
+			// PeripheralDevices.update(pd._id, {$set: {
+			// 	mappings: mappings
+			// }})
+		})
+	},
+	'initDB_layers': (really) => {
+
+		if (!really) {
+			return 'Do you really want to do this? You chould only do it when initializing a new database. Confirm with initDB(true).'
+		}
+		// initializes the stuff that's not place specific (so not setting things hat has ip adresses, etc)
 		// Initiate database:
 		StudioInstallations.upsert('studio0', {$set: {
 			name: 'DKSL',
@@ -535,121 +659,5 @@ Meteor.methods({
 			baselineTemplate: 'baseline',
 			messageTemplate: 'message'
 		}})
-
-		PeripheralDevices.upsert('initDBPlayoutDeviceParent', {$set: literal<PeripheralDevice>({
-			_id: 'initDBPlayoutDeviceParent',
-			name: 'initDBPlayoutDeviceParent',
-			type: PeripheralDeviceAPI.DeviceType.PLAYOUT,
-			studioInstallationId: 'studio0',
-			created: getCurrentTime(),
-			status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
-			lastSeen: getCurrentTime(),
-			connected: false,
-			connectionId: null,
-			token: '',
-			settings: {
-				devices: {},
-				mediaScanner: {
-					host: '',
-					port: 8000
-				},
-				casparcgLauncher: {
-					host: '',
-					port: 8005
-				}
-			}
-		})})
-
-		PeripheralDevices.find({
-			type: PeripheralDeviceAPI.DeviceType.PLAYOUT
-		}).forEach((pd) => {
-			PeripheralDevices.update(pd._id, {$set: {
-				'settings.devices.casparcg0': ((pd['settings'] || {})['devices'] || {})['casparcg0'] || {
-					type: PlayoutDeviceType.CASPARCG,
-					options: {
-						host: '160.67.87.50',
-						port: 5250
-					}
-				},
-				'settings.devices.atem0': ((pd['settings'] || {})['devices'] || {})['atem0'] || {
-					type: PlayoutDeviceType.ATEM,
-					options: {
-						host: '160.67.87.51',
-						port: 9910
-					}
-				},
-				'settings.devices.lawo0': ((pd['settings'] || {})['devices'] || {})['lawo0'] || {
-					type: PlayoutDeviceType.LAWO,
-					options: {
-						host: '160.67.96.51',
-						port: 9000,
-						sourcesPath: 'Sapphire.Sources',
-						rampMotorFunctionPath: '1.5.2'
-					}
-				},
-				'settings.devices.abstract0': ((pd['settings'] || {})['devices'] || {})['abstract0'] || {
-					type: PlayoutDeviceType.ABSTRACT,
-					options: {
-					}
-				},
-				'settings.devices.http0': ((pd['settings'] || {})['devices'] || {})['http0'] || {
-					type: PlayoutDeviceType.HTTPSEND,
-					options: {
-					}
-				}
-			}})
-			// PeripheralDevices.update(pd._id, {$set: {
-			// 	mappings: mappings
-			// }})
-		})
-		_.each(((PeripheralDevices.findOne('initDBPlayoutDeviceParent') || {})['settings'] || {}).devices, (device, key) => {
-			PeripheralDevices.upsert('initDBPlayoutDevice' + key, {$set: literal<PeripheralDevice>({
-				_id: 'initDBPlayoutDevice' + key,
-				name: 'initDBPlayoutDevice' + key,
-				type: PeripheralDeviceAPI.DeviceType.OTHER,
-				studioInstallationId: 'studio0',
-				parentDeviceId: 'initDBPlayoutDeviceParent',
-				created: getCurrentTime(),
-				status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
-				lastSeen: getCurrentTime(),
-				connected: false,
-				connectionId: null,
-				token: ''
-			})})
-		})
-
-		PeripheralDevices.upsert('initDBMosDeviceParent', {$set: literal<PeripheralDevice>({
-			_id: 'initDBMosDeviceParent',
-			name: 'initDBMosDeviceParent',
-			type: PeripheralDeviceAPI.DeviceType.MOSDEVICE,
-			studioInstallationId: 'studio0',
-			created: getCurrentTime(),
-			status: {statusCode: PeripheralDeviceAPI.StatusCode.BAD},
-			lastSeen: getCurrentTime(),
-			connected: false,
-			connectionId: null,
-			token: ''
-		})})
-
-		PeripheralDevices.find({
-			type: PeripheralDeviceAPI.DeviceType.MOSDEVICE
-		}).forEach((pd) => {
-			PeripheralDevices.update(pd._id, {$set: {
-				'settings.mosId': 'SOFIE1.XPRO.MOS',
-				'settings.devices.enps0': ((pd['settings'] || {})['devices'] || {})['enps0'] || {
-					primary: {
-						id: 'MAENPSTEST14',
-						host: '160.67.149.155'
-					},
-					secondary: {
-						id: 'MAENPSTEST15',
-						host: '160.67.149.156'
-					}
-				},
-			}})
-			// PeripheralDevices.update(pd._id, {$set: {
-			// 	mappings: mappings
-			// }})
-		})
 	}
 })
