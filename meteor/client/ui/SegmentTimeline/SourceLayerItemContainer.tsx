da30f52f50c748b0b62ce2493746dc5ad7e56ad6
Jan Starzak <jan.starzak@gmail.com>
2018-08-30 09:44:31 +0200

    Feat: use mediaId instead of objId for MediaObjects match

--- meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
@@ -97,7 +97,7 @@ export const SourceLayerItemContainer = withTracker((props: IPropsHeader) => {
 				if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
 					const content = props.segmentLineItem.content as VTContent
 					const mediaObject = MediaObjects.findOne({
-						objId: content.fileName.toUpperCase()
+						mediaId: content.fileName.toUpperCase()
 					})
 					// If media object not found, then...
 					if (!mediaObject) {
@@ -118,7 +118,7 @@ export const SourceLayerItemContainer = withTracker((props: IPropsHeader) => {
 				if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
 					const content = props.segmentLineItem.content as LiveSpeakContent
 					const mediaObject = MediaObjects.findOne({
-						objId: content.fileName.toUpperCase()
+						mediaId: content.fileName.toUpperCase()
 					})
 					// If media object not found, then...
 					if (!mediaObject) {
