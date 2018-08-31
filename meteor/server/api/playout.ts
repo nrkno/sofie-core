f733ffebae5a5151a1669f1491b41c6401b5b485
Jan Starzak <jan.starzak@gmail.com>
2018-08-31 20:53:32 +0200

    Fix: don't break roActivate if no atemSSrcBackground defined in config

--- meteor/server/api/playout.ts
+++ meteor/server/api/playout.ts
@@ -19,7 +19,7 @@ import { PlayoutTimelinePrefixes, LookaheadMode } from '../../lib/api/playout'
 import { TemplateContext, TemplateResultAfterPost, runNamedTemplate } from './templates/templates'
 import { RunningOrderBaselineAdLibItem, RunningOrderBaselineAdLibItems } from '../../lib/collections/RunningOrderBaselineAdLibItems'
 import { sendStoryStatus } from './peripheralDevice'
-import { StudioInstallations, StudioInstallation } from '../../lib/collections/StudioInstallations'
+import { StudioInstallations, StudioInstallation, IStudioConfigItem } from '../../lib/collections/StudioInstallations'
 import { PlayoutAPI } from '../../lib/api/playout'
 import { triggerExternalMessage } from './externalMessage'
 import { getHash } from '../lib'
@@ -112,8 +112,10 @@ export namespace ServerPlayoutAPI {
 		}).fetch()
 		// PeripheralDevices.find()
 
-		const ssrcBg = [ studioInstallation.config.find((o) => o._id === 'atemSSrcBackground') ]
+		const ssrcBg: Array<IStudioConfigItem> = []
+		const ssrcBg1 = studioInstallation.config.find((o) => o._id === 'atemSSrcBackground')
 		const ssrcBg2 = studioInstallation.config.find((o) => o._id === 'atemSSrcBackground2')
+		if (ssrcBg1) ssrcBg.push(ssrcBg1)
 		if (ssrcBg2) ssrcBg.push(ssrcBg2)
 		if (ssrcBg.length > 1) logger.info(ssrcBg[0]!.value + ' and ' + ssrcBg[1]!.value + ' should be loaded to atems')
 		if (ssrcBg.length > 0) logger.info(ssrcBg[0]!.value + ' should be loaded to atems')
