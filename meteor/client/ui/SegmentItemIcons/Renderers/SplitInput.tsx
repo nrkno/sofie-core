ba70dfa6cd13e52f54b986b5150a82c29fce5403
Jan Starzak <jan.starzak@gmail.com>
2018-09-07 15:14:10 +0200

    Feat: add support for two-tone splits with the same source type

--- meteor/client/ui/SegmentItemIcons/Renderers/SplitInput.tsx
+++ meteor/client/ui/SegmentItemIcons/Renderers/SplitInput.tsx
@@ -18,11 +18,33 @@ export default class SplitInputIcon extends React.Component<{ abbreviation?: str
 		}
 	}
 
+	getSourceType (type: RundownAPI.SourceLayerType): string {
+		switch (type) {
+			case RundownAPI.SourceLayerType.CAMERA:
+				return 'camera'
+			case RundownAPI.SourceLayerType.REMOTE:
+				return 'remote'
+			case RundownAPI.SourceLayerType.VT:
+				return 'vt'
+		}
+		return ''
+	}
+
+	getLeftSourceType (c: SplitsContent): string {
+		const left = (c.boxSourceConfiguration[0] || {}).type || RundownAPI.SourceLayerType.CAMERA
+		return this.getSourceType(left)
+	}
+
+	getRightSourceType (c: SplitsContent): string {
+		const right = (c.boxSourceConfiguration[1] || {}).type || RundownAPI.SourceLayerType.REMOTE
+		return this.getSourceType(right) + (this.getLeftSourceType(c) === this.getSourceType(right) ? ' second' : '')
+	}
+
 	render () {
 		return (
 			<svg className='segment_line_item_icon' version='1.1' viewBox='0 0 126.5 89' xmlns='http://www.w3.org/2000/svg'>
-				<rect width='63.25' height='89' className='camera'/>
-				<rect width='63.25' height='89' x='63.25' className='remote'/>
+				<rect width='63.25' height='89' className={this.props.segmentLineItem && this.props.segmentLineItem.content ? this.getLeftSourceType(this.props.segmentLineItem.content as SplitsContent) : 'camera'} />
+				<rect width='63.25' height='89' x='63.25' className={this.props.segmentLineItem && this.props.segmentLineItem.content ? this.getLeftSourceType(this.props.segmentLineItem.content as SplitsContent) : 'remote'} />
 				<text x='9.6414976' textLength='106.5' y='71.513954' textAnchor='middle' style={{ fill: '#ffffff', 'fontFamily': 'open-sans', 'fontSize': '40px', 'letterSpacing': '0px', 'lineHeight': '1.25', 'wordSpacing': '0px', 'textShadow': '0 2px 9px rgba(0, 0, 0, 0.5)' }} xmlSpace='preserve'>
 					<tspan x='63.25' y='71.513954' style={{ fill: '#ffffff', 'fontFamily': 'Roboto', 'fontSize': '75px', 'fontWeight': 100 }}>{this.props.segmentLineItem && this.props.segmentLineItem.content ?
 							this.getCameraLabel(this.props.segmentLineItem.content as SplitsContent)
