38380e12ecca6a6d37859e5386e19729f43b2ad2
Jan Starzak <jan.starzak@gmail.com>
2018-09-12 23:37:50 +0200

    Feat: a better implementation of SourceLayerItemContainer

--- meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItemContainer.tsx
@@ -47,145 +47,177 @@ interface IPropsHeader {
 	scrollWidth: number
 }
 /** This is a container component that allows ractivity with the Timeline collection */
-export const SourceLayerItemContainer = withTracker((props: IPropsHeader) => {
-	let overrides: {
-		[key: string]: any
-	} = {}
+export const SourceLayerItemContainer = class extends MeteorReactComponent<IPropsHeader> {
+	private mediaObjectSub: Meteor.SubscriptionHandle
+	private statusComp: Tracker.Computation
+	private objId: string
+	private overrides: any
 
-	// Disable timeline override, since it became too complicated to correctly override SLI properties
-	// with data from the Timeline
-	// ---
+	updateMediaObjectSubscription () {
+		if (this.props.segmentLineItem && this.props.segmentLineItem.sourceLayer) {
+			const sli = this.props.segmentLineItem
+			let objId: string | undefined = undefined
 
-	// Override based on Timeline collection
-	if (props.isLiveLine) {
-		// Check in Timeline collection for any changes to the related object
-		let timelineObj = Timeline.findOne({ _id: PlayoutTimelinePrefixes.SEGMENT_LINE_ITEM_GROUP_PREFIX + props.segmentLineItem._id })
-
-		if (timelineObj) {
-			let segmentCopy = (_.clone(overrides.segmentLineItem || props.segmentLineItem) as SegmentLineItemUi)
-
-			if (timelineObj.trigger.type === TriggerType.TIME_ABSOLUTE) {
-				segmentCopy.trigger = timelineObj.trigger
-				if (_.isNumber(timelineObj.trigger.value)) { // this is a normal absolute trigger value
-					segmentCopy.renderedInPoint = (timelineObj.trigger.value as number)
-				} else if (timelineObj.trigger.value === 'now') { // this is a special absolute trigger value
-					if (props.segmentLine && props.segmentLine.startedPlayback && props.segmentLine.getLastStartedPlayback()) {
-						segmentCopy.renderedInPoint = getCurrentTime() - (props.segmentLine.getLastStartedPlayback() || 0)
-					} else {
-						segmentCopy.renderedInPoint = 0
-					}
-				} else {
-					segmentCopy.renderedInPoint = 0
-				}
+			switch (this.props.segmentLineItem.sourceLayer.type) {
+				case RundownAPI.SourceLayerType.VT:
+					objId = (sli.content as VTContent).fileName.toUpperCase()
+					break
+				case RundownAPI.SourceLayerType.LIVE_SPEAK:
+					objId = (sli.content as LiveSpeakContent).fileName.toUpperCase()
+					break
 			}
 
-			if (typeof timelineObj.duration !== 'string' && !segmentCopy.cropped) {
-				segmentCopy.renderedDuration = timelineObj.duration !== 0 ? timelineObj.duration : (props.segmentLineDuration - (segmentCopy.renderedInPoint || 0))
-			}
-			// console.log(segmentCopy.renderedDuration)
-
-			overrides.segmentLineItem = _.extend(overrides.segmentLineItem || {}, segmentCopy)
-		}
-	}
-
-	// Check item status
-	if (props.segmentLineItem.sourceLayer) {
-		let newStatus: RundownAPI.LineItemStatusCode = RundownAPI.LineItemStatusCode.UNKNOWN
-		let metadata: any = undefined
-		switch (props.segmentLineItem.sourceLayer.type) {
-			case RundownAPI.SourceLayerType.VT:
-				if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
-					const content = props.segmentLineItem.content as VTContent
-					const mediaObject = MediaObjects.findOne({
-						mediaId: content.fileName.toUpperCase()
-					})
-					// If media object not found, then...
-					if (!mediaObject) {
-						newStatus = RundownAPI.LineItemStatusCode.SOURCE_MISSING
-					// All VT content should have at least two streams
-					} else if (mediaObject && mediaObject.mediainfo && mediaObject.mediainfo.streams.length < 2) {
-						newStatus = RundownAPI.LineItemStatusCode.SOURCE_BROKEN
-					} else if (mediaObject) {
-						newStatus = RundownAPI.LineItemStatusCode.OK
-					}
-
-					if (mediaObject) {
-						metadata = mediaObject
-					}
-				}
-				break
-			case RundownAPI.SourceLayerType.LIVE_SPEAK:
-				if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
-					const content = props.segmentLineItem.content as LiveSpeakContent
-					const mediaObject = MediaObjects.findOne({
-						mediaId: content.fileName.toUpperCase()
-					})
-					// If media object not found, then...
-					if (!mediaObject) {
-						newStatus = RundownAPI.LineItemStatusCode.SOURCE_MISSING
-						// All VT content should have at least two streams
-					} else if (mediaObject && mediaObject.mediainfo && mediaObject.mediainfo.streams.length < 2) {
-						newStatus = RundownAPI.LineItemStatusCode.SOURCE_BROKEN
-					} else if (mediaObject) {
-						newStatus = RundownAPI.LineItemStatusCode.OK
-					}
-
-					if (mediaObject) {
-						metadata = mediaObject
-					}
-				}
-				break
-		}
-		if (newStatus !== props.segmentLineItem.status || metadata) {
-			let segmentCopy = (_.clone(overrides.segmentLineItem || props.segmentLineItem) as SegmentLineItemUi)
-
-			segmentCopy.status = newStatus
-			segmentCopy.metadata = metadata
-
-			overrides.segmentLineItem = _.extend(overrides.segmentLineItem || {}, segmentCopy)
-		}
-	}
-
-	return overrides
-})(
-class extends MeteorReactComponent<IPropsHeader> {
-	private objIdCF: any
-
-	componentWillMount () {
-		this.objIdCF = new ComputedField(() => {
-			let sli = SegmentLineItems.findOne(this.props.segmentLineItem._id)
-			if (sli) {
-				if (this.props.segmentLineItem.sourceLayer) {
-					switch (this.props.segmentLineItem.sourceLayer.type) {
-						case RundownAPI.SourceLayerType.VT:
-							return (sli.content as VTContent).fileName.toUpperCase()
-						case RundownAPI.SourceLayerType.LIVE_SPEAK:
-							return (sli.content as LiveSpeakContent).fileName.toUpperCase()
-					}
-				}
-			}
-			return ''
-		})
-
-		this.autorun(() => {
-			let objId = this.objIdCF()
-			if (objId) {
+			if (objId && objId !== this.objId) {
+				// if (this.mediaObjectSub) this.mediaObjectSub.stop()
+				this.objId = objId
 				this.subscribe('mediaObjects', this.props.runningOrder.studioInstallationId, {
-					mediaId: objId
+					mediaId: this.objId
 				})
 			}
+		} else {
+			console.error('One of the SegmentLineItem\'s is invalid:', this.props.segmentLineItem)
+		}
+	}
+
+	shouldDataTrackerUpdate (prevProps: IPropsHeader): boolean {
+		if (this.props.segmentLineItem !== prevProps.segmentLineItem) return true
+		if (this.props.isLiveLine !== prevProps.isLiveLine) return true
+		return false
+	}
+
+	updateDataTracker () {
+		this.statusComp = this.autorun(() => {
+			const props = this.props
+			this.overrides = {}
+			const overrides = this.overrides
+
+			// console.log(`${this.props.segmentLineItem._id}: running data tracker`)
+
+			if (props.isLiveLine) {
+				// Check in Timeline collection for any changes to the related object
+				let timelineObj = Timeline.findOne({ _id: PlayoutTimelinePrefixes.SEGMENT_LINE_ITEM_GROUP_PREFIX + props.segmentLineItem._id })
+
+				if (timelineObj) {
+					let segmentCopy = (_.clone(overrides.segmentLineItem || props.segmentLineItem) as SegmentLineItemUi)
+
+					if (timelineObj.trigger.type === TriggerType.TIME_ABSOLUTE) {
+						segmentCopy.trigger = timelineObj.trigger
+						if (_.isNumber(timelineObj.trigger.value)) { // this is a normal absolute trigger value
+							segmentCopy.renderedInPoint = (timelineObj.trigger.value as number)
+						} else if (timelineObj.trigger.value === 'now') { // this is a special absolute trigger value
+							if (props.segmentLine && props.segmentLine.startedPlayback && props.segmentLine.getLastStartedPlayback()) {
+								segmentCopy.renderedInPoint = getCurrentTime() - (props.segmentLine.getLastStartedPlayback() || 0)
+							} else {
+								segmentCopy.renderedInPoint = 0
+							}
+						} else {
+							segmentCopy.renderedInPoint = 0
+						}
+					}
+
+					if (typeof timelineObj.duration !== 'string' && !segmentCopy.cropped) {
+						segmentCopy.renderedDuration = timelineObj.duration !== 0 ? timelineObj.duration : (props.segmentLineDuration - (segmentCopy.renderedInPoint || 0))
+					}
+					// console.log(segmentCopy.renderedDuration)
+
+					overrides.segmentLineItem = _.extend(overrides.segmentLineItem || {}, segmentCopy)
+				}
+			}
+
+			// Check item status
+			if (props.segmentLineItem.sourceLayer) {
+				let newStatus: RundownAPI.LineItemStatusCode = RundownAPI.LineItemStatusCode.UNKNOWN
+				let metadata: any = undefined
+				switch (props.segmentLineItem.sourceLayer.type) {
+					case RundownAPI.SourceLayerType.VT:
+						if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
+							const content = props.segmentLineItem.content as VTContent
+							const mediaObject = MediaObjects.findOne({
+								mediaId: content.fileName.toUpperCase()
+							})
+							// If media object not found, then...
+							if (!mediaObject) {
+								newStatus = RundownAPI.LineItemStatusCode.SOURCE_MISSING
+								// All VT content should have at least two streams
+							} else if (mediaObject && mediaObject.mediainfo && mediaObject.mediainfo.streams.length < 2) {
+								newStatus = RundownAPI.LineItemStatusCode.SOURCE_BROKEN
+							} else if (mediaObject) {
+								newStatus = RundownAPI.LineItemStatusCode.OK
+							}
+
+							if (mediaObject) {
+								metadata = mediaObject
+							}
+						}
+						break
+					case RundownAPI.SourceLayerType.LIVE_SPEAK:
+						if (props.segmentLineItem.content && props.segmentLineItem.content.fileName) {
+							const content = props.segmentLineItem.content as LiveSpeakContent
+							const mediaObject = MediaObjects.findOne({
+								mediaId: content.fileName.toUpperCase()
+							})
+							// If media object not found, then...
+							if (!mediaObject) {
+								newStatus = RundownAPI.LineItemStatusCode.SOURCE_MISSING
+								// All VT content should have at least two streams
+							} else if (mediaObject && mediaObject.mediainfo && mediaObject.mediainfo.streams.length < 2) {
+								newStatus = RundownAPI.LineItemStatusCode.SOURCE_BROKEN
+							} else if (mediaObject) {
+								newStatus = RundownAPI.LineItemStatusCode.OK
+							}
+
+							if (mediaObject) {
+								metadata = mediaObject
+							}
+						}
+						break
+				}
+				if (newStatus !== props.segmentLineItem.status || metadata) {
+					let segmentCopy = (_.clone(overrides.segmentLineItem || props.segmentLineItem) as SegmentLineItemUi)
+
+					segmentCopy.status = newStatus
+					segmentCopy.metadata = metadata
+
+					overrides.segmentLineItem = _.extend(overrides.segmentLineItem || {}, segmentCopy)
+				}
+			} else {
+				console.error(`SegmentLineItem "${props.segmentLineItem._id}" has no sourceLayer:`, props.segmentLineItem)
+			}
+
+			this.forceUpdate()
 		})
+		// this.statusComp.onInvalidate(() => {
+		// 	console.log(`Invalidated "${this.props.segmentLineItem._id}"...`)
+		// })
+		// this.statusComp.onStop(() => {
+		// 	console.log(`Stopping "${this.props.segmentLineItem._id}...`)
+		// })
+	}
+
+	componentDidMount () {
+		Meteor.defer(() => {
+			this.updateMediaObjectSubscription()
+			this.updateDataTracker()
+		})
+	}
+
+	componentDidUpdate (prevProps: IPropsHeader) {
+		Meteor.defer(() => {
+			this.updateMediaObjectSubscription()
+		})
+		if (this.shouldDataTrackerUpdate(prevProps)) {
+			// console.log('Invalidating computation!', this.statusComp.stopped, this.statusComp.invalidated)
+			this.statusComp.invalidate()
+		}
 	}
 
 	componentWillUnmount () {
 		super.componentWillUnmount()
-		this.objIdCF.stop()
 	}
 
 	render () {
 		return (
-			<SourceLayerItem {...this.props} />
+			<SourceLayerItem {...this.props} {...this.overrides} />
 		)
 	}
 }
-)
