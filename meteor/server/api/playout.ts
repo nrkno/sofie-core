4c56102ad6f15d967452f9f185972f5a64a7d1ed
Julian Waller <git@julusian.co.uk>
2018-09-13 09:29:40 +0100

    fix(transition): Using wrong overlap when combined with autonext

--- meteor/server/api/playout.ts
+++ meteor/server/api/playout.ts
@@ -2359,13 +2359,15 @@ export const updateTimeline: (studioInstallationId: string, forceNowToTime?: Tim
 			let nextSegmentLineItemGroup = createSegmentLineGroup(nextSegmentLineItem, 0)
 			if (currentSegmentLineGroup) {
 				const allowTransition = !currentSegmentLine.disableOutTransition
-				let overlapDuration = currentSegmentLine.transitionDuration || 0
+				let overlapDuration = nextSegmentLineItem.transitionDuration || 0
 				if (!nextSegmentLineItem.transitionDuration && allowTransition) {
 					const transitionObjs = nextSegmentLineItem.getSegmentLinesItems().filter(i => i.isTransition)
 					overlapDuration = (transitionObjs && transitionObjs.length > 0) ? transitionObjs[0].duration || 0 : 0
 				} else if (!allowTransition) {
 					overlapDuration = currentSegmentLine.autoNextOverlap || 0
 				}
+
+				console.log('OVER: ', allowTransition, overlapDuration, )
 
 				nextSegmentLineItemGroup.trigger = literal<ITimelineTrigger>({
 					type: TriggerType.TIME_RELATIVE,
