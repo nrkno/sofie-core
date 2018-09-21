ca00c04ae20da7988f430f0482518c9b90d57714
Jan Starzak <jan.starzak@gmail.com>
2018-09-21 11:43:20 +0200

    fix:translate datePicker

--- meteor/client/lib/datePicker.tsx
+++ meteor/client/lib/datePicker.tsx
@@ -8,6 +8,7 @@ import * as FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import * as faChevronRight from '@fortawesome/fontawesome-free-solid/faChevronRight'
 import * as faChevronLeft from '@fortawesome/fontawesome-free-solid/faChevronLeft'
 import { Time } from '../../lib/lib'
+import { translate, InjectedTranslateProps } from 'react-i18next'
 
 interface IProps {
 	from: Time
@@ -18,8 +19,8 @@ interface IState {
 	dateFrom: moment.Moment
 	dateTo: moment.Moment
 }
-export class DatePickerFromTo extends React.Component<IProps, IState> {
-	constructor (props: IProps) {
+export const DatePickerFromTo = translate()(class DatePickerFromTo extends React.Component<IProps & InjectedTranslateProps, IState> {
+	constructor (props: IProps & InjectedTranslateProps) {
 		super(props)
 
 		this.state = {
@@ -72,19 +73,20 @@ export class DatePickerFromTo extends React.Component<IProps, IState> {
 		})
 	}
 	render () {
+		const {t} = this.props
 		return (
 			<div className='datepicker-from-to'>
 				<button className='action-btn mod mhm' onClick={this.onClickPrevious}>
 					<FontAwesomeIcon icon={faChevronLeft} />
 				</button>
 				<label className='mod mhs mvn'>
-					From:
+					{t('From')}
 					<div className='picker expco'>
 						<DatePicker dateFormat='YYYY-MM-DD' selected={this.state.dateFrom} onChange={this.handleChangeFrom} className='expco-title' />
 					</div>
 				</label>
 				<label className='mod mhs mvn'>
-					Through:
+					{t('Until')}
 					<div className='picker expco'>
 						<DatePicker dateFormat='YYYY-MM-DD' selected={this.state.dateTo} onChange={this.handleChangeTo} className='expco-title' />
 					</div>
@@ -95,4 +97,4 @@ export class DatePickerFromTo extends React.Component<IProps, IState> {
 			</div>
 		)
 	}
-}
+})
