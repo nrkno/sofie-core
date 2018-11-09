1c96263263bb450d459eb525bd02bfb3066416b1
Johan Nyman <johan@nytamin.se>
2018-11-09 08:08:53 +0100

    feat: system version status monitoring

--- meteor/lib/collections/CoreSystem.ts
+++ meteor/lib/collections/CoreSystem.ts
@@ -2,6 +2,7 @@ import { Mongo } from 'meteor/mongo'
 import { TransformedCollection } from '../typings/meteor'
 import { Time, registerCollection, getCurrentTime } from '../lib'
 import { Meteor } from 'meteor/meteor'
+import * as _ from 'underscore'
 
 export const SYSTEM_ID = 'core'
 export interface ICoreSystem {
@@ -23,4 +24,42 @@ registerCollection('CoreSystem', CoreSystem)
 
 export function getCoreSystem () {
 	return CoreSystem.findOne(SYSTEM_ID)
+}
+export function getCoreSystemCursor () {
+	return CoreSystem.find(SYSTEM_ID)
+}
+export interface Version {
+	toString: () => string
+	major: number
+	minor: number
+	patch: number
+	label?: string
+}
+export function parseVersion (v: string): Version {
+
+	// https://github.com/semver/semver/issues/232
+	let m = (v + '').match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/)
+
+	if (m) {
+		let major = parseInt(m[1], 10)
+		let minor = parseInt(m[2], 10)
+		let patch = parseInt(m[3], 10)
+		let label = (m[4] ? (m[4] + '').trim() : '')
+		if (
+			!_.isNaN(major) &&
+			!_.isNaN(minor) &&
+			!_.isNaN(patch)
+		) {
+			return {
+				major: major,
+				minor: minor,
+				patch: patch,
+				label: label,
+				toString: () => {
+					return `${major}.${minor}.${patch}` + (label ? '-' + label : '')
+				}
+			}
+		}
+	}
+	throw new Meteor.Error(500, `Invalid version: "${v}"`)
 }
--- meteor/server/coreSystem.ts
+++ meteor/server/coreSystem.ts
@@ -1,19 +1,83 @@
-import { getCoreSystem, CoreSystem, SYSTEM_ID } from '../lib/collections/CoreSystem'
+import { getCoreSystem, CoreSystem, SYSTEM_ID, getCoreSystemCursor, parseVersion } from '../lib/collections/CoreSystem'
 import { getCurrentTime } from '../lib/lib'
 import { Meteor } from 'meteor/meteor'
 import { CURRENT_DATABASE_VERSION } from './databaseMigration'
+import { setSystemStatus, StatusCode, StatusObject } from './systemStatus'
 
 function initializeCoreSystem () {
 	let system = getCoreSystem()
 	if (!system) {
+		let version = parseVersion(CURRENT_DATABASE_VERSION)
 		CoreSystem.insert({
 			_id: SYSTEM_ID,
 			created: getCurrentTime(),
 			modified: getCurrentTime(),
-			version: CURRENT_DATABASE_VERSION
+			version: version.toString()
 		})
 	}
 
+	// Monitor database changes
+
+	let cursor = getCoreSystemCursor()
+	cursor.observeChanges({
+		added: checkDatabaseVersion,
+		changed: checkDatabaseVersion,
+		removed: checkDatabaseVersion
+	})
+
+	checkDatabaseVersion()
+}
+
+function checkDatabaseVersion () {
+	let databaseSystem = getCoreSystem()
+	if (databaseSystem) {
+
+		if (databaseSystem.version) {
+			let dbVersion = parseVersion(databaseSystem.version)
+			let currentVersion = parseVersion(CURRENT_DATABASE_VERSION)
+
+			if (dbVersion.major !== currentVersion.major) {
+
+				setSystemStatus('databaseVersion', {
+					statusCode: StatusCode.BAD,
+					messages: [`Database version mismatch (major version differ): system version: ${currentVersion.toString()}, database version: ${dbVersion.toString()} (to fix, run migration)`]
+				})
+			} else if (dbVersion.minor !== currentVersion.minor) {
+
+				setSystemStatus('databaseVersion', {
+					statusCode: StatusCode.WARNING_MAJOR,
+					messages: [`Database version mismatch (minor version differ): system version: ${currentVersion.toString()}, database version: ${dbVersion.toString()} (to fix, run migration)`]
+				})
+			} else if (dbVersion.patch !== currentVersion.patch) {
+
+				setSystemStatus('databaseVersion', {
+					statusCode: StatusCode.WARNING_MINOR,
+					messages: [`Database version mismatch (patch differ): system version: ${currentVersion.toString()}, database version: ${dbVersion.toString()} (to fix, run migration)`]
+				})
+			} else if (dbVersion.label !== currentVersion.label) {
+
+				setSystemStatus('databaseVersion', {
+					statusCode: StatusCode.WARNING_MINOR,
+					messages: [`Database version mismatch (label differ): system version: ${currentVersion.toString()}, database version: ${dbVersion.toString()} (to fix, run migration)`]
+				})
+			} else {
+				setSystemStatus('databaseVersion', {
+					statusCode: StatusCode.GOOD,
+					messages: [`System version: ${currentVersion.toString()}`]
+				})
+			}
+		} else {
+			setSystemStatus('databaseVersion', {
+				statusCode: StatusCode.FATAL,
+				messages: ['Database version missing']
+			})
+		}
+	} else {
+		setSystemStatus('databaseVersion', {
+			statusCode: StatusCode.BAD,
+			messages: ['Database not set up']
+		})
+	}
 }
 
 Meteor.startup(() => {
