b308d365c9babc8910008f4e6484b1ed3ba96918
Johan Nyman <johan@nytamin.se>
2018-12-20 09:52:39 +0100

    chore: improved extendMandatory, so it properly requires extend-properties when original type has optional properties

--- meteor/lib/lib.ts
+++ meteor/lib/lib.ts
@@ -817,15 +817,27 @@ export function fixValidPath (path) {
 }
 
 /**
+ * Thanks to https://github.com/Microsoft/TypeScript/issues/23126#issuecomment-395929162
+ */
+export type OptionalPropertyNames<T> = {
+	[K in keyof T]-?: undefined extends T[K] ? K : never
+}[keyof T]
+export type RequiredPropertyNames<T> = {
+	[K in keyof T]-?: undefined extends T[K] ? never : K
+}[keyof T]
+export type OptionalProperties<T> = Pick<T, OptionalPropertyNames<T>>
+export type RequiredProperties<T> = Pick<T, RequiredPropertyNames<T>>
+
+/**
  * Returns the difference between object A and B
  */
-type Difference<A, B extends A> = Pick<B, Exclude<keyof B, keyof A>>
+type Difference<A, B extends A> = Pick<B, Exclude<keyof B, keyof RequiredProperties<A>>>
 /**
  * Somewhat like _.extend, but with strong types & mandated additional properties
  * @param original Object to be extended
  * @param extendObj properties to add
  */
-export function extendMandadory<A, B extends A> (original: A, extendObj: Difference<A, B>): B {
+export function extendMandadory<A, B extends A> (original: A, extendObj: Difference<A, B> & Partial<A>): B {
 	return _.extend(original, extendObj)
 }
 
