c6e307da28cb860ca91538da743840c7f74f4e84
Johan Nyman <johan@nytamin.se>
2018-12-29 15:30:30 +0100

    feat(prompter): Add keyboard-controller, for control by keyboard-like devices

--- meteor/client/ui/Prompter/controller/manager.ts
+++ meteor/client/ui/Prompter/controller/manager.ts
@@ -4,7 +4,7 @@ import * as _ from 'underscore'
 import { PrompterViewInner } from '../PrompterView'
 import { MouseIshController } from './mouse-ish-device'
 import { ControllerAbstract } from './lib'
-import { ArrowKeysController } from './arrowkeys-device'
+import { KeyboardController } from './keyboard-device'
 
 export class PrompterControlManager {
 	private _view: PrompterViewInner
@@ -21,7 +21,7 @@ export class PrompterControlManager {
 
 		// tmp, activate some default controllers:
 		this._controllers.push(new MouseIshController(this._view))
-		this._controllers.push(new ArrowKeysController(this._view))
+		this._controllers.push(new KeyboardController(this._view))
 	}
 	destroy () {
 		window.removeEventListener('keydown', this._onKeyDown)
