f5fe2778bfe2a36c34968b9490cad77f542b7a3f
Jesper Stærkær <jesper@starkar.no>
2019-02-08 13:01:49 +0100

    types

--- meteor/server/migration/0_21_0.ts
+++ meteor/server/migration/0_21_0.ts
@@ -2,6 +2,8 @@ import { addMigrationSteps } from './databaseMigration'
 import * as _ from 'underscore'
 import { ShowStyleBases } from '../../lib/collections/ShowStyleBases'
 import { Random } from 'meteor/random'
+import { setExpectedVersion } from './lib';
+import { PeripheralDeviceAPI } from '../../lib/api/peripheralDevice';
 
 // 0.21.0
 addMigrationSteps( '0.21.0', [
