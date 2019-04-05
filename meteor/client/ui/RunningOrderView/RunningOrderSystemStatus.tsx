97311b1f74a77e8ecc5169ff97b83284efa3e25f
Jan Starzak <jan.starzak@gmail.com>
2019-04-05 10:35:07 +0200

    fix: more fixes for MOS status indicator in header

--- meteor/client/ui/RunningOrderView/RunningOrderSystemStatus.tsx
+++ meteor/client/ui/RunningOrderView/RunningOrderSystemStatus.tsx
@@ -141,8 +141,8 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 		}
 	}, PeripheralDeviceAPI.StatusCode.UNKNOWN)
 	const mosOnlineOffline: OnLineOffLineList = {
-		onLine: mosDevices.filter(i => i.connected),
-		offLine: mosDevices.filter(i => !i.connected)
+		onLine: mosDevices.filter(i => i.connected && i.status.statusCode < PeripheralDeviceAPI.StatusCode.WARNING_MINOR),
+		offLine: mosDevices.filter(i => !i.connected || i.status.statusCode >= PeripheralDeviceAPI.StatusCode.WARNING_MINOR)
 	}
 	const mosLastUpdate = _.reduce(mosDevices, (memo, item: MosDevice) => Math.max(item.lastDataReceived || 0, memo), 0)
 	const playoutStatus = _.reduce(playoutDevices, (memo: PeripheralDeviceAPI.StatusCode, item: PeripheralDevice) => {
@@ -259,6 +259,8 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 	render () {
 		const { t } = this.props
 		const playoutDevicesIssues = this.props.playoutDevices.offLine.filter(dev => dev.connected)
+		const mosDevicesIssues = this.props.mosDevices.offLine.filter(dev => dev.connected)
+		const mosDisconnected = this.props.mosDevices.offLine.filter(dev => !dev.connected)
 		const playoutDisconnected = this.props.playoutDevices.offLine.filter(dev => !dev.connected)
 
 		return (
@@ -317,14 +319,23 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 								{
 									this.props.mosDevices.offLine.length > 0 ?
 										<React.Fragment>
-											<h5>{t('Off-line devices')}</h5>
-											<ul>
-												{ this.props.mosDevices.offLine.map((dev) => {
-													return <li key={dev._id}>{dev.name}</li>
-												})}
-											</ul>
-										</React.Fragment>
-									:
+											{mosDisconnected.length ? <React.Fragment>
+												<h5>{t('Off-line devices')}</h5>
+												<ul>
+													{ mosDisconnected.map((dev) => {
+														return <li key={dev._id}>{dev.name}</li>
+													})}
+												</ul>
+											</React.Fragment> : null}
+											{mosDevicesIssues.length ? <React.Fragment>
+												<h5>{t('Devices with issues')}</h5>
+												<ul>
+													{mosDevicesIssues.map((dev) => {
+														return <li key={dev._id}>{dev.name}</li>
+													})}
+												</ul>
+											</React.Fragment> : null}
+										</React.Fragment> :
 										<span>{t('All connections working correctly')}</span>
 								}
 							</div>
@@ -343,8 +354,7 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 								{
 									this.props.playoutDevices.offLine.length > 0 ?
 										<React.Fragment>
-											{playoutDisconnected.length ?
-											<React.Fragment>
+											{playoutDisconnected.length ? <React.Fragment>
 												<h5>{t('Off-line devices')}</h5>
 												<ul>
 													{playoutDisconnected.map((dev) => {
@@ -352,8 +362,7 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 													})}
 												</ul>
 											</React.Fragment> : null}
-											{playoutDevicesIssues.length ?
-											<React.Fragment>
+											{playoutDevicesIssues.length ? <React.Fragment>
 												<h5>{t('Devices with issues')}</h5>
 												<ul>
 													{playoutDevicesIssues.map((dev) => {
@@ -361,8 +370,7 @@ export const RunningOrderSystemStatus = translateWithTracker((props: IProps) => 
 													})}
 												</ul>
 											</React.Fragment> : null}
-										</React.Fragment>
-									:
+										</React.Fragment> :
 										<span>{t('All devices working correctly')}</span>
 								}
 							</div>
