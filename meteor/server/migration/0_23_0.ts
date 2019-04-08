57b0adba87967ce32a25b50f8786dfe05e83d97f
Johan Nyman <johan@nytamin.se>
2019-04-08 08:48:42 +0200

    chore: add expected device-versions

--- meteor/server/migration/0_23_0.ts
+++ meteor/server/migration/0_23_0.ts
@@ -1,6 +1,8 @@
 import { addMigrationSteps } from './databaseMigration'
 import * as _ from 'underscore'
 import { RunningOrders } from '../../lib/collections/RunningOrders'
+import { setExpectedVersion } from './lib'
+import { PeripheralDeviceAPI } from '../../lib/api/peripheralDevice'
 
 // 0.23.0 ( Release 8 )
 addMigrationSteps( '0.23.0', [
@@ -32,7 +34,9 @@ addMigrationSteps( '0.23.0', [
 				multi: true
 			})
 		}
-	}
+	},
+	setExpectedVersion('expectedVersion.playoutDevice', PeripheralDeviceAPI.DeviceType.PLAYOUT, '_process', '0.18.0'),
+	setExpectedVersion('expectedVersion.mosDevice', PeripheralDeviceAPI.DeviceType.MOSDEVICE, '_process', '0.5.3'),
 
 	// add steps here:
 	// {
