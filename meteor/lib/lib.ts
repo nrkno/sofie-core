b7094dd0df312d3839341e3f464304a3b91d15f4
Johan Nyman <johan@nytamin.se>
2019-04-23 11:33:54 +0200

    fix: handle when error is thrown in function in makePromise

--- meteor/lib/lib.ts
+++ meteor/lib/lib.ts
@@ -733,9 +733,13 @@ export const waitForPromise: <T>(p: Promise<T>) => T = Meteor.wrapAsync (functio
 	})
 })
 export function makePromise<T> (fcn: () => T): Promise<T> {
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		Meteor.defer(() => {
-			resolve(fcn())
+			try {
+				resolve(fcn())
+			} catch (e) {
+				reject(e)
+			}
 		})
 	})
 }
