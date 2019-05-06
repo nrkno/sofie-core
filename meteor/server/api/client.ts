8f2a15933ee971cc1890b291a067123276e13ee4
Johan Nyman <johan@nytamin.se>
2019-05-06 08:54:19 +0200

    chore: log

--- meteor/server/api/client.ts
+++ meteor/server/api/client.ts
@@ -63,6 +63,7 @@ export namespace ServerClientAPI {
 		} catch (e) {
 			// console.log('eeeeeeeeeeeeeee')
 			// allow the exception to be handled by the Client code
+			logger.error(`Error in ${methodName}`)
 			let errMsg = e.message || e.reason || (e.toString ? e.toString() : null)
 			logger.error(errMsg + '\n' + (e.stack || ''))
 			UserActionsLog.update(actionId, {$set: {
