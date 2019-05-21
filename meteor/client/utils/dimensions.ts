726c9cab300b076128b41626483601179ff641f3
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2019-05-21 17:36:40 +0200

    Removed jQuery from TimelineGrid

--- meteor/client/utils/dimensions.ts
+++ meteor/client/utils/dimensions.ts
@@ -3,3 +3,26 @@ export function getElementWidth(element: HTMLElement): number {
 
 	return Number.isNaN(computedWidth) ? element.offsetWidth : computedWidth
 }
+
+/**
+ * Replicates the behavior of jQuery's height() function. Note that it's only
+ * really needed to get the height **excluding padding** for inline elements.
+ * For all other use cases there are properties on the Element itself 
+ * that can be read directly or via window.getComputedStyle.
+ * 
+ * @param element - the element to calculate height for
+ * @returns the calculated height of the element excluding padding
+ */
+export function getElementHeight(element: HTMLElement): number {
+	const { height, paddingTop, paddingBottom } = window.getComputedStyle(element)
+	const computedHeight = Number(height)
+
+	if (!Number.isNaN(computedHeight)) {
+		return computedHeight
+	}
+
+	const computedPaddingTop = paddingTop ? Number.parseInt(paddingTop, 10) : 0
+	const computedPaddingBottom = paddingBottom ? Number.parseInt(paddingBottom, 10) : 0
+
+	return element.scrollHeight - computedPaddingTop - computedPaddingBottom
+}
