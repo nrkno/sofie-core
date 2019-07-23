bf3e67cc6406774c7f8bc79e51a4bf344f34489e
Thomas Lee <thomasslee97@gmail.com>
2019-07-23 13:21:32 +0100

    feat: Add tooltip for source layers and output channels

--- meteor/client/ui/Settings/ShowStyleBaseSettings.tsx
+++ meteor/client/ui/Settings/ShowStyleBaseSettings.tsx
@@ -1,6 +1,7 @@
 import * as _ from 'underscore'
 import * as React from 'react'
 import * as ClassNames from 'classnames'
+const Tooltip = require('rc-tooltip')
 import { EditAttribute } from '../../lib/EditAttribute'
 import { Translated, translateWithTracker } from '../../lib/ReactMeteorData/react-meteor-data'
 import { Spinner } from '../../lib/Spinner'
@@ -26,6 +27,7 @@ import { ConfigManifestSettings, collectConfigs } from './ConfigManifestSettings
 import { Studios, Studio } from '../../../lib/collections/Studios'
 import { Link } from 'react-router-dom'
 import RundownLayoutEditor from './RundownLayoutEditor'
+import { getHelpMode } from '../../lib/localStorage'
 
 interface IProps {
 	match: {
@@ -788,7 +790,14 @@ const SourceLayerSettings = translate()(class SourceLayerSettings extends React.
 		const { t } = this.props
 		return (
 			<div>
-				<h2 className='mhn'>{t('Source Layers')}</h2>
+				<h2 className='mhn'>
+					<Tooltip
+						overlay={t('Add some source layers (e.g. Graphics) for your ENPS data to appear in rundowns')}
+						visible={getHelpMode() && !this.props.showStyleBase.sourceLayers.length}
+						placement='bottom'>
+						<span>{t('Source Layers')}</span>
+					</Tooltip>
+				</h2>
 				<table className='expando settings-studio-source-table'>
 					<tbody>
 						{this.renderInputSources()}
@@ -994,7 +1003,14 @@ const OutputSettings = translate()(class OutputSettings extends React.Component<
 		const { t } = this.props
 		return (
 			<div>
-				<h2 className='mhn'>{t('Output channels')}</h2>
+				<h2 className='mhn'>
+					<Tooltip
+						overlay={t('Output channels are required for your studio to work')}
+						visible={getHelpMode() && !this.props.showStyleBase.outputLayers.length}
+						placement='top'>
+						<span>{t('Output channels')}</span>
+					</Tooltip>
+				</h2>
 				<table className='expando settings-studio-output-table'>
 					<tbody>
 						{this.renderOutputs()}
