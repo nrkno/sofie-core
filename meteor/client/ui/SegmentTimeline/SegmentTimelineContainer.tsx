f512f702bcf27caf21e70cb8c8ec15caaa12500c
Jan Starzak <jan.starzak@gmail.com>
2019-08-22 18:55:17 +0200

    fix: onAir detach issue when the component is destroyed

--- meteor/client/ui/SegmentTimeline/SegmentTimelineContainer.tsx
+++ meteor/client/ui/SegmentTimeline/SegmentTimelineContainer.tsx
@@ -269,10 +269,7 @@ export const SegmentTimelineContainer = withTracker<IProps, IState, ITrackedProp
 	componentWillUnmount () {
 		this._cleanUp()
 		if (this.intersectionObserver && this.props.isLiveSegment && this.props.followLiveSegments) {
-			const entries = this.intersectionObserver.takeRecords()
-			if (entries[0] && entries[0].intersectionRatio < 0.99) {
-				if (typeof this.props.onSegmentScroll === 'function') this.props.onSegmentScroll()
-			}
+			if (typeof this.props.onSegmentScroll === 'function') this.props.onSegmentScroll()
 		}
 		this.stopLive()
 		window.removeEventListener(RundownViewEvents.rewindsegments, this.onRewindSegment)
@@ -340,7 +337,7 @@ export const SegmentTimelineContainer = withTracker<IProps, IState, ITrackedProp
 		// console.log("visibleChanged")
 		if ((entries[0].intersectionRatio < 0.99) && !isMaintainingFocus() && (Date.now() - this.mountedTime > 2000)) {
 			if (typeof this.props.onSegmentScroll === 'function') this.props.onSegmentScroll()
-			// console.log("onSegmentScroll", entries[0].intersectionRatio, isMaintainingFocus())
+			console.log("onSegmentScroll", entries[0].intersectionRatio, isMaintainingFocus())
 			this.isVisible = false
 		} else {
 			this.isVisible = true
