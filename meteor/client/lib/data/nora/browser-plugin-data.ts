92d6b2a7eadbe3b9176e967f1a786a1aba557217
Ola Christian Gundelsby <Ola.Christian.Gundelsby@nrk.no>
2019-10-08 15:27:00 +0200

    Utility to create XML for Nora plugin based on content payload from AdLibPiece

--- meteor/client/lib/data/nora/browser-plugin-data.ts
+++ meteor/client/lib/data/nora/browser-plugin-data.ts
@@ -2,7 +2,7 @@ import { NoraPayload, NoraContent } from 'tv-automation-sofie-blueprints-integra
 import { PieceGeneric } from '../../../../lib/collections/Pieces'
 import { generateMosPluginItemXml } from '../../parsers/mos/mosXml2Js'
 
-export { createMosObjectXmlStringNoraBluePrintPiece }
+export { createMosObjectXmlStringNoraBluePrintPiece, createMosObjectXmlStringFromPayload }
 
 function createMosObjectXmlStringNoraBluePrintPiece(piece: PieceGeneric): string {
 	if (!piece.content || !piece.content.payload) {
@@ -11,4 +11,68 @@ function createMosObjectXmlStringNoraBluePrintPiece(piece: PieceGeneric): string
 	const noraContent = piece.content as NoraContent
 
 	return generateMosPluginItemXml(noraContent.externalPayload)
+}
+
+function createMosObjectXmlStringFromPayload(payload: NoraPayload): string {
+	const doc = objectToXML({
+		ncsItem: {
+			item: {
+				itemSlug: null,
+				mosExternalMetadata: {
+					mosSchema: 'http://nora.core.mesosint.nrk.no/mos/content',
+					mosPayload: {
+						metadata: {
+							selection: {
+								design: {
+									id: payload.manifest
+								},
+								type: {
+									id: payload.template.layer
+								},
+								mal: {
+									id: payload.template.name
+								}
+							},
+							type: payload.template.layer,
+							userContext: {}
+						},
+						content: {
+							navn: payload.content.navn,
+							tittel: payload.content.tittel,
+							_valid: payload.content._valid
+						}
+					}
+				}
+			},
+		}
+	}, 'mos'
+	)
+
+	return new XMLSerializer().serializeToString(doc)
+}
+
+function objectToXML(obj: object, rootName: string): Document {
+	const doc = new Document()
+	const root = doc.createElement(rootName)
+
+	addNodes(obj, root)
+
+	doc.appendChild(root)
+	return doc
+}
+
+function addNodes(obj: object, rootNode: Node): void {
+	const doc = rootNode.ownerDocument
+
+	for (const name of Object.keys(obj)) {
+		const node = doc.createElement(name)
+		rootNode.appendChild(node)
+
+		const value = obj[name]
+		if (typeof value === 'object' && value !== null) {
+			addNodes(value, node)
+		} else {
+			node.textContent = value
+		}
+	}
 }
