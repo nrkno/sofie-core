72b8136c1b1d4acf54ffe143a99fed55b90b7f41
Johan Nyman <johan@nytamin.se>
2019-10-14 08:17:47 +0200

    chore: test: fix snapshot data

--- meteor/__mocks__/helpers/snapshot.ts
+++ meteor/__mocks__/helpers/snapshot.ts
@@ -2,10 +2,13 @@ import * as _ from 'underscore'
 import { clone } from '../../lib/lib'
 import { TimelineObjGeneric } from '../../lib/collections/Timeline'
 import { DBRundown } from '../../lib/collections/Rundowns'
+import { DBSegment } from '../../lib/collections/Segments'
+import { Part } from '../../lib/collections/Parts'
+import { Piece } from '../../lib/collections/Pieces'
 
 // About snapshot testing: https://jestjs.io/docs/en/snapshot-testing
 
-type Data = TimelineObjGeneric | DBRundown
+type Data = undefined | TimelineObjGeneric | DBRundown | DBSegment | Part | Piece
 /**
  * Remove certain fields from data that change often, so that it can be used in snapshots
  * @param data
@@ -17,6 +20,7 @@ export function fixSnapshot (
 		return _.map(data, fixSnapshot)
 	} else {
 		let o = clone(data)
+		if (!o) return o
 		if (isTimelineObj(o)) {
 			delete o['modified']
 			if (o.content) {
@@ -27,6 +31,9 @@ export function fixSnapshot (
 		} else if (isRundown(o)) {
 			delete o['created']
 			delete o['modified']
+		// } else if (isPiece(o)) {
+		// } else if (isPart(o)) {
+		// } else if (isSegment(o)) {
 		}
 		return o
 	}
@@ -36,4 +43,29 @@ function isTimelineObj (o): o is TimelineObjGeneric {
 }
 function isRundown (o): o is DBRundown {
 	return o._id && _.has(o, 'currentPartId')
+}
+function isSegment (o): o is DBSegment {
+	return (
+		o._id &&
+		_.has(o, 'rundownId') &&
+		_.has(o, 'externalId') &&
+		_.has(o, 'name')
+	)
+}
+function isPart (o): o is Part {
+	return (
+		o._id &&
+		_.has(o, 'rundownId') &&
+		_.has(o, 'externalId') &&
+		_.has(o, 'segmentId') &&
+		_.has(o, 'title')
+	)
+}
+function isPiece (o): o is Piece {
+	return (
+		o._id &&
+		_.has(o, 'rundownId') &&
+		_.has(o, 'externalId') &&
+		_.has(o, 'partId')
+	)
 }
