34a693ff804407d6f5f7b147030b2df6824e7ffd
Johan Nyman <johan@nytamin.se>
2019-10-23 08:16:06 +0200

    fix: make a check of user input before setting system.storePath.
    Also added a migration step for fix that previous issue in database.
    
    This is a possible fix for the issue in PR #91 where there was an issue the CoreSystem.storePath had been set to 'undefined'

--- meteor/server/migration/X_X_X.ts
+++ meteor/server/migration/X_X_X.ts
@@ -1,5 +1,6 @@
 import { addMigrationSteps } from './databaseMigration'
 import { ensureCollectionProperty } from './lib'
+import { getCoreSystem, setCoreSystemStorePath } from '../../lib/collections/CoreSystem'
 
 /*
  * **************************************************************************************
@@ -23,4 +24,22 @@ addMigrationSteps('1.2.0', [
 	// 		//
 	// 	}
 	// },
+	{
+		id: 'CoreSystem.storePath fix',
+		// Fix a bug where CoreSystemPath is the string "undefined", ref (https://github.com/nrkno/tv-automation-server-core/pull/91)
+		canBeRunAutomatically: true,
+		validate: () => {
+			let system = getCoreSystem()
+			if (system && system.storePath === 'undefined') {
+				return 'CoreSystem.storePath is "undefined"'
+			}
+			return false
+		},
+		migrate: () => {
+			let system = getCoreSystem()
+			if (system && system.storePath === 'undefined') {
+				setCoreSystemStorePath(undefined)
+			}
+		}
+	},
 ])
