f0ceb67c7b22ab2cb7c5f92fdfee0cb6b6c8dba9
Johan Nyman <johan@nytamin.se>
2019-10-25 16:05:07 +0200

    chore: system version, release 14
    (cherry picked from commit c65344a6c025be4ee2e358c1a753eb87a039eabc)

--- meteor/server/migration/1_2_0.ts
+++ meteor/server/migration/1_2_0.ts
@@ -2,28 +2,8 @@ import { addMigrationSteps } from './databaseMigration'
 import { ensureCollectionProperty } from './lib'
 import { getCoreSystem, setCoreSystemStorePath } from '../../lib/collections/CoreSystem'
 
-/*
- * **************************************************************************************
- *
- *  These migrations are destined for the next release
- *
- * (This file is to be renamed to the correct version number when finalizing the release)
- *
- * **************************************************************************************
-*/
-// x.x.x (Release X)
+// 1.2.0 (Release 14)
 addMigrationSteps('1.2.0', [
-	// add steps here:
-	// {
-	// 	id: 'my fancy step',
-	// 	canBeRunAutomatically: true,
-	// 	validate: () => {
-	// 		return false
-	// 	},
-	// 	migrate: () => {
-	// 		//
-	// 	}
-	// },
 	{
 		id: 'CoreSystem.storePath fix',
 		// Fix a bug where CoreSystemPath is the string "undefined", ref (https://github.com/nrkno/tv-automation-server-core/pull/91)
