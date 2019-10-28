5722aaeb7ed38526d9453c194d182d919e1fb622
Johan Nyman <johan@nytamin.se>
2019-10-28 11:22:50 +0100

    feat: expectedPlayoutItems: implement fetching of items from pieces, using temporary typings

--- meteor/server/api/ingest/rundownInput.ts
+++ meteor/server/api/ingest/rundownInput.ts
@@ -31,7 +31,7 @@ import { PeripheralDeviceSecurity } from '../../security/peripheralDevices'
 import { IngestRundown, IngestSegment, IngestPart, BlueprintResultSegment } from 'tv-automation-sofie-blueprints-integration'
 import { logger } from '../../../lib/logging'
 import { Studio } from '../../../lib/collections/Studios'
-import { selectShowStyleVariant, afterRemoveSegments, afterRemoveParts, ServerRundownAPI, removeSegments, updatePartRanks, afterRemovePieces } from '../rundown'
+import { selectShowStyleVariant, afterRemoveSegments, afterRemoveParts, ServerRundownAPI, removeSegments, updatePartRanks } from '../rundown'
 import { loadShowStyleBlueprints, getBlueprintOfRundown } from '../blueprints/cache'
 import { ShowStyleContext, RundownContext, SegmentContext } from '../blueprints/context'
 import { Blueprints, Blueprint } from '../../../lib/collections/Blueprints'
@@ -50,6 +50,8 @@ import { PartNote, NoteType } from '../../../lib/api/notes'
 import { syncFunction } from '../../codeControl'
 import { updateSourceLayerInfinitesAfterPart } from '../playout/infinites'
 import { UpdateNext } from './updateNext'
+import { extractExpectedPlayoutItems } from './expectedPlayoutItems'
+import { ExpectedPlayoutItem, ExpectedPlayoutItems } from '../../../lib/collections/ExpectedPlayoutItems'
 
 export enum RundownSyncFunctionPriority {
 	Ingest = 0,
@@ -351,6 +353,11 @@ function updateRundownFromIngestData (
 		adlibPieces.push(...segmentContents.adlibPieces)
 	})
 
+	const expectedPlayoutItems: ExpectedPlayoutItem[] = extractExpectedPlayoutItems(dbRundown, [
+		...segmentPieces,
+		...adlibPieces
+	])
+
 	changes = sumChanges(
 		changes,
 		// Save the baseline
@@ -410,9 +417,6 @@ function updateRundownFromIngestData (
 			},
 			afterRemove (piece) {
 				logger.debug('deleted piece ' + piece._id)
-			},
-			afterRemoveAll (pieces) {
-				afterRemovePieces(rundownId, pieces)
 			}
 		}),
 
@@ -428,12 +432,13 @@ function updateRundownFromIngestData (
 			},
 			afterRemove (adLibPiece) {
 				logger.debug('deleted piece ' + adLibPiece._id)
-			},
-			afterRemoveAll (pieces) {
-				afterRemovePieces(rundownId, pieces)
 			}
-		})
+		}),
+		saveIntoDb<ExpectedPlayoutItem, ExpectedPlayoutItem>(ExpectedPlayoutItems, {
+			rundownId: rundownId,
+		}, expectedPlayoutItems)
 	)
+
 	const didChange = anythingChanged(changes)
 	if (didChange) {
 		afterIngestChangedData(dbRundown, _.map(segments, s => s._id))
