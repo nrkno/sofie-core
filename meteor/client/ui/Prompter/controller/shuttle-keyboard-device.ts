e6e28d927e46277ee4c3fbfdcb8bcbdd19ccb53f
Jesper Stærkær <jesper@starkar.no>
2019-10-30 12:16:41 +0100

    chore: debug prod

--- meteor/client/ui/Prompter/controller/shuttle-keyboard-device.ts
+++ meteor/client/ui/Prompter/controller/shuttle-keyboard-device.ts
@@ -171,12 +171,13 @@ export class ShuttleKeyboardController extends ControllerAbstract {
 		let factor = 1
 		// calculate the length of the frame and how it should affect the amount scrolled
 		if (time && this._lastTick) {
-			factor = (time - this._lastTick) / 16.66 // 1000 ms / 60 f = 16.66 ms/f
+			factor = (time - this._lastTick) / (1000 / 16) //16.66 -> 1000 ms / 60 f = 16.66 ms/f
 		}
 		this._lastTick = time
 		
 		// update scroll position
-		window.scrollBy(0, this._lastSpeed)
+		console.log(`factor: ${factor}, actual speed: ${this._lastSpeed * factor}`)
+		window.scrollBy(0, this._lastSpeed * factor)
 
 		// check for reached end-of-scroll:
 		const scrollPosition = window.scrollY
