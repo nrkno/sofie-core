b72a94a227b9e09b4ce16fc79e443619ae0a65f2
Jan Starzak <jan.starzak@gmail.com>
2019-11-25 17:52:02 +0100

    fix(invalidTiming): do not set currentRemaining to 0 when handling an invalid part

--- meteor/client/ui/RundownView/RundownTiming.tsx
+++ meteor/client/ui/RundownView/RundownTiming.tsx
@@ -347,7 +347,6 @@ withTracker<IRundownTimingProviderProps, IRundownTimingProviderState, IRundownTi
 
 				// Handle invalid parts by overriding the values to preset values for Invalid parts
 				if (part.invalid) {
-					currentRemaining = 0
 					partDisplayDuration = this.props.defaultDuration || DEFAULT_DURATION
 					this.partPlayed[part._id] = 0
 				}
