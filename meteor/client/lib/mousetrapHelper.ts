9098936cf00a61235884daa1ce1b73145cbfd46e
Jan Starzak <jan.starzak@gmail.com>
2020-01-06 15:55:55 +0100

    chore: fix ANstudio to ANSI in mousetrapHelper

--- meteor/client/lib/mousetrapHelper.ts
+++ meteor/client/lib/mousetrapHelper.ts
@@ -124,9 +124,9 @@ export namespace mousetrapHelper {
 
 // Add mousetrap keycodes for special keys
 mousetrap.addKeycodes({
-	220: '§', // on US-based (ANstudio) keyboards (single-row, Enter key), this is the key above Enter, usually with a backslash and the vertical pipe character
-	222: '\\', // on ANstudio-based keyboards, this is the key with single quote
-	223: '|', // this key is not present on ANstudio-based keyboards
+	220: '§', // on US-based (ANSI) keyboards (single-row, Enter key), this is the key above Enter, usually with a backslash and the vertical pipe character
+	222: '\\', // on ANSI-based keyboards, this is the key with single quote
+	223: '|', // this key is not present on ANSI-based keyboards
 
 	96: 'num0',
 	97: 'num1',
