2e69fe8d88e54fb2b901789f255143b98c1aceb8
Jan Starzak <jan.starzak@gmail.com>
2020-01-17 01:25:03 +0100

    fix: update mediaObject subscriptions for AdLibListItems and DashboardPieceButtons

--- meteor/client/ui/Shelf/AdLibListItem.tsx
+++ meteor/client/ui/Shelf/AdLibListItem.tsx
@@ -54,6 +54,18 @@ export const AdLibListItem = translateWithTracker<IListViewItemProps, {}, IAdLib
 		super(props)
 	}
 
+	componentDidMount () {
+		Meteor.defer(() => {
+			this.updateMediaObjectSubscription()
+		})
+	}
+
+	componentDidUpdate () {
+		Meteor.defer(() => {
+			this.updateMediaObjectSubscription()
+		})
+	}
+
 	updateMediaObjectSubscription () {
 		if (this.props.item && this.props.layer) {
 			const piece = this.props.item as any as AdLibPieceUi
