5c345fae28aaad783e46357f9e36c401d2f3432a
Tom Lee <thomasslee97@gmail.com>
2020-01-28 15:02:03 +0000

    Revert "test: Recalculate infinites"
    This reverts commit 4bc3dae56298be507a124132ab308baf68af1d83.

--- meteor/server/api/playout/adlib.ts
+++ meteor/server/api/playout/adlib.ts
@@ -213,7 +213,6 @@ export namespace ServerPlayoutAdLibAPI {
 
 			ServerPlayoutAPI.setNextPartInner(rundown, partId)
 		} else {
-			updateSourceLayerInfinitesAfterPart(rundown)
 			cropInfinitesOnLayer(rundown, part, newPiece)
 			stopInfinitesRunningOnLayer(rundown, part, newPiece.sourceLayerId)
 			updateTimeline(rundown.studioId)
