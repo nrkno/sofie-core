1e2b91ead5ce90576ac9288e21deaebbe4f40406
Jan Starzak <jan.starzak@gmail.com>
2020-02-07 12:23:24 +0100

    fix: change a secondary filter to an extended query

--- meteor/client/ui/Shelf/DashboardPanel.tsx
+++ meteor/client/ui/Shelf/DashboardPanel.tsx
@@ -424,8 +424,20 @@ export function getUnfinishedPiecesReactive(rundownId: string, currentPartId: st
 			},
 			adLibSourceId: {
 				$exists: true
-			}
-		}).fetch().filter(p => !(p.userDuration && p.userDuration.duration))
+			},
+			$or: [
+				{
+					userDuration: {
+						$exists: false
+					}
+				},
+				{
+					'userDuration.duration': {
+						$exists: false
+					}
+				}
+			]
+		}).fetch()
 
 		let nearestEnd = Number.POSITIVE_INFINITY
 		prospectivePieces = prospectivePieces.filter((piece) => {
