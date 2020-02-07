7ad9b71fd34633b9db8191d67a3873b2b5baad91
Tom Lee <thomasslee97@gmail.com>
2020-02-07 12:04:05 +0000

    fix: Get only latest instance of infinite in part

--- meteor/server/api/playout/pieces.ts
+++ meteor/server/api/playout/pieces.ts
@@ -168,7 +168,9 @@ export function createPieceGroup (
 }
 export function getResolvedPieces (part: Part): Piece[] {
 	// TODO - was this mangled for endState and could it have broken something else?
-	const pieces = part.getAllPieces()
+	let pieces = allPieces ? allPieces.filter(p => p.partId === part._id) : part.getAllPieces()
+
+	pieces = pieces.filter(p => !p.stoppedPlayback)
 
 	const itemMap: { [key: string]: Piece } = {}
 	pieces.forEach(piece => itemMap[piece._id] = piece)
