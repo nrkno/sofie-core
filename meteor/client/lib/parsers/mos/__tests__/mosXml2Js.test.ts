094989028d8e105022df37712ec28e5623b84b00
Julian Waller <git@julusian.co.uk>
2020-02-12 15:52:55 +0100

    feat: generate and parse mos-plugin xml via mos-connection

--- meteor/client/lib/parsers/mos/__tests__/mosXml2Js.test.ts
+++ meteor/client/lib/parsers/mos/__tests__/mosXml2Js.test.ts
@@ -1,4 +1,4 @@
-import {mosXml2Js} from '../mosXml2Js'
+import {parseMosPluginItemXml, fixMosData, generateMosPluginItemXml} from '../mosXml2Js'
 import {readFileSync} from 'fs'
 import {join} from 'path'
 import * as parser from 'xml2json'
@@ -8,46 +8,45 @@ const domParser = new DOMParser()
 
 describe('MOS XML to JavaScript object parser', () => {
 	describe('mosXml2Js', () => {
-		it('should return an object', () => {
-			const doc = domParser.parseFromString('<root />', 'text/xml')
+		// it('should return an object', () => {
+		// 	const doc = domParser.parseFromString('<root />', 'text/xml')
 
-			const actual = mosXml2Js(doc)
+		// 	const actual = mosXml2Js(doc)
 
-			expect(actual).toBeInstanceOf(Object)
-		})
+		// 	expect(actual).toBeInstanceOf(Object)
+		// })
 		
-		describe('ReqAppInfo', () => {
-			const doc = domParser.parseFromString(mosReqAppInfoXmlString, 'text/xml')
+		// describe('ReqAppInfo', () => {
+		// 	const doc = domParser.parseFromString(mosReqAppInfoXmlString, 'text/xml')
 
-			it('should contain an empty ncsReqAppInfo node', () => {
-				const expected = {mos: {ncsReqAppInfo: {}}}
+		// 	it('should contain an empty ncsReqAppInfo node', () => {
+		// 		const expected = {mos: {ncsReqAppInfo: {}}}
 	
-				const actual = mosXml2Js(doc)
+		// 		const actual = mosXml2Js(doc)
 
-				expect(actual).toEqual(expected)
-			}) 
-		})
+		// 		expect(actual).toEqual(expected)
+		// 	}) 
+		// })
 		
 		describe('Sample1', () => {
 			const sample1XmlStr = readFileSync(join(__dirname, './mosSample1.xml'), 'utf-8')
 			const sample1JsonStr = readFileSync(join(__dirname, './mosSample1.json'), 'utf-8')
 
-			const xmlDoc = domParser.parseFromString(sample1XmlStr, 'text/xml')
 			const jsonDoc = JSON.parse(sample1JsonStr)
 
 			it('should match the json representation', () => {
-				const actual = mosXml2Js(xmlDoc) as any;
-				const actualItem = actual.mos.ncsItem.item
+				const actual = parseMosPluginItemXml(sample1XmlStr);
+				const actualJson = fixMosData(actual) // Strip out any MosString etc
 
-				expect(actualItem).toEqual(jsonDoc)
+				expect(actualJson).toEqual(jsonDoc)
 			})
 
 			it('converting via xml should be lossless', () => {
-				const generatedXml = new XMLSerializer().serializeToString(jsonDoc)
-				const generatedDoc = domParser.parseFromString(generatedXml, 'text/xml')
-				const actual = mosXml2Js(generatedDoc) as any;
+				const generatedXml = generateMosPluginItemXml(jsonDoc)
+				const actual = parseMosPluginItemXml(generatedXml);
+				const actualJson = fixMosData(actual) // Strip out any MosString etc
 
-				expect(actual).toEqual(jsonDoc)
+				expect(actualJson).toEqual(jsonDoc)
 			})
 		})
 	})
