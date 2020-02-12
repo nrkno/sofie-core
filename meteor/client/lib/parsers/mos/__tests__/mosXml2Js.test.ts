96d79aad4e28a2f79fca566a1d21902491eef3ed
Julian Waller <git@julusian.co.uk>
2020-02-12 15:52:55 +0100

    feat: generate and parse mos-plugin xml via mos-connection

--- meteor/client/lib/parsers/mos/__tests__/mosXml2Js.test.ts
+++ meteor/client/lib/parsers/mos/__tests__/mosXml2Js.test.ts
@@ -94,28 +94,5 @@ describe('MOS XML to JavaScript object parser', () => {
 				expect(actualJson).toEqual(jsonDoc)
 			})
 		})
-		
-		describe('Sample1', () => {
-			const sample1XmlStr = readFileSync(join(__dirname, './mosSample1.xml'), 'utf-8')
-			const sample1JsonStr = readFileSync(join(__dirname, './mosSample1.json'), 'utf-8')
-
-			const xmlDoc = domParser.parseFromString(sample1XmlStr, 'text/xml')
-			const jsonDoc = JSON.parse(sample1JsonStr)
-
-			it('should match the json representation', () => {
-				const actual = mosXml2Js(xmlDoc) as any;
-				const actualItem = actual.mos.ncsItem.item
-
-				expect(actualItem).toEqual(jsonDoc)
-			})
-
-			it('converting via xml should be lossless', () => {
-				const generatedXml = new XMLSerializer().serializeToString(jsonDoc)
-				const generatedDoc = domParser.parseFromString(generatedXml, 'text/xml')
-				const actual = mosXml2Js(generatedDoc) as any;
-
-				expect(actual).toEqual(jsonDoc)
-			})
-		})
 	})
 })
