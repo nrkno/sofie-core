1bd3a6b2160544c3fdb81d882fc00d00a49fcc5f
Julian Waller <git@julusian.co.uk>
2020-02-13 13:39:05 +0100

    fix: set Part.prerollDuration when queueing adlib

--- meteor/server/api/playout/adlib.ts
+++ meteor/server/api/playout/adlib.ts
@@ -206,7 +206,8 @@ export namespace ServerPlayoutAdLibAPI {
 			title: adLibPiece.name,
 			dynamicallyInserted: true,
 			afterPart: part.afterPart || part._id,
-			typeVariant: 'adlib'
+			typeVariant: 'adlib',
+			prerollDuration: adLibPiece.adlibPreroll
 		})
 
 		updatePartRanks(rundown._id) // place in order
