21eb2633323d7a197a2b3f53cc7a9423dd0f3f3d
Jan Starzak <jan.starzak@gmail.com>
2020-02-18 10:52:46 +0100

    feat: add more bucket management APIs

--- meteor/lib/api/buckets.ts
+++ meteor/lib/api/buckets.ts
@@ -1,6 +1,9 @@
 export namespace BucketsAPI {
 	export enum methods {
 		'createBucket' = 'buckets.createBucket',
-		'removeBucket' = 'buckets.removeBucket'
+		'removeBucket' = 'buckets.removeBucket',
+		'emptyBucket'  = 'buckets.emptyBucket',
+		'modifyBucket' = 'buckets.modifyBucket',
+		'removeBucketAdLib' = 'buckets.removeBucketAdLib'
 	}
 }
--- meteor/server/api/buckets.ts
+++ meteor/server/api/buckets.ts
@@ -1,3 +1,4 @@
+import * as _ from 'underscore'
 import { Random } from 'meteor/random'
 import { Meteor } from 'meteor/meteor'
 import { check } from 'meteor/check'
@@ -8,7 +9,29 @@ import { literal } from '../../lib/lib'
 import { ClientAPI } from '../../lib/api/client'
 import { BucketSecurity } from '../security/buckets'
 import { BucketAdLibs } from '../../lib/collections/BucketAdlibs'
-import { ExpectedMediaItems } from '../../lib/collections/ExpectedMediaItems';
+import { ExpectedMediaItems } from '../../lib/collections/ExpectedMediaItems'
+
+function removeBucketAdLib(id: string) {
+	BucketAdLibs.remove({
+		_id: id
+	})
+	ExpectedMediaItems.remove({
+		bucketAdLibPieceId: id
+	})
+}
+
+function modifyBucket(id: string, bucket: Bucket) {
+	Buckets.update(id, _.omit(bucket, [ '_id' ]))
+}
+
+function emptyBucket(id: string) {
+	BucketAdLibs.remove({
+		bucketId: id
+	})
+	ExpectedMediaItems.remove({
+		bucketId: id
+	})
+}
 
 function createNewBucket(name: string) {
 	const newBucket = literal<Bucket>({
@@ -32,6 +55,22 @@ function removeBucket(id: string) {
 }
 
 let methods: Methods = {}
+methods[BucketsAPI.methods.modifyBucket] = function (id: string, bucket: Bucket) {
+	check(id, String)
+	check(bucket, Object)
+
+	return ClientAPI.responseSuccess(modifyBucket(id, bucket))
+}
+methods[BucketsAPI.methods.removeBucketAdLib] = function (id: string) {
+	check(id, String)
+
+	return ClientAPI.responseSuccess(removeBucketAdLib(id))
+}
+methods[BucketsAPI.methods.emptyBucket] = function (id: string) {
+	check(id, String)
+
+	return ClientAPI.responseSuccess(emptyBucket(id))
+}
 methods[BucketsAPI.methods.createBucket] = function (name: string) {
 	check(name, String)
 
