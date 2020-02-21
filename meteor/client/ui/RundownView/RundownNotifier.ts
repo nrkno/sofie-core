4a74e1b6aab8b582a3f19559421703c6fce50628
Tom Lee <thomasslee97@gmail.com>
2020-02-21 10:45:19 +0000

    fix: Rundown notifier crash when removing segments

--- meteor/client/ui/RundownView/RundownNotifier.ts
+++ meteor/client/ui/RundownView/RundownNotifier.ts
@@ -312,7 +312,7 @@ class RundownViewNotifier extends WithManagedTracker {
 			Parts.find({
 				rundownId: rRundownId,
 				segmentId: { $in: segments.map(segment => segment._id) }
-			}, { sort: { _rank: 1 }}).map(part => part.notes && segmentNotes[part.segmentId].notes.concat(part.notes))
+			}, { sort: { _rank: 1 }}).map(part => part.notes && segmentNotes[part.segmentId] && segmentNotes[part.segmentId].notes.concat(part.notes))
 			notes = notes.concat(_.flatten(_.map(_.values(segmentNotes), (o) => {
 				return o.notes.map(note => _.extend(note, {
 					rank: o.rank
