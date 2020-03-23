dd4bc51574398aae74d549bf0af7db44b2932157
ianshade <zegzulakrzysztof@gmail.com>
2020-03-23 23:15:52 +0100

    fix: Check rundown and current part presence

--- meteor/client/ui/Shelf/MultiViewPanel.tsx
+++ meteor/client/ui/Shelf/MultiViewPanel.tsx
@@ -82,7 +82,7 @@ export class MultiViewPanelInner extends MeteorReactComponent<Translated<IAdLibP
 			return
 		}
 
-		if (!this.isAdLibOnAir(piece)) {
+		if (!this.isAdLibOnAir(piece) && this.props.rundown && this.props.rundown.currentPartId) {
 			if (!piece.isGlobal) {
 				doUserAction(t, e, UserActionAPI.methods.segmentAdLibPieceStart, [
 					this.props.rundown._id, this.props.rundown.currentPartId, piece._id, true
