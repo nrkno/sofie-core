194af61846f2bd99af470edaa5a16bde2488172e
Richard Cartwright <spark@streampunk.media>
2020-03-30 15:34:57 +0100

    feat: merge in placeholder property display ready for testing

--- meteor/server/migration/X_X_X.ts
+++ meteor/server/migration/X_X_X.ts
@@ -12,14 +12,14 @@ import { addMigrationSteps, CURRENT_SYSTEM_VERSION } from './databaseMigration'
 // x.x.x (Release X)
 addMigrationSteps(CURRENT_SYSTEM_VERSION, [ // <--- To be set to an absolute version number when doing the release
 	// add steps here:
-	// {
-	// 	id: 'my fancy step',
-	// 	canBeRunAutomatically: true,
-	// 	validate: () => {
-	// 		return false
-	// 	},
-	// 	migrate: () => {
-	// 		//
-	// 	}
-	// },
+	{
+		id: 'no media scanner',
+		canBeRunAutomatically: true,
+		validate: () => {
+			return false
+		},
+		migrate: () => {
+			//
+		}
+	},
 ])
