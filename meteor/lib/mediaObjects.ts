2433fae359ad47bc98f04aebea604b365765c825
ianshade <zegzulakrzysztof@gmail.com>
2020-03-31 17:23:24 +0200

    feat: Display online/offline status of jingles

--- meteor/lib/mediaObjects.ts
+++ meteor/lib/mediaObjects.ts
@@ -101,6 +101,7 @@ export function getMediaObjectMediaId (piece: IBlueprintPieceGeneric, sourceLaye
 	switch (sourceLayer.type) {
 		case SourceLayerType.VT:
 		case SourceLayerType.LIVE_SPEAK:
+		case SourceLayerType.TRANSITION:
 			if (piece.content && piece.content.fileName) {
 				return (piece.content as VTContent).fileName.toUpperCase()
 			}
@@ -121,6 +122,7 @@ export function checkPieceContentStatus (piece: IBlueprintPieceGeneric, sourceLa
 		switch (sourceLayer.type) {
 			case SourceLayerType.VT:
 			case SourceLayerType.LIVE_SPEAK:
+			case SourceLayerType.TRANSITION:
 				const fileName = getMediaObjectMediaId(piece, sourceLayer)
 				const displayName = piece.name
 				const messages: Array<string> = []
