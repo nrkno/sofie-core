f9cc4cbac42f74b2caad30b873741c4bf3daa5e0
Jan Starzak <jan.starzak@gmail.com>
2020-04-02 16:08:28 +0200

    feat: use ResizeObserver instead of element-resize-event (#210)
    * feat: (experiment): use ResizeObserver instead of element-resize-event
    
    * chore: lint

---
+++ meteor/client/lib/resizeObserver.ts
@@ -0,0 +1,31 @@
+declare global {
+	interface IObserveOptions {
+		box: 'content-box' | 'border-box'
+	}
+
+	interface ResizeObserverEntry {
+		contentBoxSize?: DOMRectReadOnly
+		borderBoxSize?: DOMRectReadOnly
+		target?: HTMLElement
+	}
+
+	class ResizeObserver {
+		constructor(clb: (entries: ResizeObserverEntry[], observer: ResizeObserver) => void)
+		/** Make the observer observe changes to the size of the element */
+		observe(target: HTMLElement, options?: IObserveOptions): void
+		/** Remove all observed elements */
+		disconnect(): void
+		/** Remove a single element from the observed collection */
+		unobserve(target: HTMLElement): void
+	}
+}
+
+export function onElementResize(el: HTMLElement, clb: (entries: ResizeObserverEntry[], observer: ResizeObserver) => void) {
+	const resizeObserver = new ResizeObserver(clb)
+	resizeObserver.observe(el)
+	return resizeObserver
+}
+
+export function offElementResize(resizeObserver: ResizeObserver, el: HTMLElement) {
+	resizeObserver.unobserve(el)
+}
