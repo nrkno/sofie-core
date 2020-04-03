teowoz/tv-automation-server-core

feat: validate config properties in system health

meteor/server/api/rundown.ts
@@ -345,8 +345,8 @@ export namespace ClientRundownAPI {
 		if (!showStyleBlueprint) throw new Meteor.Error(404, `ShowStyle blueprint "${showStyle.blueprintId}" not found!`)
 
 		return {
-			studio: findMissingConfigs(studioBlueprint.studioConfigManifest || [], studio.config),
-			showStyle: findMissingConfigs(showStyleBlueprint.showStyleConfigManifest || [], showStyle.config)
+			studio: findMissingConfigs(studioBlueprint.studioConfigManifest, studio.config),
+			showStyle: findMissingConfigs(showStyleBlueprint.showStyleConfigManifest, showStyle.config)
 		}
 	}
 }

nrkno/sofie-core

meteor/server/api/rundown.ts
@@ -529,13 +529,13 @@ export namespace ClientRundownAPI {
 					id: id,
 					name: compound.name,
 					checkFailed: false,
-					fields: findMissingConfigs(blueprint.showStyleConfigManifest || [], compound.config)
+					fields: findMissingConfigs(blueprint.showStyleConfigManifest, compound.config)
 				}
 			}
 		})
 
 		return {
-			studio: findMissingConfigs(studioBlueprint.studioConfigManifest || [], studio.config),
+			studio: findMissingConfigs(studioBlueprint.studioConfigManifest, studio.config),
 			showStyles: showStyleWarnings
 		}
 	}