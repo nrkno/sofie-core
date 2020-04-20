f0491facecc84c581c13ebd5c0d8807df17fcf79
Julian Waller <git@julusian.co.uk>
2020-04-20 15:56:38 +0100

    fix: another typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Read in settings file if it exists, and variable has not already been set
 SETTINGS_FILE=/opt/core-settings.json
 if [ -z ${METEOR_SETTINGS+x} ] && [ -f "$SETTINGS_FILE" ]; then
-	export METEOR_SETTINGS=$(cat $SETTINGS_FILE)
+	export METEOR_SETTINGS="$(cat $SETTINGS_FILE)"
 fi
 
 # Start meteor
