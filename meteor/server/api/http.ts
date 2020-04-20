26091e53fe0cff56b96491ddc25d7559c12fa085
Johan Nyman <johan@nytamin.se>
2020-04-20 09:22:05 +0200

    fix: increase upload limit, to allow for large rundown-playlists, etc

--- meteor/server/api/http.ts
+++ meteor/server/api/http.ts
@@ -6,15 +6,15 @@ import * as bodyParser from 'body-parser'
 export const PickerPOST = Picker.filter((req) => req.method === 'POST')
 
 PickerPOST.middleware(bodyParser.json({
-	limit: '10mb' // Arbitrary limit
+	limit: '50mb' // Arbitrary limit
 }))
 PickerPOST.middleware(bodyParser.text({
 	type: 'application/json',
-	limit: '10mb'
+	limit: '50mb'
 }))
 PickerPOST.middleware(bodyParser.text({
 	type: 'text/javascript',
-	limit: '1mb' // 1
+	limit: '50mb' // 1
 }))
 
 
