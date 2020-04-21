ecac3130275e125296a744d0d0624ab6bbe4458b
Jan Starzak <jan.starzak@gmail.com>
2020-04-21 15:21:43 +0200

    fix: limit amount of rendered notifications in DOM

--- meteor/client/ui/RundownView.tsx
+++ meteor/client/ui/RundownView.tsx
@@ -1882,7 +1882,7 @@ class RundownView extends MeteorReactComponent<Translated<IProps & ITrackedProps
 										translateX: ['100%', '0%']
 									}, easing: 'ease-in', duration: 500
 								}}>
-									{this.state.isNotificationsCenterOpen && <NotificationCenterPanel />}
+									{this.state.isNotificationsCenterOpen && <NotificationCenterPanel limitCount={15} />}
 								</VelocityReact.VelocityTransitionGroup>
 								<VelocityReact.VelocityTransitionGroup enter={{
 									animation: {
