1776767f39d1f0702d6679237be30e4f82746659
Tom Lee <thomasslee97@gmail.com>
2020-04-23 10:22:08 +0100

    feat: DVE, Jingle and VO thumbnails
    (cherrypicked from 187b4c7e31684f950fe0bab97c27404ab4c2bb4b)

--- meteor/client/ui/Shelf/DashboardPieceButton.tsx
+++ meteor/client/ui/Shelf/DashboardPieceButton.tsx
@@ -19,6 +19,7 @@ import { PubSub } from '../../../lib/api/pubsub'
 import { PieceId } from '../../../lib/collections/Pieces'
 import SplitInputIcon from '../PieceIcons/Renderers/SplitInput'
 import { PieceDisplayStyle } from '../../../lib/collections/RundownLayouts'
+import { DashboardPieceButtonSplitPreview } from './DashboardPieceButtonSplitPreview'
 
 export interface IAdLibListItem {
 	_id: PieceId,
@@ -134,12 +135,16 @@ export const DashboardPieceButton = translateWithTracker<IDashboardButtonProps, 
 		}
 	}
 
-	renderSplits () {
+	renderSplits (renderThumbnail: boolean = false) {
 		const splitAdLib = this.props.adLibListItem as AdLibPieceUi
 		if (splitAdLib && splitAdLib.content) {
-			return (
-				<SplitInputIcon abbreviation={this.props.layer.abbreviation} piece={splitAdLib} hideLabel={true} />
-			)
+			const splitContent = splitAdLib.content as SplitsContent
+			return <React.Fragment>
+				{renderThumbnail ?
+					<DashboardPieceButtonSplitPreview piece={this.props.adLibListItem as any as AdLibPieceUi} /> :
+					<SplitInputIcon abbreviation={this.props.layer.abbreviation} piece={splitAdLib} hideLabel={true} />
+				}
+			</React.Fragment>
 		}
 	}
 
@@ -176,7 +181,7 @@ export const DashboardPieceButton = translateWithTracker<IDashboardButtonProps, 
 					(this.props.layer.type === SourceLayerType.VT || this.props.layer.type === SourceLayerType.LIVE_SPEAK) ?
 						this.renderVTLiveSpeak() :
 					(this.props.layer.type === SourceLayerType.SPLITS) ?
-						this.renderSplits() :
+						this.renderSplits(isList) :
 						null
 				}
 				<span className='dashboard-panel__panel__button__label'>{isList && hasMediaInfo ? this.props.metadata!.mediainfo!.name : this.props.adLibListItem.name}</span>
