032b324e9418f7f0451651360a5efcbd698a6483
Jan Starzak <jan.starzak@gmail.com>
2020-05-07 17:24:59 +0200

    feat: validate in- & out-point change in MOS (#209)
    * feat: reject non-ACK response to replaceStoryItem
    
    * feat: show a message when waiting long for execution
    
    * fix: prevent default doUserAction handler
    
    * chore: more logging
    
    * feat: executeFunctionWithCustomTimeout
    
    * fix: make sure observer is defined before stopping
    
    * fix: pass the call exception to the front-end
    
    * fix: better error handling
    
    * feat: unset EditorialStart if ===0
    
    * fix: verify that the response is invalid
    
    * chore: notification formatting

--- meteor/client/ui/ClipTrimPanel/ClipTrimDialog.tsx
+++ meteor/client/ui/ClipTrimPanel/ClipTrimDialog.tsx
@@ -8,6 +8,9 @@ import { ModalDialog } from '../../lib/ModalDialog'
 import { doUserAction } from '../../lib/userAction'
 import { RundownPlaylistId } from '../../../lib/collections/RundownPlaylists'
 import { MeteorCall } from '../../../lib/api/methods'
+import { NotificationCenter, Notification, NoticeLevel } from '../../lib/notifications/notifications';
+import { protectString } from '../../../lib/lib';
+import { ClientAPI } from '../../../lib/api/client';
 
 export interface IProps {
 	playlistId: RundownPlaylistId
@@ -38,20 +41,71 @@ export const ClipTrimDialog = translate()(class ClipTrimDialog extends React.Com
 		})
 	}
 	handleAccept = (e) => {
+		const { t, selectedPiece } = this.props
+
 		this.props.onClose && this.props.onClose()
+		let pendingInOutPoints: NodeJS.Timer
 		doUserAction(this.props.t, e, 'Set In & Out points', (e) => MeteorCall.userAction.setInOutPoints(e,
 			this.props.playlistId,
-			this.props.selectedPiece.partId,
-			this.props.selectedPiece._id,
+			selectedPiece.partId,
+			selectedPiece._id,
 			this.state.inPoint,
 			this.state.duration
-		))
+		), (err, res) => {
+			clearTimeout(pendingInOutPoints)
+
+			if (ClientAPI.isClientResponseError(err) && err.message && err.message.match(/timed out/)) {
+				NotificationCenter.push(new Notification(
+					undefined,
+					NoticeLevel.CRITICAL,
+					<>
+						<strong>{selectedPiece.name}</strong>:&ensp;
+						{t('Trimming this clip has timed out. It\'s possible that the story is currently locked for writing in {{nrcsName}} and will eventually be updated. Make sure that the story is not being edited by other users.', { nrcsName: 'ENPS' })}
+					</>,
+					protectString('ClipTrimDialog')
+				))
+			} else if (ClientAPI.isClientResponseError(err) || err) {
+				NotificationCenter.push(new Notification(
+					undefined,
+					NoticeLevel.CRITICAL,
+					<>
+						<strong>{selectedPiece.name}</strong>:&ensp;
+						{t('Trimming this clip has failed due to an error: {{error}}.', { error: err.message || err.error || err })}
+					</>,
+					protectString('ClipTrimDialog')
+				))
+			} else {
+				NotificationCenter.push(new Notification(
+					undefined,
+					NoticeLevel.NOTIFICATION,
+					<>
+						<strong>{selectedPiece.name}</strong>:&ensp;
+						{t('Trimmed succesfully.')}
+					</>,
+					protectString('ClipTrimDialog')
+				))
+			}
+
+			return false // do not use default doUserAction failure handler
+		})
+		pendingInOutPoints = setTimeout(() => {
+			NotificationCenter.push(new Notification(
+				undefined,
+				NoticeLevel.WARNING,
+				<>
+					<strong>{selectedPiece.name}</strong>:&ensp;
+					{t('Trimming this clip is taking longer than expected. It\'s possible that the story is locked for writing in {{nrcsName}}.', { nrcsName: 'ENPS' })}
+				</>,
+				protectString('ClipTrimDialog')
+			))
+		}, 5 * 1000)
 	}
 	render () {
 		const { t } = this.props
 		return (
 			<ModalDialog title={t('Trim "{{name}}"', { name: this.props.selectedPiece.name })} show={true} acceptText={t('OK')} secondaryText={t('Cancel')}
-			onAccept={this.handleAccept} onDiscard={(e) => this.props.onClose && this.props.onClose()} onSecondary={(e) => this.props.onClose && this.props.onClose()}>
+			onAccept={this.handleAccept} onDiscard={(e) => this.props.onClose && this.props.onClose()} onSecondary={(e) => this.props.onClose && this.props.onClose()}
+			className='big'>
 				<ClipTrimPanel
 					studioId={this.props.studio._id}
 					playlistId={this.props.playlistId}
