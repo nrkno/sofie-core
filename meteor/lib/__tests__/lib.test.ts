389bd34d7f1af92d1efa36b4e123e459557750cf
Julian Waller <git@julusian.co.uk>
2020-05-19 18:21:05 +0100

    fix: implementation of mongoFindOptions and unit tests for it

--- meteor/lib/__tests__/lib.test.ts
+++ meteor/lib/__tests__/lib.test.ts
@@ -27,7 +27,8 @@ import {
 	partial,
 	partialExceptId,
 	escapeHtml,
-	protectString
+	protectString,
+	mongoFindOptions
 } from '../lib'
 import { Timeline, TimelineObjType, TimelineObjGeneric } from '../collections/Timeline'
 import { TSR } from 'tv-automation-sofie-blueprints-integration'
@@ -487,5 +488,170 @@ describe('lib/lib', () => {
 		expect(escapeHtml(`<div>Hello & goodbye! Please use '"'-signs!</div>`))
 		.toBe(`&lt;div&gt;Hello &amp; goodbye! Please use &#039;&quot;&#039;-signs!&lt;/div&gt;`)
 
+	})
+
+	describe('mongoFindOptions', () => {
+		const rawDocs = [1,2,3,4,5,6,7]
+
+		test('nothing', () => {
+			expect(mongoFindOptions(rawDocs)).toEqual(rawDocs)
+			expect(mongoFindOptions(rawDocs, {})).toEqual(rawDocs)
+		})
+		test('range', () => {
+			expect(mongoFindOptions(rawDocs, { limit: 4 })).toEqual([1,2,3,4])
+			expect(mongoFindOptions(rawDocs, { skip: 4 })).toEqual([5,6,7])
+			expect(mongoFindOptions(rawDocs, { skip: 2, limit: 3 })).toEqual([3,4,5])
+		})
+		test('transform', () => {
+			expect(() => mongoFindOptions(rawDocs, { transform: () => 1})).toThrowError('options.transform not implemented')
+		})
+
+		const rawDocs2 = [
+			{
+				_id: 1,
+				val: 'a',
+				val2: 'c'
+			},
+			{
+				_id: 2,
+				val: 'x',
+				val2: 'c'
+			},
+			{
+				_id: 3,
+				val: 'n',
+				val2: 'b'
+			},
+		]
+
+		test('fields', () => {
+			expect(() => mongoFindOptions(rawDocs, { fields: { val: 0, val2: 1 } })).toThrowError('options.fields cannot contain both include and exclude rules')
+			expect(() => mongoFindOptions(rawDocs, { fields: { _id: 0, val2: 1 } })).not.toThrowError()
+			expect(() => mongoFindOptions(rawDocs, { fields: { _id: 1, val: 0 } })).not.toThrowError()
+
+			expect(mongoFindOptions(rawDocs2, { fields: { val: 0 } })).toEqual([
+				{
+					_id: 1,
+					val2: 'c'
+				},
+				{
+					_id: 2,
+					val2: 'c'
+				},
+				{
+					_id: 3,
+					val2: 'b'
+				},
+			])
+			expect(mongoFindOptions(rawDocs2, { fields: { val: 0, _id: 0 } })).toEqual([
+				{
+					val2: 'c'
+				},
+				{
+					val2: 'c'
+				},
+				{
+					val2: 'b'
+				},
+			])
+			expect(mongoFindOptions(rawDocs2, { fields: { val: 1 } })).toEqual([
+				{
+					_id: 1,
+					val: 'a',
+				},
+				{
+					_id: 2,
+					val: 'x',
+				},
+				{
+					_id: 3,
+					val: 'n',
+				},
+			])
+			expect(mongoFindOptions(rawDocs2, { fields: { val: 1, _id: 0 } })).toEqual([
+				{
+					val: 'a',
+				},
+				{
+					val: 'x',
+				},
+				{
+					val: 'n',
+				},
+			])
+		})
+		
+		test('fields', () => {
+			expect(mongoFindOptions(rawDocs2, { sort: { val: 1 } })).toEqual([
+				{
+					_id: 1,
+					val: 'a',
+					val2: 'c'
+				},
+				{
+					_id: 3,
+					val: 'n',
+					val2: 'b'
+				},
+				{
+					_id: 2,
+					val: 'x',
+					val2: 'c'
+				},
+			])
+			expect(mongoFindOptions(rawDocs2, { sort: { val: -1 } })).toEqual([
+				{
+					_id: 2,
+					val: 'x',
+					val2: 'c'
+				},
+				{
+					_id: 3,
+					val: 'n',
+					val2: 'b'
+				},
+				{
+					_id: 1,
+					val: 'a',
+					val2: 'c'
+				},
+			])
+
+			expect(mongoFindOptions(rawDocs2, { sort: { val2: 1, val: 1 } })).toEqual([
+				{
+					_id: 3,
+					val: 'n',
+					val2: 'b'
+				},
+				{
+					_id: 1,
+					val: 'a',
+					val2: 'c'
+				},
+				{
+					_id: 2,
+					val: 'x',
+					val2: 'c'
+				},
+			])
+			expect(mongoFindOptions(rawDocs2, { sort: { val2: 1, val: -1 } })).toEqual([
+				{
+					_id: 3,
+					val: 'n',
+					val2: 'b'
+				},
+				{
+					_id: 2,
+					val: 'x',
+					val2: 'c'
+				},
+				{
+					_id: 1,
+					val: 'a',
+					val2: 'c'
+				},
+			])
+
+		})
 	})
 })
