7e313bfbd4a09c6ec271b24e78db1d4e358ca837
Julian Waller <git@julusian.co.uk>
2020-05-25 15:49:25 +0100

    feat: further prune some gateway device types that do not need to be known by core

--- meteor/lib/collections/PeripheralDeviceSettings/playoutDevice.ts
+++ meteor/lib/collections/PeripheralDeviceSettings/playoutDevice.ts
@@ -1,13 +1,11 @@
 import { TSR } from 'tv-automation-sofie-blueprints-integration'
 
+/** 
+ * The basic PlayoutDevice settings structure.
+ * Note: playout-gateway will likely have more than this here, but this is that core needs to know about
+ */
 export interface PlayoutDeviceSettings {
 	devices: {
 		[deviceId: string]: TSR.DeviceOptionsAny
 	}
-	mediaScanner: {
-		host: string
-		port: number
-	}
-	multiThreading?: boolean
-	multiThreadedResolver?: boolean
 }
