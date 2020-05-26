1fc74a315ad98f8879824611d2bf11520a5cc90e
Jan Starzak <jan.starzak@gmail.com>
2020-05-26 15:20:23 +0200

    fix: add typings for GamepadVibrationActuator

--- meteor/client/ui/Prompter/controller/joycon-device.ts
+++ meteor/client/ui/Prompter/controller/joycon-device.ts
@@ -3,6 +3,24 @@ import { PrompterViewInner } from '../PrompterView'
 
 const LOCALSTORAGE_MODE = 'prompter-controller-arrowkeys'
 
+declare global {
+	type GamepadHapticEffectType = 'dual-rumble'
+
+	type GamepadHapticsResult = 'invalid-parameter' | 'preempted' | 'not-supported' | 'complete'
+
+	interface GamepadHapticActuator {
+		canPlayEffectType(type: GamepadHapticEffectType)
+		playEffect(type: GamepadHapticEffectType, params: object): Promise<GamepadHapticsResult>
+		reset(): Promise<GamepadHapticsResult>
+	}
+
+	interface GamepadVibrationActuator extends GamepadHapticActuator { }
+
+	interface Gamepad {
+		vibrationActuator?: GamepadVibrationActuator
+	}
+}
+
 /**
  * This class handles control of the prompter using Keyboard keys sent from a contour shuttle
  * Up: shift + control + alt + [1-7]
@@ -15,24 +33,26 @@ export class JoyconController extends ControllerAbstract {
 
 	private _destroyed: boolean = false
 	private _prompterView: PrompterViewInner
+	private _hasReachedEnd: boolean = false
 
 
 	private static readonly _speedMap = [1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 7, 8, 9, 10, 12, 15, 18, 30]
-	private static readonly JOYCON_SLACK = .25
+	private static readonly JOYCON_SLACK = 0.25
 
 	private _updateSpeedHandle: number | null = null
 	private _lastSpeed = 0
 	private _currentPosition = 0
-	private __hasReachedEnd: boolean = true
-	private _joycon: Object | null = null
-	private _haptic: Object | null = null
+	private _joycon: Gamepad | null = null
+	private _haptic: GamepadHapticActuator | null = null
 
-	constructor (view: PrompterViewInner) {
+	constructor(view: PrompterViewInner) {
 		super(view)
-		window.addEventListener('gamepadconnected', e => {
-			if(!this._joycon) {
+		window.addEventListener('gamepadconnected', (e: GamepadEvent) => {
+			if (!this._joycon) {
 				this._joycon = e.gamepad
-				this._haptic = this._joycon.vibrationActuator
+				if (this._joycon!.vibrationActuator) {
+					this._haptic = this._joycon!.vibrationActuator
+				}
 				this._updateScrollPosition()
 			}
 		})
@@ -43,46 +63,43 @@ export class JoyconController extends ControllerAbstract {
 
 		this._prompterView = view
 	}
-	public destroy () {
+	public destroy() {
 		this._destroyed = true
 	}
-	public onKeyDown (e: KeyboardEvent) {
+	public onKeyDown(e: KeyboardEvent) {
 		// Nothing
 	}
-	public onKeyUp (e: KeyboardEvent) {
+	public onKeyUp(e: KeyboardEvent) {
 		// Nothing
 	}
-	public onMouseKeyDown (e: MouseEvent) {
+	public onMouseKeyDown(e: MouseEvent) {
 		// Nothing
 	}
-	public onMouseKeyUp (e: MouseEvent) {
+	public onMouseKeyUp(e: MouseEvent) {
 		// Nothing
 	}
-	public onWheel (e: WheelEvent) {
+	public onWheel(e: WheelEvent) {
 		// Nothing
 	}
 
-	private _updateScrollPosition () {
+	private _updateScrollPosition() {
 		if (this._joycon === null) return
 		if (this._updateSpeedHandle !== null) return
 		this._updateSpeedHandle = null
 		this._lastSpeed = 0
-		
-		
-		
+
 		const pad = navigator.getGamepads()[0]
-		if(pad) {
+		if (pad) {
 			const direction = pad.axes[1] < 0 ? 1 : -1	// @todo: decide direction by the controllers mapping/being inversed
 			const stickPos = Math.abs(pad.axes[1])		// @todo: decide direction by the controllers mapping/being inversed
-			
+
 			if (stickPos >= JoyconController.JOYCON_SLACK) { // filters out deadband
 				const mappedInput = 0 + (1 - 0) * ((stickPos - JoyconController.JOYCON_SLACK) / (1 - JoyconController.JOYCON_SLACK))
 				const i = Math.round(mappedInput * (JoyconController._speedMap.length - 1))
 				this._lastSpeed = JoyconController._speedMap[i] * direction
-			} else {
 			}
-		} 
-		
+		}
+
 
 		// update scroll position
 		window.scrollBy(0, this._lastSpeed)
@@ -92,11 +109,11 @@ export class JoyconController extends ControllerAbstract {
 		if (this._currentPosition !== undefined && scrollPosition !== undefined) {
 			if (this._currentPosition === scrollPosition) {
 				// We tried to move, but haven't
-				if(!this._hasReachedEnd && this._lastSpeed !== 0) {
+				if (!this._hasReachedEnd && this._lastSpeed !== 0 && this._haptic) {
 					this._hasReachedEnd = true
-					this._haptic.playEffect('dual-rumble', {duration: .03, startDelay: 0, strongMagnitude: .08, weakMagnitude: 0})
+					this._haptic.playEffect('dual-rumble', { duration: 0.03, startDelay: 0, strongMagnitude: 0.08, weakMagnitude: 0 }).catch(console.error)
 					setTimeout(() => {
-						this._haptic.playEffect('dual-rumble', {duration: .02, startDelay: 0, strongMagnitude: .04, weakMagnitude: 0})
+						this._haptic && this._haptic.playEffect('dual-rumble', { duration: 0.02, startDelay: 0, strongMagnitude: 0.04, weakMagnitude: 0 }).catch(console.error)
 					}, 70)
 				}
 
