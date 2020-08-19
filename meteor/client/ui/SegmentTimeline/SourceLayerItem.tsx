b977e271833d47769ee9b533d2a2944defc7f4d7
Jan Starzak <jan.starzak@gmail.com>
2020-08-19 17:16:37 +0200

    fix: UI: Still shows some cleared onEnd infinites

--- meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
@@ -335,7 +335,7 @@ export const SourceLayerItem = withTranslation()(
 				(innerPiece.lifespan !== PieceLifespan.WithinPart ||
 					(innerPiece.enable.start !== undefined && innerPiece.enable.duration === undefined)) &&
 				!piece.cropped &&
-				!piece.instance.userDuration
+				piece.renderedDuration === null
 			) {
 				itemDuration = this.props.partDuration - (piece.renderedInPoint || 0)
 			}
