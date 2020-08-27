fdc7fe99dd021c5f22facbee5c86d7034efd5847
Johan Nyman <johan@nytamin.se>
2020-08-27 10:33:42 +0200

    chore: fix test: Don't use stringigied function, due to istanbul messing things up.

--- meteor/server/api/blueprints/__tests__/lib.ts
+++ meteor/server/api/blueprints/__tests__/lib.ts
@@ -3,30 +3,26 @@ import { literal, protectString } from '../../../../lib/lib'
 import { Blueprint } from '../../../../lib/collections/Blueprints'
 
 export function generateFakeBlueprint(id: string, type?: BlueprintManifestType, codeFcn?: () => SomeBlueprintManifest) {
-	let codeFcnString = ''
-	if (!codeFcn) {
-		codeFcn = () => ({
-			blueprintType: (18321 as any) as BlueprintManifestType.SYSTEM, // magic number, to be replaced later
-			blueprintVersion: '0.0.0',
-			integrationVersion: '0.0.0',
-			TSRVersion: '0.0.0',
-			minimumCoreVersion: '0.0.0',
+	const codeFcnString = codeFcn
+		? codeFcn.toString()
+		: `\
+() => ({
+  blueprintType: ${type ? `"${type}"` : 'undefined'},
+  blueprintVersion: '0.0.0',
+  integrationVersion: '0.0.0',
+  TSRVersion: '0.0.0',
+  minimumCoreVersion: '0.0.0',
+  studioConfigManifest: [],
+  studioMigrations: [],
+  getBaseline: () => [],
+  getShowStyleId: () => null
+})`
 
-			studioConfigManifest: [],
-			studioMigrations: [],
-			getBaseline: () => [],
-			getShowStyleId: () => null,
-		})
-		codeFcnString = codeFcn && codeFcn.toString()
-		codeFcnString = codeFcnString.replace(/18321/, type ? `"${type}"` : 'undefined')
-	} else {
-		codeFcnString = codeFcn && codeFcn.toString()
-	}
 	return literal<Blueprint>({
 		_id: protectString(id),
 		name: 'Fake blueprint',
 		organizationId: null,
-		code: `({default: (${codeFcnString || '() => 5'})()})`,
+		code: `({default: (${codeFcnString})()})`,
 		created: 0,
 		modified: 0,
 
