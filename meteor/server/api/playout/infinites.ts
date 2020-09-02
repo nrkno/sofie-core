43390fb23616f67ed1d304f8b001bb978bc933c7
Tom Lee <thomasslee97@gmail.com>
2020-09-02 12:19:54 +0100

    test: Lower definitely ended duration to 3 seconds

--- meteor/server/api/playout/infinites.ts
+++ meteor/server/api/playout/infinites.ts
@@ -18,7 +18,7 @@ import {
 import { profiler } from '../profiler'
 
 // /** When we crop a piece, set the piece as "it has definitely ended" this far into the future. */
-export const DEFINITELY_ENDED_FUTURE_DURATION = 10 * 1000
+export const DEFINITELY_ENDED_FUTURE_DURATION = 3 * 1000
 
 /**
  * We can only continue adlib onEnd infinites if we go forwards in the rundown. Any distance backwards will clear them.
