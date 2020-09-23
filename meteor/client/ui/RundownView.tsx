75ee1c95151011d9646749dca517428a5d0ea5a0
Tom Lee <thomasslee97@users.noreply.github.com>
2020-09-23 23:17:08 +0100

    feat: tally tags (#309)
    This feature allows use of "tally tags", so that pieces on the timeline can be used as signals for tally state (On Air/Next) of AdLib Actions.
    
    * feat: Actions tally using piece tags
    
    * feat: Reduce data stored in RAM
    
    * fix: Next tally in current part
    
    * fix: Tally both on dashboard panels and adlib region panels
    
    * fix: Subscribe to all parts, pieces, and instances if in standalone shelf
    
    may need work, this fixes the issue where tally would not be present in a standalone shelf, due to not subscribing to any piece or part instances
    
    * fix: Only fetch piece instances for current, next, and previous part
    
    * fix: Allow adlib actions to happen close to autonext
    
    * fix: Next tally
    
    * fix: Rebase errors
    
    * chore: Rename onAir/setNext tags to currentPieceTags and nextPieceTags
    
    * fix: Look for adLibSourceId on piece instance
    
    * chore: use proper blueprints-integration dependency
    
    * chore: update package-lock
    
    * chore: update test after changing "definitely ended" constant to lower value
    
    Co-authored-by: Jan Starzak <jan.starzak@gmail.com>

--- meteor/client/ui/RundownView.tsx
+++ meteor/client/ui/RundownView.tsx
@@ -1531,6 +1531,7 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 					})
 				}
 			})
+
 			this.autorun(() => {
 				let playlist = RundownPlaylists.findOne(playlistId, {
 					fields: {
@@ -1580,6 +1581,23 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 							$in: rundownIDs,
 						},
 					})
+					if (this.props.onlyShelf) {
+						this.subscribe(PubSub.pieceInstances, {
+							rundownId: {
+								$in: rundownIDs,
+							},
+							partInstanceId: {
+								$in: [
+									playlist.currentPartInstanceId,
+									playlist.nextPartInstanceId,
+									playlist.previousPartInstanceId,
+								].filter((p) => p !== null),
+							},
+							reset: {
+								$ne: true,
+							},
+						})
+					}
 				}
 			})
 			this.autorun(() => {
