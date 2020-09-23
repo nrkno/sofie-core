77532742f13000c0dfcda0944d04340cf9413f34
Jan Starzak <jan.starzak@gmail.com>
2020-09-23 07:23:08 +0200

    chore: fix a bad merge in client/lib/rundown.ts

--- meteor/client/lib/rundown.ts
+++ meteor/client/lib/rundown.ts
@@ -240,17 +240,32 @@ export namespace RundownUtils {
 		segmentExtended: SegmentExtended
 		/** Parts in the segment, with additional information on the Part and the Pieces */
 		parts: Array<PartExtended>
+		/** A flag if the segment is currently on air (one of it's Parts is on air) */
+		isLiveSegment: boolean
+		/** A flag if the segment is currently next (one of it's Parts is on air) */
+		isNextSegment: boolean
+		/** The part that is currently on air, if the Segment is on air */
+		currentLivePart: PartExtended | undefined
+		/** The part that is currently set as next, if the Segment is next */
+		currentNextPart: PartExtended | undefined
 		/** A flag if any of the Parts have a Piece on a Layer with the 'Remote' flag on */
 		hasRemoteItems: boolean
 		/** A flag if any of the Parts have a Piece on a Layer with the 'Guest' flag on */
 		hasGuestItems: boolean
 		/** A flag if any of the Parts have already played */
 		hasAlreadyPlayed: boolean
+		/** A flag if the current on air part (doesn't have to be of this segment) will autonext */
+		autoNextPart: boolean
 	} {
-		// let nextPart: PartExtended | undefined = undefined
+		let isLiveSegment = false
+		let isNextSegment = false
+		let currentLivePart: PartExtended | undefined = undefined
+		let currentNextPart: PartExtended | undefined = undefined
 		let hasAlreadyPlayed = false
 		let hasRemoteItems = false
 		let hasGuestItems = false
+
+		let autoNextPart = false
 
 		let segmentExtended = literal<SegmentExtended>({
 			...segment,
@@ -262,6 +277,7 @@ export namespace RundownUtils {
 		// fetch all the parts for the segment
 		let partsE: Array<PartExtended> = []
 
+		const { currentPartInstance, nextPartInstance } = playlist.getSelectedPartInstances()
 		const segmentsAndParts = playlist.getSegmentsAndPartsSync(
 			{
 				_id: segment._id,
@@ -400,7 +416,9 @@ export namespace RundownUtils {
 					}
 
 					const { pieceGroup, capObjs } = createPieceGroupAndCap(piece)
-					pieceGroup.metaData = literal<PieceGroupMetadata>({ id: piece.piece._id })
+					pieceGroup.metaData = literal<PieceGroupMetadata>({
+						id: piece.piece._id,
+					})
 					partTimeline.push(pieceGroup)
 					partTimeline.push(...capObjs)
 
@@ -609,13 +627,28 @@ export namespace RundownUtils {
 
 			segmentExtended.outputLayers = outputLayers
 			segmentExtended.sourceLayers = sourceLayers
+
+			if (isNextSegment && !isLiveSegment && !autoNextPart && currentPartInstance) {
+				if (
+					currentPartInstance &&
+					currentPartInstance.part.expectedDuration &&
+					currentPartInstance.part.autoNext
+				) {
+					autoNextPart = true
+				}
+			}
 		}
 		return {
 			segmentExtended,
 			parts: partsE,
+			isLiveSegment,
+			currentLivePart,
+			currentNextPart,
+			isNextSegment,
 			hasAlreadyPlayed,
 			hasGuestItems,
 			hasRemoteItems,
+			autoNextPart,
 		}
 
 		// get the part immediately after the last segment
