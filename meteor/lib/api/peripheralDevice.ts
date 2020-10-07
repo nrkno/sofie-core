a0df7e5a55d1558a1e1763f4176fc90661fc27e2
Johan Nyman <johan@nytamin.se>
2020-10-07 10:45:22 +0200

    fix: timeoutTime was null

--- meteor/lib/api/peripheralDevice.ts
+++ meteor/lib/api/peripheralDevice.ts
@@ -436,10 +436,12 @@ export namespace PeripheralDeviceAPI {
 	export function executeFunctionWithCustomTimeout(
 		deviceId: PeripheralDeviceId,
 		cb: (err, result) => void,
-		timeoutTime: number = 3000,
+		timeoutTime: number | undefined,
 		functionName: string,
 		...args: any[]
 	) {
+		if (!timeoutTime) timeoutTime = 3000 // also handles null
+
 		let commandId: PeripheralDeviceCommandId = getRandomId()
 
 		let subscription: Meteor.SubscriptionHandle | null = null
