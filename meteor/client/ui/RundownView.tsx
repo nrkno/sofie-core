nrkno/sofie-core

commit 72d8500626df1c77cef585b88caa9455175e1260

feat: Gui Optimizations (#327)
* fix: make getResolvedSegment get executed less often

The tracker for all visible segments was re-running the autorun on every
take because it was reacting to current and next part changing.
Now the current and next parts affecting a segment will come as props.

* fix: make getResolvedSegment get executed even less often

The tracker was re-running the autorun when timings of Pieces were changing.

* refactor: move some calculations out of getResolvedSegment

* fix: properly handle setting a part as next

Props can update faster than the durations so we need to account for that.

* fix: not scrolling to a manually set segment on take

* fix: segments not rewound when resetting the rundown

Resetting the livePosition seems to fix the problem of segments
occasionally not rewinding correctly when resetting the rundown.
onFollowLiveLine called in componentDidUpdate was setting wrong scrollLeft based on old livePosition.

* fix: segments not autorewinding when leaving

State is updated in componentDidUpdate later than onRewindSegment is
called. this.isLiveSegment mirrors this.state.isLiveSegment but is set earlier.

* chore: remove a fix for IntersectionObserver bug in Chrome 76

The bug seems to be resolved by now, so the workaround could be removed.

* fix: a problem with double scrolling after a take

* chore: make the set as next menu item disabled

The item was not displayed when the context part was dynamically inserted. This has been replaced with displaying a disabled menu item, like with the current part.

* fix: prevent viewport jumping on takes

* chore: remove leftover console.log

There was a console.log in SegmentTimelineContainer left over after doing some investigations

* feat: an attempt at fixing the part jumping back-forth issue

* chore: Lint

* fix: restore a flush of temporaryPartInstances in RundownTiming

meteor/client/ui/RundownView.tsx
@@ -93,7 +93,7 @@ import { MeteorCall } from '../../lib/api/methods'
 import { PointerLockCursor } from '../lib/PointerLockCursor'
 import { AdLibPieceUi } from './Shelf/AdLibPanel'
 import { documentTitle } from '../lib/documentTitle'
-import { PartInstanceId } from '../../lib/collections/PartInstances'
+import { PartInstanceId, PartInstance } from '../../lib/collections/PartInstances'
 import { RundownDividerHeader } from './RundownView/RundownDividerHeader'
 
 export const MAGIC_TIME_SCALE_FACTOR = 0.03
@@ -721,7 +721,9 @@ const RundownHeader = withTranslation()(
 						(e) => MeteorCall.userAction.moveNext(e, this.props.playlist._id, horizonalDelta, verticalDelta),
 						(err, partId) => {
 							if (!err && partId) {
-								scrollToPart(partId).catch(() => console.error)
+								scrollToPart(partId).catch((error) => {
+									if (!error.toString().match(/another scroll/)) console.error(error)
+								})
 							}
 						}
 					)
@@ -1056,7 +1058,9 @@ const RundownHeader = withTranslation()(
 						if (!err && reloadResponse) {
 							if (!handleRundownPlaylistReloadResponse(t, this.props.playlist, reloadResponse)) {
 								if (this.props.playlist && this.props.playlist.nextPartInstanceId) {
-									scrollToPartInstance(this.props.playlist.nextPartInstanceId).catch(() => console.error)
+									scrollToPartInstance(this.props.playlist.nextPartInstanceId).catch((error) => {
+										if (!error.toString().match(/another scroll/)) console.error(error)
+									})
 								}
 							}
 						}
@@ -1305,6 +1309,8 @@ interface ITrackedProps {
 		inspector: boolean
 	}
 	bucketDisplayFilter: number[] | undefined
+	currentPartInstance: PartInstance | undefined
+	nextPartInstance: PartInstance | undefined
 }
 export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((props: IProps) => {
 	let playlistId
@@ -1318,6 +1324,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 	let rundowns: Rundown[] = []
 	let studio: Studio | undefined
 	let allParts: PartId[] = []
+	let currentPartInstance: PartInstance | undefined
+	let nextPartInstance: PartInstance | undefined
+
 	if (playlist) {
 		studio = Studios.findOne({ _id: playlist.studioId })
 		rundowns = playlist.getRundowns()
@@ -1328,6 +1337,7 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				},
 			})
 			.map((part) => part._id)
+		;({ currentPartInstance, nextPartInstance } = playlist.getSelectedPartInstances())
 	}
 
 	const params = queryStringParse(location.search)
@@ -1388,6 +1398,8 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 			inspector: displayOptions.includes('inspector'),
 		},
 		bucketDisplayFilter,
+		currentPartInstance,
+		nextPartInstance,
 	}
 })(
 	class RundownView extends MeteorReactComponent<Translated<IProps & ITrackedProps>, IState> {
@@ -1717,16 +1729,23 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				prevProps.playlist.currentPartInstanceId !== this.props.playlist.currentPartInstanceId &&
 				this.state.manualSetAsNext
 			) {
+				// reset followLiveSegments after a manual set as next
 				this.setState({
 					manualSetAsNext: false,
 					followLiveSegments: true,
 				})
+				if (this.props.playlist.currentPartInstanceId) {
+					scrollToPartInstance(this.props.playlist.currentPartInstanceId, true).catch((error) => {
+						if (!error.toString().match(/another scroll/)) console.error(error)
+					})
+				}
 			} else if (
 				this.props.playlist &&
 				prevProps.playlist &&
 				prevProps.playlist.active &&
 				!this.props.playlist.active
 			) {
+				// reset followLiveSegments after deactivating a rundown
 				this.setState({
 					followLiveSegments: true,
 				})
@@ -1737,7 +1756,10 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				this.props.playlist.active &&
 				this.props.playlist.nextPartInstanceId
 			) {
-				scrollToPartInstance(this.props.playlist.nextPartInstanceId).catch(() => console.error)
+				// scroll to next after activation
+				scrollToPartInstance(this.props.playlist.nextPartInstanceId).catch((error) => {
+					if (!error.toString().match(/another scroll/)) console.error(error)
+				})
 			} else if (
 				// after take
 				this.props.playlist &&
@@ -1746,7 +1768,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				this.props.playlist.currentPartInstanceId &&
 				this.state.followLiveSegments
 			) {
-				scrollToPartInstance(this.props.playlist.currentPartInstanceId, true).catch(() => console.error)
+				scrollToPartInstance(this.props.playlist.currentPartInstanceId, true).catch((error) => {
+					if (!error.toString().match(/another scroll/)) console.error(error)
+				})
 			} else if (
 				// initial Rundown open
 				this.props.playlist &&
@@ -1913,7 +1937,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 		}
 
 		onGoToTop = () => {
-			scrollToPosition(0).catch(console.error)
+			scrollToPosition(0).catch((error) => {
+				if (!error.toString().match(/another scroll/)) console.error(error)
+			})
 
 			window.requestIdleCallback(
 				() => {
@@ -1934,7 +1960,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				this.setState({
 					followLiveSegments: true,
 				})
-				scrollToPartInstance(this.props.playlist.nextPartInstanceId, true).catch(console.error)
+				scrollToPartInstance(this.props.playlist.nextPartInstanceId, true).catch((error) => {
+					if (!error.toString().match(/another scroll/)) console.error(error)
+				})
 				setTimeout(() => {
 					this.setState({
 						followLiveSegments: true,
@@ -1945,7 +1973,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 				this.setState({
 					followLiveSegments: true,
 				})
-				scrollToPartInstance(this.props.playlist.currentPartInstanceId, true).catch(console.error)
+				scrollToPartInstance(this.props.playlist.currentPartInstanceId, true).catch((error) => {
+					if (!error.toString().match(/another scroll/)) console.error(error)
+				})
 				setTimeout(() => {
 					this.setState({
 						followLiveSegments: true,
@@ -2057,7 +2087,9 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 								})
 							)
 						})
-						.catch(console.error)
+						.catch((error) => {
+							if (!error.toString().match(/another scroll/)) console.error(error)
+						})
 				}
 			}
 		}
@@ -2149,6 +2181,16 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 												onPieceClick={this.onSelectPiece}
 												onPieceDoubleClick={this.onPieceDoubleClick}
 												onHeaderNoteClick={(level) => this.onHeaderNoteClick(segment._id, level)}
+												ownCurrentPartInstance={
+													this.props.currentPartInstance && this.props.currentPartInstance.segmentId === segment._id
+														? this.props.currentPartInstance
+														: undefined
+												}
+												ownNextPartInstance={
+													this.props.nextPartInstance && this.props.nextPartInstance.segmentId === segment._id
+														? this.props.nextPartInstance
+														: undefined
+												}
 											/>
 										</VirtualElement>
 									</ErrorBoundary>

meteor/client/ui/SegmentTimeline/SegmentTimelineContainer.tsx
@@ -28,7 +28,7 @@ import { unprotectString, equalSets, equivalentArrays } from '../../../lib/lib'
 import { RundownUtils } from '../../lib/rundown'
 import { Settings } from '../../../lib/Settings'
 import { RundownId } from '../../../lib/collections/Rundowns'
-import { PartInstanceId, PartInstances } from '../../../lib/collections/PartInstances'
+import { PartInstanceId, PartInstances, PartInstance } from '../../../lib/collections/PartInstances'
 import { Parts, PartId } from '../../../lib/collections/Parts'
 import { doUserAction, UserAction } from '../../lib/userAction'
 import { MeteorCall } from '../../../lib/api/methods'
@@ -82,6 +82,8 @@ interface IProps {
 	followLiveSegments: boolean
 	segmentRef?: (el: React.ComponentClass, sId: string) => void
 	isLastSegment: boolean
+	ownCurrentPartInstance: PartInstance | undefined
+	ownNextPartInstance: PartInstance | undefined
 }
 interface IState {
 	scrollLeft: number
@@ -93,19 +95,19 @@ interface IState {
 	livePosition: number
 	displayTimecode: number
 	autoExpandCurrentNextSegment: boolean
+	isLiveSegment: boolean
+	isNextSegment: boolean
+	currentLivePart: PartUi | undefined
+	currentNextPart: PartUi | undefined
+	autoNextPart: boolean
 }
 interface ITrackedProps {
 	segmentui: SegmentUi | undefined
 	parts: Array<PartUi>
 	segmentNotes: Array<SegmentNote>
-	isLiveSegment: boolean
-	isNextSegment: boolean
-	currentLivePart: PartUi | undefined
-	currentNextPart: PartUi | undefined
 	hasRemoteItems: boolean
 	hasGuestItems: boolean
 	hasAlreadyPlayed: boolean
-	autoNextPart: boolean
 	lastValidPartIndex: number | undefined
 }
 export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITrackedProps>(
@@ -118,14 +120,9 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 				segmentui: undefined,
 				parts: [],
 				segmentNotes: [],
-				isLiveSegment: false,
-				isNextSegment: false,
-				currentLivePart: undefined,
-				currentNextPart: undefined,
 				hasRemoteItems: false,
 				hasGuestItems: false,
 				hasAlreadyPlayed: false,
-				autoNextPart: false,
 				lastValidPartIndex: undefined,
 			}
 		}
@@ -160,14 +157,9 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 			segmentui: o.segmentExtended,
 			parts: o.parts,
 			segmentNotes: notes,
-			isLiveSegment: o.isLiveSegment,
-			currentLivePart: o.currentLivePart,
-			currentNextPart: o.currentNextPart,
-			isNextSegment: o.isNextSegment,
 			hasAlreadyPlayed: o.hasAlreadyPlayed,
 			hasRemoteItems: o.hasRemoteItems,
 			hasGuestItems: o.hasGuestItems,
-			autoNextPart: o.autoNextPart,
 			lastValidPartIndex,
 		}
 	},
@@ -236,6 +228,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 		intersectionObserver: IntersectionObserver | undefined
 		mountedTime: number
 		playbackSimulationPercentage: number = 0
+		nextPartDisplayStartsAt: number
 
 		private pastInfinitesComp: Tracker.Computation | undefined
 
@@ -258,9 +251,14 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 				livePosition: 0,
 				displayTimecode: 0,
 				autoExpandCurrentNextSegment: !!Settings.autoExpandCurrentNextSegment,
+				isLiveSegment: false,
+				isNextSegment: false,
+				autoNextPart: false,
+				currentLivePart: undefined,
+				currentNextPart: undefined,
 			}
 
-			this.isLiveSegment = props.isLiveSegment || false
+			this.isLiveSegment = false
 			this.isVisible = false
 		}
 
@@ -343,12 +341,44 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 			window.requestAnimationFrame(() => {
 				this.mountedTime = Date.now()
 				if (this.isLiveSegment && this.props.followLiveSegments && !this.isVisible) {
-					scrollToSegment(this.props.segmentId, true).catch(console.error)
+					scrollToSegment(this.props.segmentId, true).catch((error) => {
+						if (!error.toString().match(/another scroll/)) console.error(error)
+					})
 				}
 			})
 		}
 
 		componentDidUpdate(prevProps: IProps & ITrackedProps) {
+			let isLiveSegment = false
+			let isNextSegment = false
+			let currentLivePart: PartExtended | undefined = undefined
+			let currentNextPart: PartExtended | undefined = undefined
+
+			let autoNextPart = false
+
+			if (this.props.ownCurrentPartInstance) {
+				isLiveSegment = true
+				currentLivePart = this.props.parts.find((part) => part.instance._id === this.props.ownCurrentPartInstance?._id)
+			}
+			if (this.props.ownNextPartInstance) {
+				isNextSegment = true
+				currentNextPart = this.props.parts.find((part) => part.instance._id === this.props.ownNextPartInstance?._id)
+			}
+			autoNextPart = !!(
+				currentLivePart &&
+				currentLivePart.instance.part.autoNext &&
+				currentLivePart.instance.part.expectedDuration
+			)
+			if (isNextSegment && !isLiveSegment && !autoNextPart && this.props.ownCurrentPartInstance) {
+				if (
+					this.props.ownCurrentPartInstance &&
+					this.props.ownCurrentPartInstance.part.expectedDuration &&
+					this.props.ownCurrentPartInstance.part.autoNext
+				) {
+					autoNextPart = true
+				}
+			}
+
 			if (this.rundownCurrentPartInstanceId !== this.props.playlist.currentPartInstanceId) {
 				this.playbackSimulationPercentage = 0
 			}
@@ -356,7 +386,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 			this.rundownCurrentPartInstanceId = this.props.playlist.currentPartInstanceId
 
 			// segment is becoming live
-			if (this.isLiveSegment === false && this.props.isLiveSegment === true) {
+			if (this.isLiveSegment === false && isLiveSegment === true) {
 				this.isLiveSegment = true
 				this.onFollowLiveLine(true, {})
 				this.startLive()
@@ -368,7 +398,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 				}
 			}
 			// segment is stopping from being live
-			if (this.isLiveSegment === true && this.props.isLiveSegment === false) {
+			if (this.isLiveSegment === true && isLiveSegment === false) {
 				this.isLiveSegment = false
 				this.stopLive()
 				if (Settings.autoRewindLeavingSegment) this.onRewindSegment()
@@ -394,27 +424,31 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 			}
 			if (
 				// the segment isn't live, is next, and the nextPartId has changed
-				!this.props.isLiveSegment &&
-				this.props.isNextSegment &&
-				this.props.currentNextPart &&
+				!isLiveSegment &&
+				isNextSegment &&
+				currentNextPart &&
 				this.props.playlist.nextPartInstanceId &&
-				prevProps.playlist.nextPartInstanceId !== this.props.playlist.nextPartInstanceId
+				(prevProps.playlist.nextPartInstanceId !== this.props.playlist.nextPartInstanceId ||
+					this.nextPartDisplayStartsAt !==
+						(this.context.durations?.partDisplayStartsAt &&
+							this.context.durations.partDisplayStartsAt[unprotectString(currentNextPart.partId)]))
 			) {
+				const nextPartDisplayStartsAt =
+					this.context.durations?.partDisplayStartsAt &&
+					this.context.durations.partDisplayStartsAt[unprotectString(currentNextPart.partId)]
 				const partOffset =
-					(this.context.durations &&
-						this.context.durations.partDisplayStartsAt &&
-						this.context.durations.partDisplayStartsAt[unprotectString(this.props.currentNextPart.partId)] -
-							this.context.durations.partDisplayStartsAt[unprotectString(this.props.parts[0].instance.part._id)]) ||
-					0
+					nextPartDisplayStartsAt -
+						this.context.durations.partDisplayStartsAt[unprotectString(this.props.parts[0].instance.part._id)] || 0
 
 				if (this.state.scrollLeft > partOffset) {
 					this.setState({
 						scrollLeft: partOffset,
 					})
 				}
+				this.nextPartDisplayStartsAt = nextPartDisplayStartsAt
 			}
 			// segment is becoming next
-			if (prevProps.isNextSegment === false && this.props.isNextSegment === true) {
+			if (this.state.isNextSegment === false && isNextSegment === true) {
 				if (this.state.autoExpandCurrentNextSegment) {
 					this.setState({
 						collapsed: false,
@@ -422,11 +456,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 				}
 			}
 			// segment is stopping from becoming and it's not live either
-			if (
-				prevProps.isNextSegment === true &&
-				this.props.isNextSegment === false &&
-				this.props.isLiveSegment === false
-			) {
+			if (this.state.isNextSegment === true && isNextSegment === false && isLiveSegment === false) {
 				if (this.state.autoExpandCurrentNextSegment) {
 					this.setState({
 						collapsed: UIStateStorage.getItemBoolean(
@@ -461,11 +491,19 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 			if (this.pastInfinitesComp && !equalSets(this.props.segmentsIdsBefore, prevProps.segmentsIdsBefore)) {
 				this.pastInfinitesComp.invalidate()
 			}
+
+			this.setState({
+				isLiveSegment,
+				isNextSegment,
+				currentLivePart,
+				currentNextPart,
+				autoNextPart,
+			})
 		}
 
 		componentWillUnmount() {
 			this._cleanUp()
-			if (this.intersectionObserver && this.props.isLiveSegment && this.props.followLiveSegments) {
+			if (this.intersectionObserver && this.state.isLiveSegment && this.props.followLiveSegments) {
 				if (typeof this.props.onSegmentScroll === 'function') this.props.onSegmentScroll()
 			}
 			if (this.partInstanceSub !== undefined) {
@@ -551,9 +589,10 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 		}
 
 		onRewindSegment = () => {
-			if (!this.props.isLiveSegment) {
+			if (!this.isLiveSegment) {
 				this.setState({
 					scrollLeft: 0,
+					livePosition: 0,
 				})
 			}
 		}
@@ -582,8 +621,8 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 		}
 
 		onAirLineRefresh = (e: TimingEvent) => {
-			if (this.props.isLiveSegment && this.props.currentLivePart) {
-				const currentLivePartInstance = this.props.currentLivePart.instance
+			if (this.state.isLiveSegment && this.state.currentLivePart) {
+				const currentLivePartInstance = this.state.currentLivePart.instance
 				const currentLivePart = currentLivePartInstance.part
 
 				let simulationPercentage = this.playbackSimulationPercentage
@@ -646,12 +685,12 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 
 		startLive = () => {
 			window.addEventListener(RundownTiming.Events.timeupdateHR, this.onAirLineRefresh)
-			// calculate the browser viewport zoom factor. Works perfectly in Chrome on Windows.
-			const zoomFactor = window.outerWidth / window.innerWidth
+			// As of Chrome 76, IntersectionObserver rootMargin works in screen pixels when root
+			// is viewport. This seems like an implementation bug and IntersectionObserver is
+			// an Experimental Feature in Chrome, so this might change in the future.
+			// Additionally, it seems that the screen scale factor needs to be taken into account as well
+			const zoomFactor = window.outerWidth / window.innerWidth / window.devicePixelRatio
 			this.intersectionObserver = new IntersectionObserver(this.visibleChanged, {
-				// As of Chrome 76, IntersectionObserver rootMargin works in screen pixels when root
-				// is viewport. This seems like an implementation bug and IntersectionObserver is
-				// an Experimental Feature in Chrome, so this might change in the future.
 				rootMargin: `-${getHeaderHeight() * zoomFactor}px 0px -${20 * zoomFactor}px 0px`,
 				threshold: [0, 0.25, 0.5, 0.75, 0.98],
 			})
@@ -684,7 +723,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 		onShowEntireSegment = (event: any) => {
 			this.setState({
 				scrollLeft: 0,
-				followLiveLine: this.props.isLiveSegment ? false : this.state.followLiveLine,
+				followLiveLine: this.state.isLiveSegment ? false : this.state.followLiveLine,
 			})
 			if (typeof this.props.onTimeScaleChange === 'function') {
 				this.props.onTimeScaleChange(
@@ -724,12 +763,12 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 						scrollLeft={this.state.scrollLeft}
 						playlist={this.props.playlist}
 						followLiveSegments={this.props.followLiveSegments}
-						isLiveSegment={this.props.isLiveSegment}
-						isNextSegment={this.props.isNextSegment}
+						isLiveSegment={this.state.isLiveSegment}
+						isNextSegment={this.state.isNextSegment}
 						isQueuedSegment={this.props.playlist.nextSegmentId === this.props.segmentId}
 						hasRemoteItems={this.props.hasRemoteItems}
 						hasGuestItems={this.props.hasGuestItems}
-						autoNextPart={this.props.autoNextPart}
+						autoNextPart={this.state.autoNextPart}
 						hasAlreadyPlayed={this.props.hasAlreadyPlayed}
 						followLiveLine={this.state.followLiveLine}
 						liveLineHistorySize={this.props.liveLineHistorySize}

meteor/client/lib/rundown.ts
@@ -261,7 +261,6 @@ export namespace RundownUtils {
 		let isNextSegment = false
 		let currentLivePart: PartExtended | undefined = undefined
 		let currentNextPart: PartExtended | undefined = undefined
-		// let nextPart: PartExtended | undefined = undefined
 		let hasAlreadyPlayed = false
 		let hasRemoteItems = false
 		let hasGuestItems = false
@@ -417,7 +416,9 @@ export namespace RundownUtils {
 					}
 
 					const { pieceGroup, capObjs } = createPieceGroupAndCap(piece)
-					pieceGroup.metaData = literal<PieceGroupMetadata>({ id: piece.piece._id })
+					pieceGroup.metaData = literal<PieceGroupMetadata>({
+						id: piece.piece._id,
+					})
 					partTimeline.push(pieceGroup)
 					partTimeline.push(...capObjs)
 

meteor/client/lib/viewPort.ts
@@ -104,6 +104,9 @@ export function getHeaderHeight(): number {
 	return HEADER_HEIGHT
 }
 
+let pendingSecondStageScroll: number | undefined
+let currentScrollingElement: HTMLElement | undefined
+
 export function scrollToSegment(
 	elementToScrollToOrSegmentId: HTMLElement | SegmentId,
 	forceScroll?: boolean,
@@ -118,44 +121,69 @@ export function scrollToSegment(
 		return Promise.reject('Could not find segment element')
 	}
 
+	if (!secondStage) {
+		currentScrollingElement = elementToScrollTo
+	} else if (secondStage && elementToScrollTo !== currentScrollingElement) {
+		return Promise.reject('Scroll overriden by another scroll')
+	}
+
 	let { top, bottom } = elementToScrollTo.getBoundingClientRect()
-	top += window.scrollY
-	bottom += window.scrollY
+	top = Math.floor(top)
+	bottom = Math.floor(bottom)
+
+	const headerHeight = Math.floor(getHeaderHeight())
 
 	// check if the item is in viewport
-	if (
-		forceScroll ||
-		bottom > window.scrollY + window.innerHeight ||
-		top < window.scrollY + getHeaderHeight() + HEADER_MARGIN
-	) {
-		return scrollToPosition(top, noAnimation).then(() => {
-			// retry scroll in case we have to load some data
-			return new Promise<boolean>((resolve, reject) => {
-				if (!secondStage) {
-					// check if we still need to scroll
-					let { top, bottom } = elementToScrollTo!.getBoundingClientRect()
-					top += window.scrollY
-					bottom += window.scrollY
+	if (forceScroll || bottom > Math.floor(window.innerHeight) || top < headerHeight) {
+		if (pendingSecondStageScroll) window.cancelIdleCallback(pendingSecondStageScroll)
 
-					if (
-						bottom > window.scrollY + window.innerHeight ||
-						top < window.scrollY + getHeaderHeight() + HEADER_MARGIN
-					) {
-						setTimeout(() => {
-							scrollToSegment(elementToScrollToOrSegmentId, false, true, true).then(resolve, reject)
-						}, 3000)
-					} else {
-						resolve(true)
-					}
-				} else {
-					resolve(true)
-				}
-			})
-		})
+		return scrollToPosition(top + window.scrollY, noAnimation).then(
+			() => {
+				// retry scroll in case we have to load some data
+				if (pendingSecondStageScroll) window.cancelIdleCallback(pendingSecondStageScroll)
+				return new Promise<boolean>((resolve, reject) => {
+					// scrollToPosition will resolve after some time, at which point a new pendingSecondStageScroll may have been created
+
+					pendingSecondStageScroll = window.requestIdleCallback(
+						() => {
+							let { top, bottom } = elementToScrollTo!.getBoundingClientRect()
+							top = Math.floor(top)
+							bottom = Math.floor(bottom)
+
+							if (!secondStage) {
+								let { top, bottom } = elementToScrollTo!.getBoundingClientRect()
+								top = Math.floor(top)
+								bottom = Math.floor(bottom)
+
+								if (bottom > Math.floor(window.innerHeight) || top < headerHeight) {
+									return scrollToSegment(elementToScrollToOrSegmentId, forceScroll, true, true).then(
+										resolve,
+										reject
+									)
+								} else {
+									resolve(true)
+								}
+							} else {
+								currentScrollingElement = undefined
+								resolve(true)
+							}
+						},
+						{ timeout: 250 }
+					)
+				})
+			},
+			(error) => {
+				if (!error.toString().match(/another scroll/)) console.error(error)
+				return false
+			}
+		)
 	}
 
 	return Promise.resolve(false)
 }
+
+let scrollToPositionRequest: number | undefined
+let scrollToPositionRequestReject: ((reason?: any) => void) | undefined
 
 export function scrollToPosition(scrollPosition: number, noAnimation?: boolean): Promise<void> {
 	if (noAnimation) {
@@ -168,7 +196,12 @@ export function scrollToPosition(scrollPosition: number, noAnimation?: boolean):
 		})
 	} else {
 		return new Promise((resolve, reject) => {
-			window.requestIdleCallback(
+			if (scrollToPositionRequest !== undefined) window.cancelIdleCallback(scrollToPositionRequest)
+			if (scrollToPositionRequestReject !== undefined)
+				scrollToPositionRequestReject('Prevented by another scroll')
+
+			scrollToPositionRequestReject = reject
+			scrollToPositionRequest = window.requestIdleCallback(
 				() => {
 					window.scroll({
 						top: Math.max(0, scrollPosition - getHeaderHeight() - HEADER_MARGIN),
@@ -177,7 +210,8 @@ export function scrollToPosition(scrollPosition: number, noAnimation?: boolean):
 					})
 					setTimeout(() => {
 						resolve()
-					}, 2000)
+						scrollToPositionRequestReject = undefined
+					}, 3000)
 				},
 				{ timeout: 250 }
 			)

meteor/client/ui/RundownView/RundownSystemStatus.tsx
@@ -244,7 +244,9 @@ export const RundownSystemStatus = translateWithTracker(
 				}
 			}
 			if (segmentId) {
-				scrollToSegment(segmentId).catch(console.error)
+				scrollToSegment(segmentId).catch((error) => {
+					if (!error.toString().match(/another scroll/)) console.error(error)
+				})
 			}
 		}
 		clickNotes() {

commit 45481ec8f7e59eefa46ba3758d14100847d17987

fix: show autoNext status not shown in some cases #72d85006 caused a regression which made autoNext status not to be correctly reported on a Part when the current Part and the next Part were in different Segments.

meteor/client/ui/RundownView.tsx
@@ -2246,7 +2246,15 @@ export const RundownView = translateWithTracker<IProps, IState, ITrackedProps>((
 												onPieceDoubleClick={this.onPieceDoubleClick}
 												onHeaderNoteClick={(level) => this.onHeaderNoteClick(segment._id, level)}
 												ownCurrentPartInstance={
-													this.props.currentPartInstance && this.props.currentPartInstance.segmentId === segment._id
+													// feed the currentPartInstance into the SegmentTimelineContainer component, if the currentPartInstance
+													// is a part of the segment
+													(this.props.currentPartInstance &&
+														this.props.currentPartInstance.segmentId === segment._id) ||
+													// or the nextPartInstance is a part of this segment, and the currentPartInstance is autoNext
+													(this.props.nextPartInstance &&
+														this.props.nextPartInstance.segmentId === segment._id &&
+														this.props.currentPartInstance &&
+														this.props.currentPartInstance.part.autoNext)
 														? this.props.currentPartInstance
 														: undefined
 												}

meteor/client/ui/SegmentTimeline/SegmentTimelineContainer.tsx
@@ -356,7 +356,7 @@ export const SegmentTimelineContainer = translateWithTracker<IProps, IState, ITr
 
 			let autoNextPart = false
 
-			if (this.props.ownCurrentPartInstance) {
+			if (this.props.ownCurrentPartInstance && this.props.ownCurrentPartInstance.segmentId === this.props.segmentId) {
 				isLiveSegment = true
 				currentLivePart = this.props.parts.find((part) => part.instance._id === this.props.ownCurrentPartInstance?._id)
 			}