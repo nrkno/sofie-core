fcdac9b89e316d22a000c740f2f6bc8eb4a03d36
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-11-16 18:04:52 +0100

    fix: better formatting of dates in rundown lobby

--- meteor/client/ui/RundownList/JonasFormattedTime.tsx
+++ meteor/client/ui/RundownList/JonasFormattedTime.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 import { TFunction } from 'i18next'
 import moment from 'moment'
 import { getCurrentTime } from '../../../lib/lib'
-import { time } from 'console'
-
-const yesterday = moment().subtract(1, 'days')
-const lastWeek = moment().subtract(6, 'days')
-const tomorrow = moment().add(1, 'days')
-const nextWeek = moment().add(6, 'days')
 
 interface IJonasFormattedTimeProps {
 	t: TFunction
@@ -16,41 +10,34 @@ interface IJonasFormattedTimeProps {
 
 export default function JonasFormattedTime(props: IJonasFormattedTimeProps) {
 	const { timestamp, t } = props
-	// const { t } = props
-
-	// const timestamp = yesterday.toDate().valueOf()
 
 	const now = moment(getCurrentTime()) // use synced time instead of client time
+	const timeToFormat = moment(timestamp)
 
 	// Overrides to be able to display calendar based string with Moment without
-	// time of day. This is horrible, and hopefully will be replaced with something
-	// smoother when we replace Moment
-	// const momentCalendarOptions = {
-	// 	sameDay: `[${t('Today')}] HH:mm:ss`,
-	// 	nextDay: `[${t('Tomorrow')}] HH:mm:ss`,
-	// 	nextWeek: 'dddd HH:mm:ss',
-	// 	lastDay: `[${t('Yesterday')}] HH:mm:ss`,
-	// 	lastWeek: `[${t('Last')}] dddd HH:mm:ss`,
-	// 	sameElse: 'DD/MM/YYYY'
-	// }
-	const momentCalendarOptions = Object.assign(
-		{},
-		{
-			lastDay: `[${t('Yesterday')}] HH:mm:ss`,
-			nextDay: `[${t('Tomorrow')}] HH:mm:ss`,
-		}
-	)
+	// time of day. Because we override the strings from Moment they won't be
+	// automatically localized by Moment. Therefore we need to use the general
+	// translation functionality for this in order for localization to work.
+	// This is horrible, and hopefully will be replaced with something
+	// smoother when we replace Moment.
+	const momentCalendarOptions = {
+		sameDay: `[${t('Today')}] HH:mm:ss`,
+		lastDay: `[${t('Yesterday')}] HH:mm:ss`,
+		nextDay: `[${t('Tomorrow')}] HH:mm:ss`,
+		nextWeek: 'dddd HH:mm:ss',
+		lastWeek: `[${t('Last')}] dddd HH:mm:ss`,
+	}
 
-	const diff = now.diff(timestamp, 'days')
+	const diff = now.diff(timeToFormat, 'days')
 
 	console.debug('diff', diff)
 	let formattedDateString: string
 	if (Math.abs(diff) < 6) {
-		formattedDateString = moment(timestamp).calendar(now, momentCalendarOptions)
+		formattedDateString = timeToFormat.calendar(now, momentCalendarOptions)
 	} else if (now.isBefore(timestamp)) {
-		formattedDateString = now.from(timestamp)
+		formattedDateString = now.from(timeToFormat)
 	} else {
-		formattedDateString = now.to(timestamp)
+		formattedDateString = now.to(timeToFormat)
 	}
 
 	return <span>{formattedDateString}</span>
