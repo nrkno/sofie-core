bbe47a8a1530f9820407aed36f9bc42ffff4e1d8
Jan Starzak <jan.starzak@gmail.com>
2020-11-19 13:41:27 +0100

    feat: additional Action Manifest properties

--- packages/blueprints-integration/src/action.ts
+++ packages/blueprints-integration/src/action.ts
@@ -5,10 +5,37 @@ export interface ActionUserData {
 	[key: string]: any
 }
 
+export enum ActionWorksOn {
+	/** AdLib action operates on the current part */
+	currentPart = 'currentPart',
+	/** AdLib action operates on the next part */
+	nextPart = 'nextPart',
+	/** AdLib action may operate on the current or next part, depending on current part's contents */
+	currentOrNextPart = 'currentOrNextPart',
+	/** AdLib action queues a new part and sets it as next */
+	queuesPart = 'queuesNewPart',
+	/** AdLib action may queue a new part or operate on the next part, depending on next part's contents */
+	queuesOrNextPart = 'queuesOrNextPart',
+}
+
+export enum ActionExecuteAfterChanged {
+	/** Do not execute the action after userData has changed, unless specifically triggered by the user */
+	none = 'none',
+	/** Execute the action immediately after userData has changed */
+	immediately = 'immediately',
+	/** Execute the action after userData has changed and there was an identifiable period of calm in the changes */
+	debounce = 'debounce',
+}
+
 export interface IBlueprintActionManifestDisplay {
+	/** A label to be displayed to the user */
 	label: string
+	/** An optional, longer description that will not be immediately visible to the user */
 	description?: string
 	_rank?: number
+
+	/** A flag, indicating if the action operates on the On Air/Current Part or if it operates on the Next Part/Queues a new Part. If not present, ActionWorksOn.currentPart is assumed. */
+	worksOn?: ActionWorksOn
 
 	tags?: string[]
 	/** Piece tags to use to determine if action is currently active */
@@ -38,8 +65,9 @@ export interface IBlueprintActionManifest {
 	userDataManifest: {
 		/** List of editable fields in userData, to allow for customising */
 		editableFields?: ConfigManifestEntry[]
-		// Example future options:
-		// callActionrightAfterEdit: boolean,
+		/** Execute the action after userData is changed. If not present ActionExecuteAfterChanged.none is assumed. */
+		executeOnUserDataChanged?: ActionExecuteAfterChanged
+		// Potential future properties:
 		// asloDisplayACtionButton: boolean
 	}
 
