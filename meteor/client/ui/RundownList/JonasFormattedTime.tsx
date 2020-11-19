837e409fca4477c9e5ce59922065b40168fe05c6
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-11-19 14:24:27 +0100

    fix: localized time for last modified value in rundown lobby

--- meteor/client/ui/RundownList/JonasFormattedTime.tsx
+++ meteor/client/ui/RundownList/JonasFormattedTime.tsx
@@ -8,6 +8,9 @@ interface IJonasFormattedTimeProps {
 	timestamp: number
 }
 
+const browserLanguage = window.navigator.language
+moment.locale(browserLanguage === 'no' ? 'nb' : browserLanguage)
+
 export default function JonasFormattedTime(props: IJonasFormattedTimeProps) {
 	const { timestamp, t } = props
 
