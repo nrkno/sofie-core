ad3672d55d0b5368e710ed487fc646f9bffcf4ed
Tom Lee <thomasslee97@gmail.com>
2020-12-03 17:14:19 +0000

    fix: Renamed properties

--- meteor/client/ui/Shelf/DashboardPanel.tsx
+++ meteor/client/ui/Shelf/DashboardPanel.tsx
@@ -795,7 +795,7 @@ export function isAdLibOnAir(
 ) {
 	if (
 		unfinishedAdLibIds.includes(adLib._id) ||
-		(adLib.onAirTags && adLib.onAirTags.every((tag) => unfinishedTags.includes(tag)))
+		(adLib.currentPieceTags && adLib.currentPieceTags.every((tag) => unfinishedTags.includes(tag)))
 	) {
 		return true
 	}
@@ -810,8 +810,8 @@ export function isAdLibNext(
 ) {
 	if (
 		nextAdLibIds.includes(adLib._id) ||
-		(adLib.setNextTags && adLib.setNextTags.every((tag) => unfinishedTags.includes(tag))) ||
-		(adLib.setNextTags && adLib.setNextTags.every((tag) => nextTags.includes(tag)))
+		(adLib.nextPieceTags && adLib.nextPieceTags.every((tag) => unfinishedTags.includes(tag))) ||
+		(adLib.nextPieceTags && adLib.nextPieceTags.every((tag) => nextTags.includes(tag)))
 	) {
 		return true
 	}
