a592371788603009cbe53e83fcf93de07517e3b1
Julian Waller <git@julusian.co.uk>
2020-12-09 12:02:05 +0000

    fix: Continue adlib action OnEnd infinites into queued parts

--- meteor/server/api/playout/adlib.ts
+++ meteor/server/api/playout/adlib.ts
@@ -26,7 +26,12 @@ import {
 	getAllPieceInstancesFromCache,
 	getSelectedPartInstancesFromCache,
 } from './lib'
-import { convertAdLibToPieceInstance, getResolvedPieces, convertPieceToAdLibPiece } from './pieces'
+import {
+	convertAdLibToPieceInstance,
+	getResolvedPieces,
+	convertPieceToAdLibPiece,
+	setupPieceInstanceInfiniteProperties,
+} from './pieces'
 import { updateTimeline } from './timeline'
 import { updatePartRanks, afterRemoveParts } from '../rundown'
 import { rundownPlaylistSyncFunction, RundownSyncFunctionPriority } from '../ingest/rundownInput'
@@ -431,15 +436,16 @@ export namespace ServerPlayoutAdLibAPI {
 
 		newPieceInstances.forEach((pieceInstance) => {
 			// Ensure it is labelled as dynamic
+			pieceInstance.dynamicallyInserted = getCurrentTime()
 			pieceInstance.partInstanceId = newPartInstance._id
 			pieceInstance.piece.startPartId = newPartInstance.part._id
+
+			setupPieceInstanceInfiniteProperties(pieceInstance)
 
 			cache.PieceInstances.insert(pieceInstance)
 		})
 
 		updatePartRanks(cache, rundownPlaylist, [newPartInstance.part.segmentId])
-
-		setNextPart(cache, rundownPlaylist, newPartInstance)
 
 		// Find and insert any rundown defined infinites that we should inherit
 		const part = cache.Parts.findOne(newPartInstance.part._id)
@@ -457,6 +463,8 @@ export namespace ServerPlayoutAdLibAPI {
 			cache.PieceInstances.insert(pieceInstance)
 		}
 
+		setNextPart(cache, rundownPlaylist, newPartInstance)
+
 		if (span) span.end()
 	}
 
@@ -472,6 +480,8 @@ export namespace ServerPlayoutAdLibAPI {
 		newPieceInstance.partInstanceId = existingPartInstance._id
 		newPieceInstance.piece.startPartId = existingPartInstance.part._id
 		newPieceInstance.dynamicallyInserted = getCurrentTime()
+
+		setupPieceInstanceInfiniteProperties(newPieceInstance)
 
 		// exclusiveGroup is handled at runtime by processAndPrunePieceInstanceTimings
 
