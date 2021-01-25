424065697f412c9f089b3f9bd9637712144a818d
Julian Waller <git@julusian.co.uk>
2021-01-25 12:57:19 +0000

    fix: reject ingest updates when rundown or segment are orphaned

--- meteor/server/api/__tests__/rundown.test.ts
+++ meteor/server/api/__tests__/rundown.test.ts
@@ -31,7 +31,6 @@ export enum RundownAPIMethods { // Using our own method definition, to catch ext
 	'resyncRundown' = 'rundown.resyncRundown',
 	'resyncSegment' = 'rundown.resyncSegment',
 	'unsyncRundown' = 'rundown.unsyncRundown',
-	'unsyncSegment' = 'rundown.unsyncSegment',
 	'moveRundown' = 'rundown.moveRundown',
 	'restoreRundownsInPlaylistToDefaultOrder' = 'rundown.restoreRundownsInPlaylistToDefaultOrder',
 }
