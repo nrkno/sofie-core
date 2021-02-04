cb39214132d52af64d2f8676038f0348236989f4
ianshade <zegzulakrzysztof@gmail.com>
2021-02-04 15:59:24 +0100

    feat: add styling capability to StyledTimecode
    Wrap hours in a span. Add classes to hours and frames.
    Add class indicating when there are zero hours, letting
    custom installation css to hide it

--- meteor/client/lib/StyledTimecode.tsx
+++ meteor/client/lib/StyledTimecode.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ClassNames from 'classnames'
 import { Time, formatDurationAsTimecode } from '../../lib/lib'
 
 interface IProps {
@@ -11,12 +12,21 @@ const FRAMES_INLINE_STYLE: React.CSSProperties = {
 
 export function StyledTimecode({ time: time }: IProps) {
 	const timecode = formatDurationAsTimecode(time)
-	const hoursMinutesSeconds = timecode.substring(0, 8)
+	const hours = timecode.substring(0, 3)
+	const minutesSeconds = timecode.substring(3, 8)
 	const frames = timecode.substring(8)
 	return (
 		<>
-			{hoursMinutesSeconds}
-			<span style={FRAMES_INLINE_STYLE}>{frames}</span>
+			<span
+				className={ClassNames('styled-timecode__hours', {
+					'zero-hours': hours === '00:',
+				})}>
+				{hours}
+			</span>
+			{minutesSeconds}
+			<span className="styled-timecode__frames" style={FRAMES_INLINE_STYLE}>
+				{frames}
+			</span>
 		</>
 	)
 }
