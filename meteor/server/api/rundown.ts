16e36fa7efc4738378c600689ad347bdb366e450
Julian Waller <git@julusian.co.uk>
2021-02-09 22:42:34 +0000

    fix: add missing contexts to blueprint api methods

--- meteor/server/api/rundown.ts
+++ meteor/server/api/rundown.ts
@@ -125,7 +125,8 @@ export function selectShowStyleVariant(
 
 	const variantId: ShowStyleVariantId | null = protectString(
 		showStyleBlueprint.blueprint.getShowStyleVariantId(
-			unprotectObjectArray(showStyleVariants) as any,
+			context,
+			unprotectObjectArray(showStyleVariants),
 			ingestRundown
 		)
 	)
@@ -203,14 +204,14 @@ export function produceRundownPlaylistRanks(
 
 	const playlistInfo: BlueprintResultRundownPlaylist | null = studioBlueprint.blueprint.getRundownPlaylistInfo
 		? studioBlueprint.blueprint.getRundownPlaylistInfo(
-				// new StudioUserContext(
-				// 	{
-				// 		name: 'produceRundownPlaylistRanks',
-				// 		identifier: `studioId=${studio._id},playlistId=${unprotectString(playlistId)}`,
-				// 		tempSendUserNotesIntoBlackHole: true,
-				// 	},
-				// 	studio
-				// ),
+				new StudioUserContext(
+					{
+						name: 'produceRundownPlaylistRanks',
+						identifier: `studioId=${studio._id},playlistId=${unprotectString(playlistId)}`,
+						tempSendUserNotesIntoBlackHole: true,
+					},
+					studio
+				),
 				unprotectObjectArray(rundowns)
 		  )
 		: null
@@ -269,16 +270,16 @@ export function produceRundownPlaylistInfoFromRundown(
 
 			const playlistInfo: BlueprintResultRundownPlaylist | null = studioBlueprint.blueprint.getRundownPlaylistInfo
 				? studioBlueprint.blueprint.getRundownPlaylistInfo(
-						// new StudioUserContext(
-						// 	{
-						// 		name: 'produceRundownPlaylistInfoFromRundown',
-						// 		identifier: `studioId=${studio._id},playlistId=${unprotectString(
-						// 			playlistId
-						// 		)},rundownId=${currentRundown._id}`,
-						// 		tempSendUserNotesIntoBlackHole: true,
-						// 	},
-						// 	studio
-						// ),
+						new StudioUserContext(
+							{
+								name: 'produceRundownPlaylistInfoFromRundown',
+								identifier: `studioId=${studio._id},playlistId=${unprotectString(
+									playlistId
+								)},rundownId=${currentRundown._id}`,
+								tempSendUserNotesIntoBlackHole: true,
+							},
+							studio
+						),
 						unprotectObjectArray(rundowns)
 				  )
 				: null
