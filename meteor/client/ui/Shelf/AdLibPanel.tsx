982cbd2e1a93e7d0f085ef3ab5a20625171f4cb9
Tom Lee <thomasslee97@gmail.com>
2021-02-15 17:16:22 +0000

    fix: Register shortcuts according to rank in adlib panels

--- meteor/client/ui/Shelf/AdLibPanel.tsx
+++ meteor/client/ui/Shelf/AdLibPanel.tsx
@@ -825,6 +825,7 @@ export function fetchAndFilter(props: Translated<IAdLibPanelProps>): AdLibFetchA
 
 					rundownBaselineAdLibs = rundownBaselineAdLibs
 						.concat(globalAdLibActions)
+						.sort((a, b) => a._rank - b._rank)
 						.map((item) => {
 							// automatically assign hotkeys based on adLibItem index
 							const uiAdLib: AdLibPieceUi = _.clone(item)
@@ -851,7 +852,6 @@ export function fetchAndFilter(props: Translated<IAdLibPanelProps>): AdLibFetchA
 							// always add them to the list
 							return uiAdLib
 						})
-						.sort((a, b) => a._rank - b._rank)
 
 					return rundownBaselineAdLibs.sort((a, b) => a._rank - b._rank)
 				},
