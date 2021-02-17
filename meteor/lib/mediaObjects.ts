7c4f287a79856918c96cf927882ba157e17bcdd9
Tom Lee <thomasslee97@gmail.com>
2021-02-17 16:19:44 +0000

    Revert "temp: Disable status check for Jingles"
    This reverts commit 0062a14a51c2bfcdff0b5a3056eaecad67739448.

--- meteor/lib/mediaObjects.ts
+++ meteor/lib/mediaObjects.ts
@@ -103,7 +103,7 @@ export function getMediaObjectMediaId(piece: Pick<IBlueprintPieceGeneric, 'conte
 	switch (sourceLayer.type) {
 		case SourceLayerType.VT:
 		case SourceLayerType.LIVE_SPEAK:
-			// case SourceLayerType.TRANSITION:
+		case SourceLayerType.TRANSITION:
 			return (piece.content as VTContent)?.fileName?.toUpperCase()
 		case SourceLayerType.GRAPHICS:
 			return (piece.content as GraphicsContent)?.fileName?.toUpperCase()
@@ -133,7 +133,7 @@ export function checkPieceContentStatus(
 		switch (sourceLayer.type) {
 			case SourceLayerType.VT:
 			case SourceLayerType.LIVE_SPEAK:
-				// case SourceLayerType.TRANSITION:
+			case SourceLayerType.TRANSITION:
 				// If the fileName is not set...
 				if (!fileName) {
 					newStatus = RundownAPI.PieceStatusCode.SOURCE_NOT_SET
