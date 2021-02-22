4e78a4218b1fa32ddceefc3be2b4c3d11ab5e162
Jan Starzak <jan.starzak@gmail.com>
2021-02-22 13:17:10 +0100

    fix: polyfills need to be required first

--- meteor/client/lib/polyfill/promise.allSettled.ts
+++ meteor/client/lib/polyfill/promise.allSettled.ts
@@ -2,7 +2,7 @@
  * CasparCG HTML renderer in 2.1.12 uses an old version of Chromium that doesn't support Promise.allSettled and as a
  * result causes views rendered using it to crash. Promise.allSettled is used as a part of the i18next integration.
  * This polyfill can be removed once CasparCG HTML renderer is updated to Chromium >= 76.
- * 	   -- Jan Starzak, 2021/02/22
+ * 	   -- 2021/02/22, Jan Starzak <jan.starzak@gmail.com>
  */
 import allSettled from 'promise.allsettled'
 
