e36dcdd7d904e0b42367b5b76704ad125c903bef
Jan Starzak <jan.starzak@gmail.com>
2021-02-23 11:54:35 +0100

    chore: fix checkLicenses script

--- scripts/checkLicenses.js
+++ scripts/checkLicenses.js
@@ -5,23 +5,22 @@ const legally = require('../meteor/node_modules/legally')
 let validLicenses = []
 let excludePackages = []
 
+process.argv.forEach((argString) => {
+	let m
+	m = argString.match(/--allowed=["']([^"']*)["']/)
+	if (!m) m = argString.match(/--allowed=(.*)/)
+	if (m) {
+		const strs = m[1].split(',')
+		console.log(`Valid licenses: ${strs.join(', ')}`)
+		validLicenses = strs.map(str => new RegExp(str), 'i')
+	}
 
-const argString = process.argv.join(' ')
-
-let m
-m = argString.match(/--allowed=['"]([^"']*)["']/)
-if (!m) m = argString.match(/--allowed=([^ ]*)/)
-if (m) {
-	const strs = m[1].split(',')
-	console.log(`Valid licenses: ${strs.join(', ')}`)
-	validLicenses = strs.map(str => new RegExp(str), 'i')
-}
-
-m = argString.match(/--excludePackages=(.*)/)
-if (m) {
-	excludePackages = m[1].split(',')
-	console.log(`Excluding packages: ${excludePackages.join(', ')}`)
-}
+	m = argString.match(/--excludePackages=(.*)/)
+	if (m) {
+		excludePackages = m[1].split(',')
+		console.log(`Excluding packages: ${excludePackages.join(', ')}`)
+	}
+})
 
 if (!validLicenses.length) {
 	throw 'usage: node checkLicenses.js --allowed=MIT,ISC --excludePackages=badPackageWhoDoesntSpeficyLicense'
@@ -30,40 +29,40 @@ if (!validLicenses.length) {
 console.log('Checking used licenses...')
 
 legally()
-.then((licenses) => {
-	let ok = true
+	.then((licenses) => {
+		let ok = true
 
-	for (const [package, info] of Object.entries(licenses)) {
-		let exclude = false
-		for (const excludePackage of excludePackages) {
-			if (package.match(new RegExp(`^${excludePackage}@`))) {
-				exclude = true
-			}
-		}
-		if (exclude) continue
-
-		let foundApproved = false
-		for (const license of info.package) {
-			for (const validLicense of validLicenses) {
-				if (license.match(validLicense) ) {
-					foundApproved = true
-					break
+		for (const [package, info] of Object.entries(licenses)) {
+			let exclude = false
+			for (const excludePackage of excludePackages) {
+				if (package.match(new RegExp(`^${excludePackage}@`))) {
+					exclude = true
 				}
 			}
-		}
-		if (!foundApproved) {
-			console.log(`Unapproved License: "${info.package}" in ${package}`)
-			ok = false
-		}
-	}
+			if (exclude) continue
 
-	if (ok) {
-		console.log('All is well :)')
-		process.exit(0)
-	} else {
+			let foundApproved = false
+			for (const license of info.package) {
+				for (const validLicense of validLicenses) {
+					if (license.match(validLicense)) {
+						foundApproved = true
+						break
+					}
+				}
+			}
+			if (!foundApproved) {
+				console.log(`Unapproved License: "${info.package}" in ${package}`)
+				ok = false
+			}
+		}
+
+		if (ok) {
+			console.log('All is well :)')
+			process.exit(0)
+		} else {
+			process.exit(100)
+		}
+	}).catch((e) => {
+		console.error(e)
 		process.exit(100)
-	}
-}).catch((e) => {
-	console.error(e)
-	process.exit(100)
-})
+	})
