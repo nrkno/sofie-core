1c7c2cb910ef042f7f3b9f257ef9f7a08e18ba80
Jan Starzak <jan.starzak@gmail.com>
2021-03-02 14:07:33 +0100

    chore: update expected pDevices versions

--- meteor/server/migration/1_19_0.ts
+++ meteor/server/migration/1_19_0.ts
@@ -1,4 +1,6 @@
+import { PeripheralDeviceAPI } from '../../lib/api/peripheralDevice'
 import { addMigrationSteps } from './databaseMigration'
+import { setExpectedVersion } from './lib'
 
 /*
  * **************************************************************************************
@@ -8,4 +10,23 @@ import { addMigrationSteps } from './databaseMigration'
  * **************************************************************************************
  */
 // Release 31
-export const addSteps = addMigrationSteps('1.19.0', [])
+export const addSteps = addMigrationSteps('1.19.0', [
+	setExpectedVersion(
+		'expectedVersion.playoutDevice',
+		PeripheralDeviceAPI.DeviceType.PLAYOUT,
+		'_process',
+		'1.17.0-release31.1'
+	),
+	setExpectedVersion(
+		'expectedVersion.mosDevice',
+		PeripheralDeviceAPI.DeviceType.MOS,
+		'_process',
+		'1.10.0-release31.0'
+	),
+	setExpectedVersion(
+		'expectedVersion.mediaManager',
+		PeripheralDeviceAPI.DeviceType.MEDIA_MANAGER,
+		'_process',
+		'1.6.0-release31.0'
+	),
+])
