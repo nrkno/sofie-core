d9a6acfc94589694b21cbc5e2baf116449b6c1ca
ianshade <zegzulakrzysztof@gmail.com>
2021-03-10 18:34:31 +0100

    feat: add NextInfo dashboard panel
    Shows the segment name and part title of the next part

--- meteor/lib/api/rundownLayouts.ts
+++ meteor/lib/api/rundownLayouts.ts
@@ -12,6 +12,7 @@ import {
 	PieceDisplayStyle,
 	RundownLayoutKeyboardPreview,
 	RundownLayoutPieceCountdown,
+	RundownLayoutNextInfo,
 } from '../collections/RundownLayouts'
 import { ShowStyleBaseId } from '../collections/ShowStyleBases'
 import * as _ from 'underscore'
@@ -57,6 +58,10 @@ export namespace RundownLayoutsAPI {
 
 	export function isPieceCountdown(element: RundownLayoutElementBase): element is RundownLayoutPieceCountdown {
 		return element.type === RundownLayoutElementType.PIECE_COUNTDOWN
+	}
+
+	export function isNextInfo(element: RundownLayoutElementBase): element is RundownLayoutNextInfo {
+		return element.type === RundownLayoutElementType.NEXT_INFO
 	}
 
 	export function adLibRegionToFilter(element: RundownLayoutAdLibRegion): RundownLayoutFilterBase {
