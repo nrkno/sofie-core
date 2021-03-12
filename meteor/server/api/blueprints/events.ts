ea66b109c7633db52c697f558e9d086f379b9d04
Julian Waller <git@julusian.co.uk>
2021-03-12 13:14:15 +0000

    feat: debounce timing event

--- meteor/server/api/blueprints/events.ts
+++ meteor/server/api/blueprints/events.ts
@@ -16,6 +16,7 @@ import { Studios } from '../../../lib/collections/Studios'
 import { ReadonlyDeep } from 'type-fest'
 import { asyncCollectionFindOne, asyncCollectionUpdate } from '../../lib/database'
 import { getShowStyleCompoundForRundown } from '../showStyles'
+import debounceFn, { DebouncedFunction } from 'debounce-fn'
 
 const EVENT_WAIT_TIME = 500
 
@@ -41,87 +42,105 @@ async function getBlueprintAndDependencies(rundown: ReadonlyDeep<Rundown>) {
 	}
 }
 
-function handlePartInstanceTimingEvent(playlistId: RundownPlaylistId, partInstanceId: PartInstanceId): void {
-	// TODO - debounce on partInstanceId
-	// wait EVENT_WAIT_TIME, because blueprint.onAsRunEvent() might depend on events that
-	// might havent been reported yet
-	Meteor.setTimeout(() => {
-		const span = profiler.startSpan('handlePartInstanceTimingEvent')
-		try {
-			const timestamp = getCurrentTime()
+const partInstanceTimingDebounceFunctions = new Map<string, DebouncedFunction<[], void>>()
 
-			const partInstance = PartInstances.findOne(partInstanceId)
-			if (!partInstance) throw new Meteor.Error(404, `PartInstance "${partInstanceId}" not found!`)
+function handlePartInstanceTimingEventInner(playlistId: RundownPlaylistId, partInstanceId: PartInstanceId): void {
+	const span = profiler.startSpan('handlePartInstanceTimingEvent')
+	try {
+		const timestamp = getCurrentTime()
 
-			const rundown = Rundowns.findOne(partInstance.rundownId)
-			if (!rundown) throw new Meteor.Error(404, `Rundown "${partInstance.rundownId}" not found!`)
+		const partInstance = PartInstances.findOne(partInstanceId)
+		if (!partInstance) throw new Meteor.Error(404, `PartInstance "${partInstanceId}" not found!`)
 
-			const { studio, showStyle, playlist, blueprint } = waitForPromise(getBlueprintAndDependencies(rundown))
+		const rundown = Rundowns.findOne(partInstance.rundownId)
+		if (!rundown) throw new Meteor.Error(404, `Rundown "${partInstance.rundownId}" not found!`)
 
-			if (playlist._id !== playlistId)
-				throw new Meteor.Error(
-					404,
-					`PartInstance "${partInstanceId}" does not belong to RundownPlaylist "${playlistId}"!`
-				)
+		const { studio, showStyle, playlist, blueprint } = waitForPromise(getBlueprintAndDependencies(rundown))
 
-			if (blueprint.onRundownTimingEvent) {
-				// The the PartInstances(events) before and after the one we are processing
-				const [previousPartInstance, nextPartInstance] = waitForPromiseAll([
-					asyncCollectionFindOne(
-						PartInstances,
-						{
-							rundownId: partInstance.rundownId,
-							playlistActivationId: partInstance.playlistActivationId,
-							takeCount: { $lt: partInstance.takeCount },
-						},
-						{
-							sort: {
-								// TODO - test
-								takeCount: -1,
-							},
-						}
-					),
-					asyncCollectionFindOne(
-						PartInstances,
-						{
-							rundownId: partInstance.rundownId,
-							playlistActivationId: partInstance.playlistActivationId,
-							takeCount: { $gt: partInstance.takeCount },
-						},
-						{
-							sort: {
-								// TODO - test
-								takeCount: 1,
-							},
-						}
-					),
-				])
+		if (playlist._id !== playlistId)
+			throw new Meteor.Error(
+				404,
+				`PartInstance "${partInstanceId}" does not belong to RundownPlaylist "${playlistId}"!`
+			)
 
-				const context = new AsRunPartEventContext(
+		if (blueprint.onRundownTimingEvent) {
+			// The the PartInstances(events) before and after the one we are processing
+			const [previousPartInstance, nextPartInstance] = waitForPromiseAll([
+				asyncCollectionFindOne(
+					PartInstances,
 					{
-						name: rundown.name,
-						identifier: `rundownId=${rundown._id},timestamp=${timestamp}`,
+						rundownId: partInstance.rundownId,
+						playlistActivationId: partInstance.playlistActivationId,
+						takeCount: { $lt: partInstance.takeCount },
 					},
-					studio,
-					showStyle,
-					rundown,
-					previousPartInstance,
-					partInstance,
-					nextPartInstance
-				)
-				Promise.resolve(blueprint.onRundownTimingEvent(context))
-					.then((messages: Array<IBlueprintExternalMessageQueueObj>) => {
-						queueExternalMessages(rundown, messages)
-					})
-					.catch((error) => logger.error(error))
-			}
-		} catch (e) {
-			logger.error(`handlePartInstanceTimingEvent: ${e}`)
+					{
+						sort: {
+							// TODO - test
+							takeCount: -1,
+						},
+					}
+				),
+				asyncCollectionFindOne(
+					PartInstances,
+					{
+						rundownId: partInstance.rundownId,
+						playlistActivationId: partInstance.playlistActivationId,
+						takeCount: { $gt: partInstance.takeCount },
+					},
+					{
+						sort: {
+							// TODO - test
+							takeCount: 1,
+						},
+					}
+				),
+			])
+
+			const context = new AsRunPartEventContext(
+				{
+					name: rundown.name,
+					identifier: `rundownId=${rundown._id},timestamp=${timestamp}`,
+				},
+				studio,
+				showStyle,
+				rundown,
+				previousPartInstance,
+				partInstance,
+				nextPartInstance
+			)
+			Promise.resolve(blueprint.onRundownTimingEvent(context))
+				.then((messages: Array<IBlueprintExternalMessageQueueObj>) => {
+					queueExternalMessages(rundown, messages)
+				})
+				.catch((error) => logger.error(error))
 		}
-		span?.end()
-	}, EVENT_WAIT_TIME)
+	} catch (e) {
+		logger.error(`handlePartInstanceTimingEvent: ${e}`)
+	}
+	span?.end()
 }
 
+function handlePartInstanceTimingEvent(playlistId: RundownPlaylistId, partInstanceId: PartInstanceId): void {
+	// wait EVENT_WAIT_TIME, because blueprint.onAsRunEvent() it is likely for there to be a bunch of started and stopped events coming in at the same time
+	// These blueprint methods are not time critical (meaning they do raw db operations), and can be easily delayed
+
+	const funcId = `${playlistId}_${partInstanceId}`
+	const cachedFunc = partInstanceTimingDebounceFunctions.get(funcId)
+	if (cachedFunc) {
+		cachedFunc()
+	} else {
+		const newFunc = debounceFn(
+			Meteor.bindEnvironment(() => handlePartInstanceTimingEventInner(playlistId, partInstanceId)),
+			{
+				before: false,
+				after: true,
+				wait: EVENT_WAIT_TIME,
+			}
+		)
+		partInstanceTimingDebounceFunctions.set(funcId, newFunc)
+		newFunc()
+	}
+}
 export function reportRundownDataHasChanged(playlist: ReadonlyDeep<RundownPlaylist>, rundown: ReadonlyDeep<Rundown>) {
 	// Called when the data in rundown is changed
 
