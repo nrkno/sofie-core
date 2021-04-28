1a4975548f51f80094574b6e034f4fe9bc99f39d
Balte de Wit <contact@balte.nl>
2021-04-28 09:23:17 +0200

    chore: lint fix

--- meteor/server/migration/1_33_0.ts
+++ meteor/server/migration/1_33_0.ts
@@ -4,10 +4,10 @@ import { setExpectedVersion } from './lib'
 
 // Release 33
 export const addSteps = addMigrationSteps('1.33.0', [
-    setExpectedVersion(
-        'expectedVersion.mediaManager',
-        PeripheralDeviceAPI.DeviceType.MEDIA_MANAGER,
-        '_process',
-        '1.8.0-release33.0'
-    ),
+	setExpectedVersion(
+		'expectedVersion.mediaManager',
+		PeripheralDeviceAPI.DeviceType.MEDIA_MANAGER,
+		'_process',
+		'1.8.0-release33.0'
+	),
 ])
