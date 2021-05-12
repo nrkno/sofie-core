9c3426361a7c7bf0fadd2e93815260a659ed5128
Julian Waller <git@julusian.co.uk>
2021-05-12 14:18:02 +0100

    feat: pass the ending partInstance into ShowStyleBlueprintManifest.getEndStateForPart. [publish]

--- packages/blueprints-integration/src/api.ts
+++ packages/blueprints-integration/src/api.ts
@@ -180,7 +180,7 @@ export interface ShowStyleBlueprintManifest extends BlueprintManifestBase {
 	getEndStateForPart?: (
 		context: IRundownContext,
 		previousPersistentState: TimelinePersistentState | undefined,
-		previousPartEndState: PartEndState | undefined,
+		partInstance: IBlueprintPartInstance,
 		resolvedPieces: IBlueprintResolvedPieceInstance[],
 		time: number
 	) => PartEndState
