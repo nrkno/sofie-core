d15bfc58db678dba0415e706a0c570bbe04efbd3
Julian Waller <git@julusian.co.uk>
2021-07-15 17:41:12 +0100

    fix: job timings being calculated incorrectly

--- meteor/server/worker/worker.ts
+++ meteor/server/worker/worker.ts
@@ -1,6 +1,6 @@
 import { StudioId } from '@sofie-automation/corelib/dist/dataModel/Ids'
 import { StudioJobFunc, getStudioQueueName } from '@sofie-automation/corelib/dist/worker/studio'
-import { Queue, ConnectionOptions, QueueEvents } from 'bullmq'
+import { Queue, ConnectionOptions, QueueEvents, Job } from 'bullmq'
 import { logger } from '../../lib/logging'
 import PLazy from 'p-lazy'
 import _ from 'underscore'
@@ -14,7 +14,7 @@ import _ from 'underscore'
 // type JobNames = keyof StudioJobFunc
 
 export interface JobTimings {
-	queuedTime: number
+	queueTime: number
 	startedTime: number | undefined
 	finishedTime: number | undefined
 	completedTime: number
@@ -51,18 +51,19 @@ export async function QueueStudioJob<T extends keyof StudioJobFunc>(
 	studioId: StudioId,
 	data: Parameters<StudioJobFunc[T]>[0]
 ): Promise<WorkerJob<ReturnType<StudioJobFunc[T]>>> {
-	let queue = studioQueueCache.get(studioId)
-	if (!queue) {
+	let queue0 = studioQueueCache.get(studioId)
+	if (!queue0) {
 		logger.info(`Setting up work queue for Studio "${studioId}"`)
 		const queueId = getStudioQueueName(studioId)
-		queue = [new Queue(queueId, { connection }), new QueueEvents(queueId, { connection })]
-		studioQueueCache.set(studioId, queue)
+		queue0 = [new Queue(queueId, { connection }), new QueueEvents(queueId, { connection })]
+		studioQueueCache.set(studioId, queue0)
 	}
+	const [queue, queueEvents] = queue0
 
-	const job = await queue[0].add(name, data, {
+	const queuedTime = Date.now()
+	const job = await queue.add(name, data, {
 		// priority,
 	})
-	const queuedTime = Date.now()
 
 	// TODO - timeouts
 	// TODO - better errors
@@ -71,11 +72,13 @@ export async function QueueStudioJob<T extends keyof StudioJobFunc>(
 	// Lazily watch for completion once, to be used for multiple caller promises
 	const completedPromise = PLazy.from<WrappedResult<ReturnType<StudioJobFunc[T]>>>(async () => {
 		try {
+			// TODO - this should be rewritten to manuall use queueEvents
+			// After reading the implementation, we can do that and avoid having to load the job again for getTimings
 			const res: ReturnType<StudioJobFunc[T]> = await job.waitUntilFinished(queueEvents)
 
 			return {
 				result: res,
-				queuedTime: queuedTime,
+				queueTime: queuedTime,
 				startedTime: job.processedOn,
 				finishedTime: job.finishedOn,
 				completedTime: Date.now(),
@@ -83,7 +86,7 @@ export async function QueueStudioJob<T extends keyof StudioJobFunc>(
 		} catch (e) {
 			return {
 				error: e,
-				queuedTime: queuedTime,
+				queueTime: queuedTime,
 				startedTime: job.processedOn,
 				finishedTime: job.finishedOn,
 				completedTime: Date.now(),
@@ -91,7 +94,6 @@ export async function QueueStudioJob<T extends keyof StudioJobFunc>(
 		}
 	})
 
-	const queueEvents = queue[1]
 	return {
 		complete: PLazy.from(async () => {
 			// lazily await the result
@@ -108,8 +110,18 @@ export async function QueueStudioJob<T extends keyof StudioJobFunc>(
 			// lazily await the completion
 			// TODO - should this error be re-wrapped?
 			const result = await completedPromise
+			const result2 = _.pick(result, 'queueTime', 'startedTime', 'finishedTime', 'completedTime')
 
-			return _.pick(result, 'queuedTime', 'startedTime', 'finishedTime', 'completedTime')
+			// reload the job from the queue to get the updated times
+			if (!job.id) throw new Error('No job id!')
+			const job2 = await Job.fromId(queue, job.id)
+			if (!job2) return result2
+
+			return {
+				...result2,
+				startedTime: job2.processedOn,
+				finishedTime: job2.finishedOn,
+			}
 		}),
 	}
 }
