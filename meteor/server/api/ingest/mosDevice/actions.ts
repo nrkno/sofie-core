8ab91e349292e76679c09a8a4157109e597d7d81
Johan Nyman <johan@nytamin.se>
2021-09-03 07:18:02 +0200

    fix: send PLAY before STOP story status
    This seems to give us slightly better responsiveness in ENPS
    
    (cherry picked from commit 39e7e8462caa7a77faa95db594f9ddc263ec1833)

--- meteor/server/api/ingest/mosDevice/actions.ts
+++ meteor/server/api/ingest/mosDevice/actions.ts
@@ -70,21 +70,35 @@ export namespace MOSDeviceActions {
 		newPlayingPartExternalId: string | null
 	) {
 		if (oldPlayingPartExternalId !== newPlayingPartExternalId) {
-			if (oldPlayingPartExternalId) {
-				setStoryStatus(
-					peripheralDevice._id,
-					rundown,
-					oldPlayingPartExternalId,
-					MOS.IMOSObjectStatus.STOP
-				).catch((e) => logger.error('Error in setStoryStatus', e))
-			}
+			// Note: We send the PLAY first, since that seems to give us _slightly_ better responsiveness in ENPS.
+
 			if (newPlayingPartExternalId) {
-				setStoryStatus(
-					peripheralDevice._id,
-					rundown,
-					newPlayingPartExternalId,
-					MOS.IMOSObjectStatus.PLAY
-				).catch((e) => logger.error('Error in setStoryStatus', e))
+				try {
+					waitForPromise(
+						setStoryStatus(
+							peripheralDevice._id,
+							rundown,
+							newPlayingPartExternalId,
+							MOS.IMOSObjectStatus.PLAY
+						)
+					)
+				} catch (error) {
+					logger.error('Error in setStoryStatus PLAY', error)
+				}
+			}
+			if (oldPlayingPartExternalId) {
+				try {
+					waitForPromise(
+						setStoryStatus(
+							peripheralDevice._id,
+							rundown,
+							oldPlayingPartExternalId,
+							MOS.IMOSObjectStatus.STOP
+						)
+					)
+				} catch (error) {
+					logger.error('Error in setStoryStatus STOP', error)
+				}
 			}
 		}
 	}
