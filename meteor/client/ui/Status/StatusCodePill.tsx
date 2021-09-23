8b62c2798e0f349f3261dfe645f8542fdefc82ab
Johan Nyman <johan@nytamin.se>
2021-09-23 10:18:40 +0200

    chore: refactor classes into functional components with react hooks

--- meteor/client/ui/Status/StatusCodePill.tsx
+++ meteor/client/ui/Status/StatusCodePill.tsx
@@ -1,62 +1,55 @@
 import * as React from 'react'
-import * as reacti18next from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { PeripheralDeviceAPI } from '../../../lib/api/peripheralDevice'
 import { assertNever } from '../../../lib/lib'
 import ClassNames from 'classnames'
 import { StatusCode } from '@sofie-automation/blueprints-integration'
 import { statusCodeToString } from './SystemStatus'
 
-interface StatusCodePillProps {
+export const StatusCodePill: React.FC<{
 	connected: boolean
 	statusCode: StatusCode
 	messages?: string[]
-}
-export const StatusCodePill = reacti18next.withTranslation()(
-	class StatusCodePill extends React.Component<StatusCodePillProps & reacti18next.WithTranslation, {}> {
-		constructor(props: StatusCodePillProps & reacti18next.WithTranslation) {
-			super(props)
-		}
-		statusCodeString() {
-			const { t } = this.props
+}> = function ExpectedPackagesStatus(props) {
+	const { t } = useTranslation()
 
-			return this.props.connected ? statusCodeToString(t, this.props.statusCode) : t('Not Connected')
-		}
-		statusMessages() {
-			const messages = this.props.messages || []
-			return messages.length ? '"' + messages.join(', ') + '"' : ''
-		}
-		getStatusClassName(): string {
-			if (!this.props.connected) return 'device-status--unknown'
+	function statusCodeString() {
+		return props.connected ? statusCodeToString(t, props.statusCode) : t('Not Connected')
+	}
+	function statusMessages() {
+		const messages = props.messages || []
+		return messages.length ? '"' + messages.join(', ') + '"' : ''
+	}
+	function getStatusClassName(): string {
+		if (!props.connected) return 'device-status--unknown'
 
-			switch (this.props.statusCode) {
-				case PeripheralDeviceAPI.StatusCode.UNKNOWN:
-					return 'device-status--unknown'
-				case PeripheralDeviceAPI.StatusCode.GOOD:
-					return 'device-status--good'
-				case PeripheralDeviceAPI.StatusCode.WARNING_MINOR:
-					return 'device-status--minor-warning'
-				case PeripheralDeviceAPI.StatusCode.WARNING_MAJOR:
-					return 'device-status--warning'
-				case PeripheralDeviceAPI.StatusCode.BAD:
-					return 'device-status--bad'
-				case PeripheralDeviceAPI.StatusCode.FATAL:
-					return 'device-status--fatal'
-				default:
-					assertNever(this.props.statusCode)
-					return 'unknown-' + this.props.statusCode
-			}
-		}
-		render() {
-			return (
-				<div className={ClassNames('device-status', this.getStatusClassName())}>
-					<div className="value">
-						<span className="pill device-status__label">{this.statusCodeString()}</span>
-					</div>
-					<div className="device-item__device-status-message">
-						<span className="text-s dimmed">{this.statusMessages()}</span>
-					</div>
-				</div>
-			)
+		switch (props.statusCode) {
+			case PeripheralDeviceAPI.StatusCode.UNKNOWN:
+				return 'device-status--unknown'
+			case PeripheralDeviceAPI.StatusCode.GOOD:
+				return 'device-status--good'
+			case PeripheralDeviceAPI.StatusCode.WARNING_MINOR:
+				return 'device-status--minor-warning'
+			case PeripheralDeviceAPI.StatusCode.WARNING_MAJOR:
+				return 'device-status--warning'
+			case PeripheralDeviceAPI.StatusCode.BAD:
+				return 'device-status--bad'
+			case PeripheralDeviceAPI.StatusCode.FATAL:
+				return 'device-status--fatal'
+			default:
+				assertNever(props.statusCode)
+				return 'unknown-' + props.statusCode
 		}
 	}
-)
+
+	return (
+		<div className={ClassNames('device-status', getStatusClassName())}>
+			<div className="value">
+				<span className="pill device-status__label">{statusCodeString()}</span>
+			</div>
+			<div className="device-item__device-status-message">
+				<span className="text-s dimmed">{statusMessages()}</span>
+			</div>
+		</div>
+	)
+}
--- meteor/client/ui/Status/package-status/PackageContainerStatus.tsx
+++ meteor/client/ui/Status/package-status/PackageContainerStatus.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react'
-import { Translated } from '../../../lib/ReactMeteorData/react-meteor-data'
+import React from 'react'
 import Tooltip from 'rc-tooltip'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { PackageContainerStatusDB } from '../../../../lib/collections/PackageContainerStatus'
 import { StatusCodePill } from '../StatusCodePill'
 import { doUserAction, UserAction } from '../../../lib/userAction'
@@ -11,66 +10,111 @@ interface IPackageContainerStatusProps {
 	packageContainerStatus: PackageContainerStatusDB
 }
 
-export const PackageContainerStatus = withTranslation()(
-	class PackageContainerStatus extends React.Component<Translated<IPackageContainerStatusProps>, {}> {
-		constructor(props) {
-			super(props)
+export const PackageContainerStatus: React.FC<IPackageContainerStatusProps> = function PackageContainerStatus({
+	packageContainerStatus,
+}: IPackageContainerStatusProps) {
+	const { t } = useTranslation()
 
-			this.state = {}
-		}
-
-		restartPackageContainer(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-			doUserAction(this.props.t, e, UserAction.PACKAGE_MANAGER_RESTART_PACKAGE_CONTAINER, (e) =>
-				MeteorCall.userAction.packageManagerRestartPackageContainer(
-					e,
-					this.props.packageContainerStatus.deviceId,
-					this.props.packageContainerStatus.containerId
-				)
+	function restartPackageContainer(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+		doUserAction(t, e, UserAction.PACKAGE_MANAGER_RESTART_PACKAGE_CONTAINER, (e) =>
+			MeteorCall.userAction.packageManagerRestartPackageContainer(
+				e,
+				packageContainerStatus.deviceId,
+				packageContainerStatus.containerId
 			)
-		}
+		)
+	}
 
-		render() {
-			const { t } = this.props
-			const packageContainerStatus = this.props.packageContainerStatus
-
-			return (
-				<>
-					<tr className="packageContainer">
-						<td></td>
-						<td>{packageContainerStatus.containerId}</td>
+	return (
+		<>
+			<tr className="packageContainer">
+				<td className="indent"></td>
+				<td>{packageContainerStatus.containerId}</td>
+				<td>
+					<StatusCodePill connected={true} statusCode={packageContainerStatus.status.status} />
+				</td>
+				<td>
+					<Tooltip overlay={packageContainerStatus.status.statusReason.tech} placement="top">
+						<span>{packageContainerStatus.status.statusReason.user}</span>
+					</Tooltip>
+				</td>
+				<td>
+					<button className="btn btn-secondary mls" onClick={(e) => restartPackageContainer(e)}>
+						{t('Restart')}
+					</button>
+				</td>
+			</tr>
+			{Object.entries(packageContainerStatus.status.monitors).map(([monitorId, monitor]) => {
+				return (
+					<tr className="packageContainer-monitor" key={monitorId}>
+						<td className="indent"></td>
+						<td>{monitorId}</td>
 						<td>
-							<StatusCodePill connected={true} statusCode={packageContainerStatus.status.status} />
+							<StatusCodePill connected={true} statusCode={monitor.status} />
 						</td>
 						<td>
-							<Tooltip overlay={packageContainerStatus.status.statusReason.tech} placement="top">
-								<span>{packageContainerStatus.status.statusReason.user}</span>
+							<Tooltip overlay={monitor.statusReason.tech} placement="top">
+								<span>{monitor.statusReason.user}</span>
 							</Tooltip>
 						</td>
-						<td>
-							<button className="btn btn-secondary mls" onClick={(e) => this.restartPackageContainer(e)}>
-								{t('Restart')}
-							</button>
-						</td>
+						<td></td>
 					</tr>
-					{Object.entries(packageContainerStatus.status.monitors).map(([monitorId, monitor]) => {
-						return (
-							<tr className="packageContainer-monitor" key={monitorId}>
-								<td></td>
-								<td>{monitorId}</td>
-								<td>
-									<StatusCodePill connected={true} statusCode={monitor.status} />
-								</td>
-								<td>
-									<Tooltip overlay={monitor.statusReason.tech} placement="top">
-										<span>{monitor.statusReason.user}</span>
-									</Tooltip>
-								</td>
-								<td></td>
-							</tr>
-						)
-					})}
-				</>
-			)
-		}
-	}
-)
+				)
+			})}
+		</>
+	)
+}
+
+// export const OLDPackageContainerStatus = withTranslation()(
+// 	class PackageContainerStatus extends React.Component<Translated<IPackageContainerStatusProps>, {}> {
+// 		constructor(props) {
+// 			super(props)
+
+// 			this.state = {}
+// 		}
+
+// 		render() {
+// 			const { t } = this.props
+// 			const packageContainerStatus = this.props.packageContainerStatus
+
+// 			return (
+// 				<>
+// 					<tr className="packageContainer">
+// 						<td></td>
+// 						<td>{packageContainerStatus.containerId}</td>
+// 						<td>
+// 							<StatusCodePill connected={true} statusCode={packageContainerStatus.status.status} />
+// 						</td>
+// 						<td>
+// 							<Tooltip overlay={packageContainerStatus.status.statusReason.tech} placement="top">
+// 								<span>{packageContainerStatus.status.statusReason.user}</span>
+// 							</Tooltip>
+// 						</td>
+// 						<td>
+// 							<button className="btn btn-secondary mls" onClick={(e) => this.restartPackageContainer(e)}>
+// 								{t('Restart')}
+// 							</button>
+// 						</td>
+// 					</tr>
+// 					{Object.entries(packageContainerStatus.status.monitors).map(([monitorId, monitor]) => {
+// 						return (
+// 							<tr className="packageContainer-monitor" key={monitorId}>
+// 								<td></td>
+// 								<td>{monitorId}</td>
+// 								<td>
+// 									<StatusCodePill connected={true} statusCode={monitor.status} />
+// 								</td>
+// 								<td>
+// 									<Tooltip overlay={monitor.statusReason.tech} placement="top">
+// 										<span>{monitor.statusReason.user}</span>
+// 									</Tooltip>
+// 								</td>
+// 								<td></td>
+// 							</tr>
+// 						)
+// 					})}
+// 				</>
+// 			)
+// 		}
+// 	}
+// )
