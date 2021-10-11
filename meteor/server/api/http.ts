5ab3f9e9ec23be20dc520547ab34b891ac457c27
Johan Nyman <johan@nytamin.se>
2021-10-11 10:38:29 +0200

    fix: increase upload file limit

--- meteor/server/api/http.ts
+++ meteor/server/api/http.ts
@@ -7,19 +7,19 @@ export const PickerPOST = Picker.filter((req) => req.method === 'POST')
 
 PickerPOST.middleware(
 	bodyParser.json({
-		limit: '50mb', // Arbitrary limit
+		limit: '100mb', // Arbitrary limit
 	})
 )
 PickerPOST.middleware(
 	bodyParser.text({
 		type: 'application/json',
-		limit: '50mb',
+		limit: '100mb',
 	})
 )
 PickerPOST.middleware(
 	bodyParser.text({
 		type: 'text/javascript',
-		limit: '50mb',
+		limit: '100mb',
 	})
 )
 
