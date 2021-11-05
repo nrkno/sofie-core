70b20da27c9f59ea5fd5c65715a0937ad29570cf
Tom Lee <thomasslee97@gmail.com>
2021-11-05 14:11:29 +0000

    fix: Failing rundownPlaylist test

--- meteor/__mocks__/helpers/database.ts
+++ meteor/__mocks__/helpers/database.ts
@@ -479,7 +479,7 @@ export function setupDefaultRundown(
 		_rank: 0,
 
 		_id: rundownId,
-		externalId: 'MOCK_RUNDOWN',
+		externalId: unprotectString(rundownId),
 		name: 'Default Rundown',
 
 		created: getCurrentTime(),
--- meteor/server/api/__tests__/rundownPlaylist.test.ts
+++ meteor/server/api/__tests__/rundownPlaylist.test.ts
@@ -12,6 +12,7 @@ import { Rundowns, Rundown } from '../../../lib/collections/Rundowns'
 import { RundownPlaylists, RundownPlaylist } from '../../../lib/collections/RundownPlaylists'
 import { produceRundownPlaylistInfoFromRundown, updateRundownsInPlaylist } from '../rundownPlaylist'
 import { DbCacheWriteCollection } from '../../cache/CacheCollection'
+import { PlaylistTimingType } from '@sofie-automation/blueprints-integration'
 
 require('../client') // include in order to create the Meteor methods needed
 require('../rundown') // include in order to create the Meteor methods needed
@@ -52,13 +53,25 @@ describe('Rundown', () => {
 		expect(rundownId02).toEqual('rundown02')
 		// The setupDefaultRundown doesn't set _rank, set expectedStart so that they will get a default order from that:
 		Rundowns.update(rundownId00, {
-			$set: { playlistId: playlistId0, playlistIdIsSetInSofie: true, expectedStart: 1000 },
+			$set: {
+				playlistId: playlistId0,
+				playlistIdIsSetInSofie: true,
+				timing: { type: PlaylistTimingType.ForwardTime, expectedStart: 1000 },
+			},
 		})
 		Rundowns.update(rundownId01, {
-			$set: { playlistId: playlistId0, playlistIdIsSetInSofie: true, expectedStart: 2000 },
+			$set: {
+				playlistId: playlistId0,
+				playlistIdIsSetInSofie: true,
+				timing: { type: PlaylistTimingType.ForwardTime, expectedStart: 2000 },
+			},
 		})
 		Rundowns.update(rundownId02, {
-			$set: { playlistId: playlistId0, playlistIdIsSetInSofie: true, expectedStart: 3000 },
+			$set: {
+				playlistId: playlistId0,
+				playlistIdIsSetInSofie: true,
+				timing: { type: PlaylistTimingType.ForwardTime, expectedStart: 3000 },
+			},
 		})
 
 		const rundown00 = Rundowns.findOne(rundownId00) as Rundown
