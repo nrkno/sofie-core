c30ade3862497f570a24c1111917a336b4128928
Julian Waller <julian@superfly.tv>
2021-11-11 10:18:46 +0000

    feat: timeline and mappings test tools to view routed timeline and mappings (#602)

--- meteor/server/publications/timeline.ts
+++ meteor/server/publications/timeline.ts
@@ -1,14 +1,15 @@
 import { Meteor } from 'meteor/meteor'
-import { Timeline, getRoutedTimeline, TimelineComplete } from '../../lib/collections/Timeline'
+import { Timeline, getRoutedTimeline, TimelineComplete, RoutedTimeline } from '../../lib/collections/Timeline'
 import { meteorPublish } from './lib'
 import { PubSub } from '../../lib/api/pubsub'
 import { FindOptions } from '../../lib/typings/meteor'
-import { meteorCustomPublishArray } from '../lib/customPublication'
+import { CustomPublishArray, meteorCustomPublishArray } from '../lib/customPublication'
 import { setUpOptimizedObserver } from '../lib/optimizedObserver'
 import { PeripheralDeviceId, PeripheralDevices } from '../../lib/collections/PeripheralDevices'
 import { Studios, getActiveRoutes, StudioId } from '../../lib/collections/Studios'
 import { PeripheralDeviceReadAccess } from '../security/peripheralDevice'
 import { StudioReadAccess } from '../security/studio'
+import { literal } from '../../lib/lib'
 
 meteorPublish(PubSub.timeline, function (selector, token) {
 	if (!selector) throw new Meteor.Error(400, 'selector argument missing')
@@ -21,7 +22,7 @@ meteorPublish(PubSub.timeline, function (selector, token) {
 	return null
 })
 
-meteorCustomPublishArray(
+meteorCustomPublishArray<RoutedTimeline>(
 	PubSub.timelineForDevice,
 	'studioTimeline',
 	function (pub, deviceId: PeripheralDeviceId, token) {
@@ -35,71 +36,90 @@ meteorCustomPublishArray(
 			const studioId = peripheralDevice.studioId
 			if (!studioId) return []
 
-			const observer = setUpOptimizedObserver(
-				`pub_${PubSub.timelineForDevice}_${studioId}`,
-				(triggerUpdate) => {
-					// Set up observers:
-					return [
-						Studios.find(studioId, {
-							fields: {
-								// It should be enough to watch the mappingsHash, since that should change whenever there is a
-								// change to the mappings or the routes
-								mappingsHash: 1,
-							},
-						}).observe({
-							added: () => triggerUpdate({ studioId: studioId }),
-							changed: () => triggerUpdate({ studioId: studioId }),
-							removed: () => triggerUpdate({ studioId: null }),
-						}),
-						Timeline.find({
-							_id: studioId,
-						}).observe({
-							added: (timeline) => triggerUpdate({ timeline: timeline }),
-							changed: (timeline) => triggerUpdate({ timeline: timeline }),
-							removed: () => triggerUpdate({ timeline: null }),
-						}),
-					]
-				},
-				() => {
-					// Initial data fetch
-					return {
-						timeline: Timeline.findOne({
-							_id: studioId,
-						}),
-						studioId: studioId,
-					}
-				},
-				(newData: { studioId: StudioId | undefined; timeline: TimelineComplete | undefined }) => {
-					// Prepare data for publication:
-
-					if (!newData.studioId || !newData.timeline) {
-						return []
-					} else {
-						const studio = Studios.findOne(newData.studioId)
-						if (!studio) return []
-
-						const routes = getActiveRoutes(studio)
-						const routedTimeline = getRoutedTimeline(newData.timeline.timeline, routes)
-
-						return [
-							{
-								_id: newData.timeline._id,
-								mappingsHash: studio.mappingsHash,
-								timelineHash: newData.timeline.timelineHash,
-								timeline: routedTimeline,
-								generated: newData.timeline.generated,
-								published: Date.now(),
-							},
-						]
-					}
-				},
-				(newData) => {
-					pub.updatedDocs(newData)
-				}
-			)
-			pub.onStop(() => {
-				observer.stop()
-			})
+			createObserverForTimelinePublication(pub, PubSub.timelineForDevice, studioId)
 		}
 	}
 )
+
+meteorCustomPublishArray<RoutedTimeline>(
+	PubSub.timelineForStudio,
+	'studioTimeline',
+	function (pub, studioId: StudioId, token) {
+		if (StudioReadAccess.studio({ _id: studioId }, { userId: this.userId, token })) {
+			createObserverForTimelinePublication(pub, PubSub.timelineForStudio, studioId)
+		}
+	}
+)
+
+/** Create an observer for each publication, to simplify the stop conditions */
+function createObserverForTimelinePublication(
+	pub: CustomPublishArray<RoutedTimeline>,
+	observerId: PubSub,
+	studioId: StudioId
+) {
+	const observer = setUpOptimizedObserver(
+		`pub_${observerId}_${studioId}`,
+		(triggerUpdate) => {
+			// Set up observers:
+			return [
+				Studios.find(studioId, {
+					fields: {
+						// It should be enough to watch the mappingsHash, since that should change whenever there is a
+						// change to the mappings or the routes
+						mappingsHash: 1,
+					},
+				}).observe({
+					added: () => triggerUpdate({ studioId: studioId }),
+					changed: () => triggerUpdate({ studioId: studioId }),
+					removed: () => triggerUpdate({ studioId: null }),
+				}),
+				Timeline.find({
+					_id: studioId,
+				}).observe({
+					added: (timeline) => triggerUpdate({ timeline: timeline }),
+					changed: (timeline) => triggerUpdate({ timeline: timeline }),
+					removed: () => triggerUpdate({ timeline: null }),
+				}),
+			]
+		},
+		() => {
+			// Initial data fetch
+			return {
+				timeline: Timeline.findOne({
+					_id: studioId,
+				}),
+				studioId: studioId,
+			}
+		},
+		(newData: { studioId: StudioId | undefined; timeline: TimelineComplete | undefined }) => {
+			// Prepare data for publication:
+
+			if (!newData.studioId || !newData.timeline) {
+				return []
+			} else {
+				const studio = Studios.findOne(newData.studioId)
+				if (!studio) return []
+
+				const routes = getActiveRoutes(studio)
+				const routedTimeline = getRoutedTimeline(newData.timeline.timeline, routes)
+
+				return [
+					literal<RoutedTimeline>({
+						_id: newData.timeline._id,
+						mappingsHash: studio.mappingsHash,
+						timelineHash: newData.timeline.timelineHash,
+						timeline: routedTimeline,
+						generated: newData.timeline.generated,
+						published: Date.now(),
+					}),
+				]
+			}
+		},
+		(newData) => {
+			pub.updatedDocs(newData)
+		}
+	)
+	pub.onStop(() => {
+		observer.stop()
+	})
+}
