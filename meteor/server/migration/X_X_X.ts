3158998ec51fc9840c7d4d091bde40401443ad9b
Tom Lee <thomasslee97@gmail.com>
2021-12-06 10:45:21 +0000

    fix: Migration for existing core actions

--- meteor/server/migration/X_X_X.ts
+++ meteor/server/migration/X_X_X.ts
@@ -1,5 +1,7 @@
 import { addMigrationSteps } from './databaseMigration'
 import { CURRENT_SYSTEM_VERSION } from './currentSystemVersion'
+import { TriggeredActions } from '../../lib/collections/TriggeredActions'
+import { getHash, protectString, unprotectString } from '../../lib/lib'
 
 /*
  * **************************************************************************************
@@ -13,4 +15,25 @@ import { CURRENT_SYSTEM_VERSION } from './currentSystemVersion'
 // Release X
 export const addSteps = addMigrationSteps(CURRENT_SYSTEM_VERSION, [
 	// Add some migrations!
+	{
+		id: `TriggeredActions.core.fixIds`,
+		canBeRunAutomatically: true,
+		validate: () => {
+			const existingActions = TriggeredActions.find({ showStyleBaseId: null }).fetch()
+			return existingActions.some((action) => !!unprotectString(action._id).match(/^core_/))
+		},
+		migrate: () => {
+			const existingActions = TriggeredActions.find({ showStyleBaseId: null }).fetch()
+			for (const action of existingActions) {
+				let actionId = unprotectString(action._id)
+				if (!!actionId.match(/^core_/)) {
+					TriggeredActions.remove(action._id)
+					TriggeredActions.insert({
+						...action,
+						_id: protectString(getHash(actionId)),
+					})
+				}
+			}
+		},
+	},
 ])
