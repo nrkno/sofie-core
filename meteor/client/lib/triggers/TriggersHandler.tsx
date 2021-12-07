1a1d51a25773c7a3f595673b4ff4c0fdd949211a
Jan Starzak <jan.starzak@gmail.com>
2021-12-07 09:38:51 +0100

    chore: remove leftover console.log

--- meteor/client/lib/triggers/TriggersHandler.tsx
+++ meteor/client/lib/triggers/TriggersHandler.tsx
@@ -455,7 +455,6 @@ export const TriggersHandler: React.FC<IProps> = function TriggersHandler(
 					})
 
 					return () => {
-						console.log('Preview cleanup!')
 						MountedAdLibTriggers.remove({
 							triggeredActionId: pair._id,
 						})
