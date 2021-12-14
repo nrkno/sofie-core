9789971a23a69659b19683cf2b8a58b7999a6e9f
Jan Starzak <jan.starzak@gmail.com>
2021-12-14 14:19:23 +0100

    chore: refactor some SegmentTimelineComponents into individual files

---
+++ meteor/client/ui/SegmentTimeline/SegmentTimelineZoomButtons.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { ZoomInIcon, ZoomOutIcon, ZoomShowAll } from '../../lib/segmentZoomIcon'
+
+interface IProps {
+	timeScale: number
+	maxTimeScale: number
+	isLiveSegment: boolean
+	scrollLeft: number
+	onScroll: (scrollLeft: number, e: React.MouseEvent<HTMLElement>) => void
+	onShowEntireSegment?: (e: React.MouseEvent<HTMLElement>) => void
+	onZoomChange: (zoomLevel: number, e: React.MouseEvent<HTMLElement>) => void
+	onRecalculateMaxTimeScale: () => Promise<number>
+}
+
+export function SegmentTimelineZoomButtons(props: IProps) {
+	const zoomIn = (e: React.MouseEvent<HTMLElement>) => {
+		props.onZoomChange(props.timeScale * 2, e)
+	}
+
+	const zoomOut = (e: React.MouseEvent<HTMLElement>) => {
+		// if the segment is live, the maxTimeScale will not be recalculated while it's being played back, because
+		// that would require layout trashing the page on every timing tick. Instead, we cause the maxTimeScale to
+		// be recalculated manually, when the user actually interacts with the zoom
+		if (props.isLiveSegment) {
+			e.persist()
+			props
+				.onRecalculateMaxTimeScale()
+				.then((maxTimeScale) => zoomOutInner(maxTimeScale, e))
+				.catch(console.error)
+		} else {
+			zoomOutInner(props.maxTimeScale, e)
+		}
+	}
+
+	const zoomOutInner = (maxTimeScale: number, e: React.MouseEvent<HTMLElement>) => {
+		const targetTimeScale = Math.max(props.timeScale * 0.5, maxTimeScale)
+		props.onZoomChange(targetTimeScale, e)
+		if (targetTimeScale === maxTimeScale && !props.isLiveSegment) {
+			props.onScroll(0, e)
+		}
+	}
+
+	const zoomNormalize = (e: React.MouseEvent<HTMLElement>) => {
+		props.onShowEntireSegment && props.onShowEntireSegment(e)
+		if (!props.isLiveSegment && props.scrollLeft > 0) {
+			props.onScroll(0, e)
+		}
+	}
+
+	return (
+		<div className="segment-timeline__timeline-zoom-buttons">
+			<button
+				className="segment-timeline__timeline-zoom-buttons__button segment-timeline__timeline-zoom-buttons__button--out"
+				onClick={zoomOut}
+				disabled={props.timeScale <= props.maxTimeScale && !props.isLiveSegment}
+			>
+				<ZoomOutIcon />
+			</button>
+			<button
+				className="segment-timeline__timeline-zoom-buttons__button segment-timeline__timeline-zoom-buttons__button--all"
+				onClick={zoomNormalize}
+			>
+				<ZoomShowAll />
+			</button>
+			<button
+				className="segment-timeline__timeline-zoom-buttons__button segment-timeline__timeline-zoom-buttons__button--in"
+				onClick={zoomIn}
+			>
+				<ZoomInIcon />
+			</button>
+		</div>
+	)
+}
