74aa80d58de5afdea3f6bd87cb2f4b286a18dfa9
Johan Nyman <johan@nytamin.se>
2022-01-18 12:23:56 +0100

    chore: minor lint & fixes

--- meteor/server/api/profiler.ts
+++ meteor/server/api/profiler.ts
@@ -3,8 +3,16 @@
 	See https://github.com/Meteor-Community-Packages/meteor-elastic-apm/pull/61
 	So instead I've just added a temporary black-hole replacement for now /Johan Nyman 2022-01-18
 */
+type StartSpan = (name: string) => undefined | { end: Function; addLabels: Function }
+type StartTransaction = (
+	description: string,
+	name: string
+) => undefined | { end: Function; addLabels: Function; startSpan: StartSpan }
 
-const Agent = {
+const Agent: {
+	startSpan: StartSpan
+	startTransaction: StartTransaction
+} = {
 	startSpan: (_name: string) => {
 		return undefined
 	},
