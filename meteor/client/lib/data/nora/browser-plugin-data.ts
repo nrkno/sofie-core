a287acdd0b0488bc70212d6ded51d8b4d229e19b
Ola Christian Gunndelsby <ola.christian.gundelsby@nrk.no>
2022-02-16 17:23:09 +0100

    fix: actually support attributes in client/lib/data/objectToXml

--- meteor/client/lib/data/nora/browser-plugin-data.ts
+++ meteor/client/lib/data/nora/browser-plugin-data.ts
@@ -1,4 +1,5 @@
 import { IBlueprintPieceGeneric, NoraContent } from '@sofie-automation/blueprints-integration'
+import { objectToXML } from '../util/object-to-xml'
 
 export { createMosObjectXmlStringNoraBluePrintPiece }
 
@@ -53,49 +54,4 @@ function createMosObjectXmlStringNoraBluePrintPiece(piece: IBlueprintPieceGeneri
 	)
 
 	return new XMLSerializer().serializeToString(doc)
-}
-
-function objectToXML(obj: object, rootName: string): Document {
-	const doc = new Document()
-	const root = doc.createElement(rootName)
-
-	addNodes(obj, root)
-
-	doc.appendChild(root)
-	return doc
-}
-
-function addNodes(obj: object, rootNode: Node): void {
-	const doc = rootNode.ownerDocument
-	if (!doc) {
-		// this should never happen, given that this is an internal function
-		// and that the rootNode is explicitly created from a document in
-		// both objectToXML() and createNode(), who are the only callers of
-		// this function
-		throw new Error('Root node has no owner document.')
-	}
-
-	for (const name of Object.keys(obj)) {
-		const value = obj[name]
-
-		if (Array.isArray(value)) {
-			value.forEach((element) => {
-				rootNode.appendChild(createNode(name, element, doc))
-			})
-		} else {
-			rootNode.appendChild(createNode(name, value, doc))
-		}
-	}
-}
-
-function createNode(name: string, value: any, doc: Document) {
-	const node = doc.createElement(name)
-
-	if (typeof value === 'object' && value !== null) {
-		addNodes(value, node)
-	} else {
-		node.textContent = value
-	}
-
-	return node
 }
