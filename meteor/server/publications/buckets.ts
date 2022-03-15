caa7aee6d66a70ee6c68c10d75c377caf9f9604a
Johan Nyman <johan@nytamin.se>
2022-03-15 13:01:59 +0100

    fix: make bucket publication great again

--- meteor/server/publications/buckets.ts
+++ meteor/server/publications/buckets.ts
@@ -8,13 +8,19 @@ import { Buckets, Bucket } from '../../lib/collections/Buckets'
 import { BucketAdLibs, BucketAdLib } from '../../lib/collections/BucketAdlibs'
 import { FindOptions } from '../../lib/typings/meteor'
 import { BucketAdLibActions, BucketAdLibAction } from '../../lib/collections/BucketAdlibActions'
+import { StudioReadAccess } from '../security/studio'
 
 meteorPublish(PubSub.buckets, function (selector: MongoQuery<Bucket>, _token) {
 	if (!selector) throw new Meteor.Error(400, 'selector argument missing')
 	const modifier: FindOptions<Bucket> = {
 		fields: {},
 	}
-	if (typeof selector._id === 'string' && BucketSecurity.allowReadAccess(this, selector._id)) {
+	if (
+		(typeof selector.studioId === 'string' &&
+			selector.studioId &&
+			StudioReadAccess.studioContent(selector, this)) ||
+		(typeof selector._id === 'string' && selector._id && BucketSecurity.allowReadAccess(this, selector._id))
+	) {
 		return Buckets.find(selector, modifier)
 	}
 	return null
