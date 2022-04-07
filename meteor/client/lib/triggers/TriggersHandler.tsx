b90f1b3fa168b6d9ee72eac4a947fa5dc79fbbf3
Anders Frederik Jørgensen <afjo@tv2.dk>
2022-04-07 09:53:10 +0200

    fix: Cherry-picked Sorensen preventDefaultDown option.
    To prevent default behaviour on keyDown for up-bound triggers.

--- meteor/client/lib/triggers/TriggersHandler.tsx
+++ meteor/client/lib/triggers/TriggersHandler.tsx
@@ -211,6 +211,7 @@ export const TriggersHandler: React.FC<IProps> = function TriggersHandler(
 				exclusive: true,
 				ordered: 'modifiersFirst',
 				preventDefaultPartials: false,
+				preventDefaultDown: true,
 				global: props.global ?? false,
 				tag: id,
 			})
