d46d13cc6beddde75378e7f46570c6a0c3491f88
Ivan Maretić <37489015+imaretic@users.noreply.github.com>
2022-04-07 17:05:16 +0200

    chore: update MOS-gateway to use sofie-code-standard-preset (#706)

--- packages/mos-gateway/src/integrationTests/index.spec.ts
+++ packages/mos-gateway/src/integrationTests/index.spec.ts
@@ -2,35 +2,41 @@ import { Connector } from '../connector'
 import * as Winston from 'winston'
 
 test('Simple test', async () => {
-	let c: Connector
-
-	let logger = new Winston.Logger({
-		transports: [new Winston.transports.Console()]
+	const logger = Winston.createLogger({
+		transports: [new Winston.transports.Console()],
 	})
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	logger.info = console.log
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	logger.debug = console.log
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	logger.error = console.log
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	logger.warn = console.log
 
-	c = new Connector(logger)
+	const c: Connector = new Connector(logger)
 
 	await c.init({
 		core: {
 			host: '127.0.0.1',
 			port: 3000,
-			watchdog: false
+			watchdog: false,
 		},
 		device: {
 			deviceId: 'JestTest',
-			deviceToken: '1234'
+			deviceToken: '1234',
 		},
 		process: {
 			unsafeSSL: true,
-			certificates: []
+			certificates: [],
 		},
 		mos: {
 			self: {
@@ -45,11 +51,11 @@ test('Simple test', async () => {
 					'4': false,
 					'5': false,
 					'6': false,
-					'7': false
-				}
-			}
+					'7': false,
+				},
+			},
 			// devices: []
-		}
+		},
 	})
 
 	expect(c).toBeInstanceOf(Connector)
