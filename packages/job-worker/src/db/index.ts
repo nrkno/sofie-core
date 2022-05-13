ed96de585a6e8155b513318c9882a58fa405dd81
Julian Waller <git@julusian.co.uk>
2022-05-13 10:45:32 +0100

    fix: mongodb shouldnt convert undefined properties to null

--- packages/job-worker/src/db/index.ts
+++ packages/job-worker/src/db/index.ts
@@ -4,7 +4,9 @@ export * from './collections'
 export * from './collection'
 
 export async function createMongoConnection(mongoUri: string): Promise<MongoClient> {
-	const client = new MongoClient(mongoUri)
+	const client = new MongoClient(mongoUri, {
+		ignoreUndefined: true,
+	})
 	await client.connect()
 	return client
 }
