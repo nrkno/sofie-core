a54fe164fe3f954761d2f682c04dc86a53158ce0
Jan Starzak <jan.starzak@gmail.com>
2022-07-06 15:12:04 +0200

    feat(Segment List): show multiple Pieces in Indicators as stacked

--- meteor/client/ui/SegmentList/LinePartPieceIndicator/LinePartPieceIndicator.tsx
+++ meteor/client/ui/SegmentList/LinePartPieceIndicator/LinePartPieceIndicator.tsx
@@ -17,7 +17,13 @@ export const LinePartPieceIndicator: React.FC<IProps> = function LinePartPieceIn
 }) {
 	const sourceLayerIds = useMemo(() => sourceLayers.map((layer) => layer._id), [sourceLayers])
 	const thisPieces = useMemo(
-		() => pieces.filter((piece) => piece.sourceLayer && sourceLayerIds.includes(piece.sourceLayer._id)),
+		() =>
+			pieces.filter(
+				(piece) =>
+					piece.sourceLayer &&
+					sourceLayerIds.includes(piece.sourceLayer._id) &&
+					(piece.renderedDuration === null || piece.renderedDuration > 0)
+			),
 		[pieces, sourceLayerIds]
 	)
 
@@ -30,19 +36,29 @@ export const LinePartPieceIndicator: React.FC<IProps> = function LinePartPieceIn
 	return (
 		<Tooltip
 			overlay={
-				label +
-				': ' +
-				(thisPieces.length === 0 ? 'Not present' : thisPieces.map((piece) => piece.instance.piece.name).join(', '))
+				<>
+					<b>{label}</b>:{' '}
+					{thisPieces.length === 0 ? 'Not present' : thisPieces.map((piece) => piece.instance.piece.name).join(', ')}
+				</>
 			}
 			placement="top"
 		>
 			<div
-				className={classNames('segment-opl__piece-indicator', typeClass, {
-					'segment-opl__piece-indicator--no-piece': !hasPiece,
+				className={classNames('segment-opl__piece-indicator-placeholder', {
+					multiple: thisPieces.length > 1,
+					'multiple--2': thisPieces.length === 2,
+					'multiple--3': thisPieces.length > 2,
 				})}
 				data-source-layer-ids={sourceLayers.map((sourceLayer) => sourceLayer._id).join(' ')}
 			>
-				{hasPiece ? label.substring(0, 1) : ''}
+				{thisPieces.length === 0 && (
+					<div className={classNames('segment-opl__piece-indicator', 'segment-opl__piece-indicator--no-piece')}></div>
+				)}
+				{/* thisPieces.length > 2 && <div className={classNames('segment-opl__piece-indicator', typeClass)}></div> */}
+				{thisPieces.length > 1 && <div className={classNames('segment-opl__piece-indicator', typeClass)}></div>}
+				{thisPieces.length > 0 && (
+					<div className={classNames('segment-opl__piece-indicator', typeClass)}>{label.substring(0, 1)}</div>
+				)}
 			</div>
 		</Tooltip>
 	)
