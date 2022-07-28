ebea0310d59778dc45afd212a668684b9aec96f6
Jan Starzak <jan.starzak@gmail.com>
2022-07-28 11:30:47 +0200

    fix: show anguilar next flag

--- meteor/client/ui/SegmentList/TakeLine.tsx
+++ meteor/client/ui/SegmentList/TakeLine.tsx
@@ -17,7 +17,15 @@ export const TakeLine: React.FC<IProps> = function TakeLine({ isNext, autoNext }
 				auto: autoNext,
 			})}
 		>
-			{isNext && <div className="segment-opl__timeline-flag__label">{autoNext ? t('Auto') : t('Next')}</div>}
+			{isNext && (
+				<div
+					className={classNames('segment-opl__timeline-flag__label', {
+						'segment-opl__timeline-flag__label--autonext': autoNext,
+					})}
+				>
+					{autoNext ? t('Auto') : t('Next')}
+				</div>
+			)}
 			{!isNext && autoNext && <div className="segment-opl__timeline-flag__auto">{t('Auto')}</div>}
 		</div>
 	)
