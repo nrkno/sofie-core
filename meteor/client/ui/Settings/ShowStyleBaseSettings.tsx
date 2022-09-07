e2ed1e3b72d9f6f922e99ee7254fbe9bd3148cb8
Rasmus <raal@tv2.dk>
2022-09-07 12:13:42 +0200

    chore: SOF-966 Showstyle is now Show Style

--- meteor/client/ui/Settings/ShowStyleBaseSettings.tsx
+++ meteor/client/ui/Settings/ShowStyleBaseSettings.tsx
@@ -1466,7 +1466,7 @@ const ShowStyleVariantsSettings = withTranslation()(
 			const { t } = this.props
 			return (
 				<div>
-					<h2 className="mhn">{t('Showstyle Variants')}</h2>
+					<h2 className="mhn">{t('Show Style Variants')}</h2>
 					<table className="table expando settings-studio-showStyleVariants-table">
 						<tbody>{this.renderShowStyleVariants()}</tbody>
 					</table>
