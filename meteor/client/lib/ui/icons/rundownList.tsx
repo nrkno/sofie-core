af04e92f61fecc5743f86dc68dc1523ed9421c5d
Jan Starzak <jan.starzak@gmail.com>
2022-09-21 10:24:33 +0200

    chore: lint icon

--- meteor/client/lib/ui/icons/rundownList.tsx
+++ meteor/client/lib/ui/icons/rundownList.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export function PathIcon() {
 	return (
-<svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 6H3H9L10 5H19C19.5523 5 20 5.44772 20 6V11.5V15C20 15.5523 19.5523 16 19 16H16H3C2.44772 16 2 15.5523 2 15V6Z" fill="#D9D9D9"/>
-<path d="M9 5L8 3H3L2 5H9Z" fill="#D9D9D9"/>
-</svg>
+		<svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M2 6H3H9L10 5H19C19.5523 5 20 5.44772 20 6V11.5V15C20 15.5523 19.5523 16 19 16H16H3C2.44772 16 2 15.5523 2 15V6Z"
+				fill="#D9D9D9"
+			/>
+			<path d="M9 5L8 3H3L2 5H9Z" fill="#D9D9D9" />
+		</svg>
 	)
 }
