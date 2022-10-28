e1dad7fdf82bdc9e7537b20506f4398105b7bec2
Julian Waller <git@julusian.co.uk>
2022-10-28 10:32:27 +0100

    chore: readd MeteorWrapAsync

--- meteor/lib/lib.ts
+++ meteor/lib/lib.ts
@@ -223,6 +223,39 @@ export function toc(name: string = 'default', logStr?: string | Promise<any>[]):
 }
 
 /**
+ * Make Meteor.wrapAsync a bit more type safe
+ * The original version makes the callback be after the last non-undefined parameter, rather than after or replacing the last parameter.
+ * Which makes it incredibly hard to find without iterating over all the parameters. This does that for you, so you dont need to check as many places
+ */
+export function MeteorWrapAsync(func: Function, context?: Object): any {
+	// A variant of Meteor.wrapAsync to fix the bug
+	// https://github.com/meteor/meteor/issues/11120
+
+	return Meteor.wrapAsync((...args: any[]) => {
+		// Find the callback-function:
+		for (let i = args.length - 1; i >= 0; i--) {
+			if (typeof args[i] === 'function') {
+				if (i < args.length - 1) {
+					// The callback is not the last argument, make it so then:
+					const callback = args[i]
+					const fixedArgs = args
+					fixedArgs[i] = undefined
+					fixedArgs.push(callback)
+
+					func.apply(context, fixedArgs)
+					return
+				} else {
+					// The callback is the last argument, that's okay
+					func.apply(context, args)
+					return
+				}
+			}
+		}
+		throw new Meteor.Error(500, `Error in MeteorWrapAsync: No callback found!`)
+	})
+}
+
+/**
  * Blocks the fiber until all the Promises have resolved
  */
 export function waitForPromiseAll<T1, T2, T3, T4, T5, T6>(
