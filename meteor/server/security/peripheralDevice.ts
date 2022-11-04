7422f1ca6f2715991c1d72bc11dfe92cb40dc118
Johan Nyman <johan@nytamin.se>
2022-11-04 14:28:39 +0100

    fix: bug fix: unable to executeFunction on sub-peripheralDevices
    When executing any function on a sub-peripheral device (such as "restartCasparCG") and error was thrown: PeripheralDevice "xxxyyyzzz" not found

--- meteor/server/security/peripheralDevice.ts
+++ meteor/server/security/peripheralDevice.ts
@@ -12,6 +12,7 @@ import { Settings } from '../../lib/Settings'
 import { triggerWriteAccess } from './lib/securityVerify'
 import { profiler } from '../api/profiler'
 import { StudioContentWriteAccess } from './studio'
+import { StudioId } from '@sofie-automation/corelib/dist/dataModel/Ids'
 
 export namespace PeripheralDeviceReadAccess {
 	/** Check for read access for a peripheral device */
@@ -55,9 +56,29 @@ export namespace PeripheralDeviceContentWriteAccess {
 	 */
 	export async function executeFunction(cred0: Credentials, deviceId: PeripheralDeviceId): Promise<ContentAccess> {
 		const device = await PeripheralDevices.findOneAsync(deviceId)
-		if (!device || !device.studioId) throw new Meteor.Error(404, `PeripheralDevice "${deviceId}" not found`)
+		if (!device) throw new Meteor.Error(404, `PeripheralDevice "${deviceId}" not found`)
 
-		const access = await StudioContentWriteAccess.executeFunction(cred0, device.studioId)
+		let studioId: StudioId
+		if (device.studioId) {
+			studioId = device.studioId
+		} else if (device.parentDeviceId) {
+			const parentDevice = await PeripheralDevices.findOneAsync(device.parentDeviceId)
+			if (!parentDevice)
+				throw new Meteor.Error(
+					404,
+					`Parent PeripheralDevice "${device.parentDeviceId}" of "${deviceId}" not found!`
+				)
+			if (!parentDevice.studioId)
+				throw new Meteor.Error(
+					404,
+					`Parent PeripheralDevice "${device.parentDeviceId}" of "${deviceId}" doesn't have any studioId set`
+				)
+			studioId = parentDevice.studioId
+		} else {
+			throw new Meteor.Error(404, `PeripheralDevice "${deviceId}" doesn't have any studioId set`)
+		}
+
+		const access = await StudioContentWriteAccess.executeFunction(cred0, studioId)
 
 		const access2 = await allowAccessToPeripheralDeviceContent(access.cred, device)
 		if (!access2.playout) throw new Meteor.Error(403, `Not allowed: ${access2.reason}`)
