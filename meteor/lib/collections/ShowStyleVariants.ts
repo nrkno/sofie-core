e247533f93fcfc70c4d02794dba30fad0f866f45
Julian Waller <git@julusian.co.uk>
2022-11-29 12:43:54 +0000

    chore: fix some merge errors

--- meteor/lib/collections/ShowStyleVariants.ts
+++ meteor/lib/collections/ShowStyleVariants.ts
@@ -1,12 +1,10 @@
 import { Meteor } from 'meteor/meteor'
 import { createMongoCollection, ObserveChangesForHash } from './lib'
 import { registerIndex } from '../database'
-import { ShowStyleVariantId } from '@sofie-automation/corelib/dist/dataModel/Ids'
 import { CollectionName } from '@sofie-automation/corelib/dist/dataModel/Collections'
 
 import { DBShowStyleVariant } from '@sofie-automation/corelib/dist/dataModel/ShowStyleVariant'
 
-export { ShowStyleVariantId }
 export * from '@sofie-automation/corelib/dist/dataModel/ShowStyleVariant'
 
 export { ShowStyleCompound } from '@sofie-automation/corelib/dist/dataModel/ShowStyleCompound'
