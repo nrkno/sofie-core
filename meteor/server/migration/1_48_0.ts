4416ec1daaf89094a30d1a99c490ed7c6947e36a
Balte de Wit <contact@balte.nl>
2022-12-13 12:23:56 +0100

    chore: remove unused imports

--- meteor/server/migration/1_48_0.ts
+++ meteor/server/migration/1_48_0.ts
@@ -1,5 +1,4 @@
 import { addMigrationSteps } from './databaseMigration'
-import { CURRENT_SYSTEM_VERSION } from './currentSystemVersion'
 import { TranslationsBundles, TranslationsBundle } from '../../lib/collections/TranslationsBundles'
 import { generateTranslationBundleOriginId } from '../api/translationsBundles'
 import { BlueprintId } from '@sofie-automation/corelib/dist/dataModel/Ids'
--- meteor/server/migration/X_X_X.ts
+++ meteor/server/migration/X_X_X.ts
@@ -1,9 +1,5 @@
 import { addMigrationSteps } from './databaseMigration'
 import { CURRENT_SYSTEM_VERSION } from './currentSystemVersion'
-import { TranslationsBundles, TranslationsBundle } from '../../lib/collections/TranslationsBundles'
-import { generateTranslationBundleOriginId } from '../api/translationsBundles'
-import { BlueprintId } from '@sofie-automation/corelib/dist/dataModel/Ids'
-import { unprotectString } from '@sofie-automation/corelib/dist/protectedString'
 
 /*
  * **************************************************************************************
