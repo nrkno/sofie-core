6f959caaeb6cabae04d7b2327abae8773d47fafd
Jan Starzak <jan.starzak@gmail.com>
2022-12-16 17:06:50 +0100

    fix: triggerTypeSelectors undefined handling

--- meteor/lib/api/triggers/triggerTypeSelectors.ts
+++ meteor/lib/api/triggers/triggerTypeSelectors.ts
@@ -5,12 +5,14 @@ import {
 	IBlueprintDeviceTrigger,
 } from '@sofie-automation/blueprints-integration'
 
-export function isHotkeyTrigger(trigger: SomeBlueprintTrigger): trigger is IBlueprintHotkeyTrigger {
+export function isHotkeyTrigger(trigger: SomeBlueprintTrigger | undefined): trigger is IBlueprintHotkeyTrigger {
+	if (!trigger) return false
 	if (trigger.type === TriggerType.hotkey) return true
 	return false
 }
 
-export function isDeviceTrigger(trigger: SomeBlueprintTrigger): trigger is IBlueprintDeviceTrigger {
+export function isDeviceTrigger(trigger: SomeBlueprintTrigger | undefined): trigger is IBlueprintDeviceTrigger {
+	if (!trigger) return false
 	if (trigger.type === TriggerType.device) return true
 	return false
 }
