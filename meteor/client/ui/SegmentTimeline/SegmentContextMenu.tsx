22b95bab1b86de93ab1eda35c6cd1588ddad10f1
Jan Starzak <jan.starzak@gmail.com>
2023-03-02 14:08:48 +0100

    chore: Hide "Set next here" (SOFIE-2184) "Set next here" has become broken and this is to disable it so that it doesn't cause confusion

--- meteor/client/ui/SegmentTimeline/SegmentContextMenu.tsx
+++ meteor/client/ui/SegmentTimeline/SegmentContextMenu.tsx
@@ -76,13 +76,13 @@ export const SegmentContextMenu = withTranslation()(
 								</MenuItem>
 								{startsAt !== null && part && this.props.enablePlayFromAnywhere ? (
 									<>
-										<MenuItem
+										{/* <MenuItem
 											onClick={(e) => this.onSetAsNextFromHere(part.instance.part, e)}
 											disabled={isCurrentPart || !!part.instance.orphaned || !canSetAsNext}
 										>
 											<span dangerouslySetInnerHTML={{ __html: t('Set <strong>Next</strong> Here') }}></span> (
 											{RundownUtils.formatTimeToShortTime(Math.floor((startsAt + timecode) / 1000) * 1000)})
-										</MenuItem>
+										</MenuItem> */}
 										<MenuItem
 											onClick={(e) => this.onPlayFromHere(part.instance.part, e)}
 											disabled={isCurrentPart || !!part.instance.orphaned || !canSetAsNext}
