e81d1d2fa8708e085b7531bf44f93f8d9e95c8d4
Julian Waller <git@julusian.co.uk>
2023-03-09 17:46:37 +0000

    chore: refactor

--- meteor/client/ui/Settings/ShowStyle/Generic.tsx
+++ meteor/client/ui/Settings/ShowStyle/Generic.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
-import { Translated } from '../../../lib/ReactMeteorData/react-meteor-data'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { unprotectString } from '../../../../lib/lib'
 import { EditAttribute } from '../../../lib/EditAttribute'
 import { ShowStyleBase } from '../../../../lib/collections/ShowStyleBases'
@@ -14,58 +13,49 @@ interface IShowStyleGenericPropertiesProps {
 	showStyleBase: ShowStyleBase
 	compatibleStudios: Array<Studio>
 }
-interface IShowStyleGenericPropertiesState {}
-export const ShowStyleGenericProperties = withTranslation()(
-	class StudioGenericProperties extends React.Component<
-		Translated<IShowStyleGenericPropertiesProps>,
-		IShowStyleGenericPropertiesState
-	> {
-		constructor(props: Translated<IShowStyleGenericPropertiesProps>) {
-			super(props)
-		}
+export function ShowStyleGenericProperties({
+	showStyleBase,
+	compatibleStudios,
+}: IShowStyleGenericPropertiesProps): JSX.Element {
+	const { t } = useTranslation()
 
-		render(): JSX.Element {
-			const { t, showStyleBase } = this.props
-
-			return (
-				<div>
-					<div>
-						<label className="field">
-							{t('Show Style Base Name')}
-							{!(this.props.showStyleBase && this.props.showStyleBase.name) ? (
-								<div className="error-notice inline">
-									<FontAwesomeIcon icon={faExclamationTriangle} /> {t('No name set')}
-								</div>
-							) : null}
-							<div className="mdi">
-								<EditAttribute
-									modifiedClassName="bghl"
-									attribute="name"
-									obj={showStyleBase}
-									type="text"
-									collection={ShowStyleBases}
-									className="mdinput"
-								></EditAttribute>
-								<span className="mdfx"></span>
-							</div>
-						</label>
+	return (
+		<div>
+			<div>
+				<label className="field">
+					{t('Show Style Base Name')}
+					{!(showStyleBase && showStyleBase.name) ? (
+						<div className="error-notice inline">
+							<FontAwesomeIcon icon={faExclamationTriangle} /> {t('No name set')}
+						</div>
+					) : null}
+					<div className="mdi">
+						<EditAttribute
+							modifiedClassName="bghl"
+							attribute="name"
+							obj={showStyleBase}
+							type="text"
+							collection={ShowStyleBases}
+							className="mdinput"
+						></EditAttribute>
+						<span className="mdfx"></span>
 					</div>
-					<div>
-						<p className="mod mhn mvs">{t('Compatible Studios:')}</p>
-						<p className="mod mhn mvs">
-							{this.props.compatibleStudios.length > 0
-								? this.props.compatibleStudios.map((i) => (
-										<span key={unprotectString(i._id)} className="pill">
-											<Link className="pill-link" to={`/settings/studio/${i._id}`}>
-												{i.name}
-											</Link>
-										</span>
-								  ))
-								: t('This Show Style is not compatible with any Studio')}
-						</p>
-					</div>
-				</div>
-			)
-		}
-	}
-)
+				</label>
+			</div>
+			<div>
+				<p className="mod mhn mvs">{t('Compatible Studios:')}</p>
+				<p className="mod mhn mvs">
+					{compatibleStudios.length > 0
+						? compatibleStudios.map((i) => (
+								<span key={unprotectString(i._id)} className="pill">
+									<Link className="pill-link" to={`/settings/studio/${i._id}`}>
+										{i.name}
+									</Link>
+								</span>
+						  ))
+						: t('This Show Style is not compatible with any Studio')}
+				</p>
+			</div>
+		</div>
+	)
+}
