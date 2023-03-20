d21006f0cef1ce4add382b1f96d7daf143ad5890
Julian Waller <git@julusian.co.uk>
2023-03-20 14:50:53 +0000

    fix: forward `removeDelay` paremeter in `PeripheralDeviceApi.removePackageInfo`

--- meteor/server/api/peripheralDevice.ts
+++ meteor/server/api/peripheralDevice.ts
@@ -1169,9 +1169,10 @@ class ServerPeripheralDeviceAPIClass extends MethodContextAPI implements NewPeri
 		deviceId: PeripheralDeviceId,
 		deviceToken: string,
 		type: string,
-		packageId: ExpectedPackageId
+		packageId: ExpectedPackageId,
+		removeDelay?: number
 	) {
-		await PackageManagerIntegration.removePackageInfo(this, deviceId, deviceToken, type, packageId)
+		await PackageManagerIntegration.removePackageInfo(this, deviceId, deviceToken, type, packageId, removeDelay)
 	}
 }
 registerClassToMeteorMethods(PeripheralDeviceAPIMethods, ServerPeripheralDeviceAPIClass, false)
