3c96d8d425b4cd9f070d02d6fee2c8a5502d46ac
Balte Mint de Wit <contact@balte.nl>
2023-03-22 13:04:34 +0000

    chore: move listperipheraldevices to shared function

--- packages/job-worker/src/blueprints/context/RundownActivationContext.ts
+++ packages/job-worker/src/blueprints/context/RundownActivationContext.ts
@@ -1,16 +1,12 @@
 import { IRundownActivationContext, TSR } from '@sofie-automation/blueprints-integration'
 import { PeripheralDeviceId } from '@sofie-automation/shared-lib/dist/core/model/Ids'
 import { PeripheralDevicePublicWithActions } from '@sofie-automation/shared-lib/dist/core/model/peripheralDevice'
-import { literal } from '@sofie-automation/shared-lib/dist/lib/lib'
-import { Complete } from '@sofie-automation/corelib/dist/lib'
-import { PeripheralDeviceType } from '@sofie-automation/shared-lib/dist/peripheralDevice/peripheralDeviceAPI'
 import { ReadonlyDeep } from 'type-fest'
 import { JobContext, ProcessedShowStyleCompound } from '../../jobs'
-import { executePeripheralDeviceAction } from '../../peripheralDevice'
+import { executePeripheralDeviceAction, listPeripheralDevices } from '../../peripheralDevice'
 import { CacheForPlayout } from '../../playout/cache'
 import { RundownEventContext } from './RundownEventContext'
 import { DBRundown } from '@sofie-automation/corelib/dist/dataModel/Rundown'
-import { PeripheralDevice } from '@sofie-automation/corelib/dist/dataModel/PeripheralDevice'
 
 export class RundownActivationContext extends RundownEventContext implements IRundownActivationContext {
 	private readonly _cache: CacheForPlayout
@@ -35,37 +31,7 @@ export class RundownActivationContext extends RundownEventContext implements IRu
 	}
 
 	async listPeripheralDevices(): Promise<PeripheralDevicePublicWithActions[]> {
-		const parentDeviceIds = this._cache.PeripheralDevices.findAll(
-			(doc: PeripheralDevice) =>
-				doc.studioId === this._context.studioId && doc.type === PeripheralDeviceType.PLAYOUT
-		).map((doc) => doc._id)
-		if (parentDeviceIds.length === 0) {
-			throw new Error('No parent devices are configured')
-		}
-
-		const devices = await this._context.directCollections.PeripheralDevices.findFetch({
-			parentDeviceId: {
-				$in: parentDeviceIds,
-			},
-		})
-
-		return devices.map((d) => {
-			// Only expose a subset of the PeripheralDevice to the blueprints
-			return literal<Complete<PeripheralDevicePublicWithActions>>({
-				_id: d._id,
-				name: d.name,
-				deviceName: d.deviceName,
-				studioId: d.studioId,
-				category: d.category,
-				type: d.type,
-				subType: d.subType,
-				parentDeviceId: d.parentDeviceId,
-				created: d.created,
-				status: d.status,
-				settings: d.settings,
-				actions: d.configManifest.subdeviceManifest?.[d.subType]?.actions,
-			})
-		})
+		return listPeripheralDevices(this._context, this._cache)
 	}
 
 	async executeTSRAction(
