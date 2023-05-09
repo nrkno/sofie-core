06ad7578f21223c618890c79646310fdf61a033f
Tom Lee <t.lee@evs.com>
2023-05-09 11:11:51 +0100

    chore: let -> const

--- meteor/server/api/rest/api.ts
+++ meteor/server/api/rest/api.ts
@@ -1756,7 +1756,7 @@ Meteor.startup(() => {
 		WebApp.connectHandlers.use('/api/v1.0', Meteor.bindEnvironment(app.callback()))
 		// Redirect `/api/latest` to the most recent API version
 		WebApp.connectHandlers.use(function (req, res, next) {
-			let path = req.url
+			const path = req.url
 			if (path == '/api/latest') {
 				res.writeHead(307, { Location: '/api/v1.0' })
 				res.end()
