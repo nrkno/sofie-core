a82183e3212d0c8f61ca14e19990232df600d391
Johan Nyman <johan@nytamin.se>
2023-05-10 06:22:08 +0200

    chore: fix reset script
    The reset script required Node 16 or higher (due to rimraf). So I added a check and an helpful message if someone tries with Node 14.

--- scripts/reset.mjs
+++ scripts/reset.mjs
@@ -1,20 +1,32 @@
 /* eslint-disable no-console */
 import rimraf from "rimraf";
 
-console.log("Removing all artifacts...");
-
 async function rimrafLog(command) {
 	console.log("  " + command);
-	return rimraf(command);
+	return rimraf(command, {
+		glob: true,
+	});
 }
 
-await rimrafLog("./meteor/.meteor/local");
-await rimrafLog("./meteor/node_modules");
-await rimrafLog("./meteor/coverage");
+// Check Nodejs version:
+// (rimraf requires Node 16 or higher)
+const m = process.versions.node.match(/^(\d+)\.(\d+)\.(\d+)$/);
+if (parseInt(m[1]) < 16) {
+	console.log("The reset script requires Node.js 16 or higher");
+	process.exit(1);
+}
 
-await rimrafLog("./packages/node_modules");
-await rimrafLog("./packages/*/node_modules");
-await rimrafLog("./packages/*/dist");
+(async () => {
+	console.log("Removing all artifacts...");
 
-console.log(`...done!`);
-console.log(`To install everything again, run "yarn start"`);
+	await rimrafLog("./meteor/.meteor/local");
+	await rimrafLog("./meteor/node_modules");
+	await rimrafLog("./meteor/coverage");
+
+	await rimrafLog("./packages/node_modules");
+	await rimrafLog("./packages/*/node_modules");
+	await rimrafLog("./packages/*/dist");
+
+	console.log(`...done!`);
+	console.log(`To install everything again, run "yarn start"`);
+})().catch(console.error());
