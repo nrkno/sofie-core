613f899809d71b2b29fb7236f2e2dac3b7dbd6ef
Julian Waller <git@julusian.co.uk>
2023-05-10 11:34:07 +0100

    chore: update jest config

--- packages/live-status-gateway/jest.config.js
+++ packages/live-status-gateway/jest.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-	globals: {
-		'ts-jest': {
-			tsconfig: 'tsconfig.json',
-		},
-	},
+	globals: {},
 	moduleFileExtensions: ['js', 'ts'],
 	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest',
+		'^.+\\.(ts|tsx)$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.json',
+			},
+		],
 	},
-	testMatch: ['**/__tests__/**/*.spec.(ts|js)'],
+	testMatch: ['**/__tests__/**/*.(spec|test).(ts|js)'],
 	testPathIgnorePatterns: ['integrationTests'],
 	testEnvironment: 'node',
-	coverageThreshold: {
-		global: {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
-		},
-	},
+	// coverageThreshold: {
+	// 	global: {
+	// 		branches: 100,
+	// 		functions: 100,
+	// 		lines: 100,
+	// 		statements: 100,
+	// 	},
+	// },
 	coverageDirectory: './coverage/',
 	collectCoverage: true,
 	preset: 'ts-jest',
