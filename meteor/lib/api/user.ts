29a92c6dbc201885266e5ca9ecf1b4c9c42cbba3
Julian Waller <git@julusian.co.uk>
2023-05-19 13:33:23 +0100

    chore: update usages of Accounts meteor package

--- meteor/lib/api/user.ts
+++ meteor/lib/api/user.ts
@@ -1,7 +1,6 @@
 import { Accounts } from 'meteor/accounts-base'
 import { UserProfile } from '../../lib/collections/Users'
 import { protectString } from '../lib'
-import { DBOrganizationBase } from '../collections/Organization'
 import { UserId } from '@sofie-automation/corelib/dist/dataModel/Ids'
 
 export interface NewUserAPI {
@@ -19,18 +18,12 @@ interface NewUser {
 	email: string
 	profile: UserProfile
 	password?: string
-	createOrganization?: DBOrganizationBase
 }
 export async function createUser(newUser: NewUser): Promise<UserId> {
 	// This is available both client-side and server side.
 	// The reason for that is that the client-side should use Accounts.createUser right away
 	// so that the password aren't sent in "plaintext" to the server.
 
-	return new Promise<UserId>((resolve, reject) => {
-		const userId = Accounts.createUser(newUser, (error) => {
-			if (error) reject(error)
-			else if (!userId) reject(new Error('Missing UserId'))
-			else resolve(protectString<UserId>(userId))
-		})
-	})
+	const userId = await Accounts.createUserAsync(newUser)
+	return protectString<UserId>(userId)
 }
