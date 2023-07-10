5adc21582c872a73ae105d99e947c42d70ad5499
Julian Waller <git@julusian.co.uk>
2023-07-10 12:33:24 +0100

    fix: lint

--- meteor/lib/api/methods.ts
+++ meteor/lib/api/methods.ts
@@ -66,26 +66,6 @@ export const MeteorCall: IMeteorCall = {
 	system: makeMethods(SystemAPIMethods),
 }
 
-/**
- * Convenience method to convert a Meteor.apply() into a Promise
- * @param callName {string} Method name
- * @param args {Array<any>} An array of arguments for the method call
- * @param options (Optional) An object with options for the call. See Meteor documentation.
- * @returns {Promise<any>} A promise containing the result of the called method.
- */
-async function MeteorPromiseApply(
-	callName: Parameters<typeof Meteor.apply>[0],
-	args: Parameters<typeof Meteor.apply>[1],
-	options?: Parameters<typeof Meteor.apply>[2]
-): Promise<any> {
-	return new Promise((resolve, reject) => {
-		Meteor.apply(callName, args, options, (err, res) => {
-			if (err) reject(err)
-			else resolve(res)
-		})
-	})
-}
-
 function makeMethods<Enum extends { [key: string]: string }>(
 	methods: Enum,
 	/** (Optional) An array of methodnames. Calls to these methods won't be retried in the case of a loss-of-connection for the client. */
