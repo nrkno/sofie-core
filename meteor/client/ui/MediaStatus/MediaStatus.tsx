65f543ad59d218dd5336bb293fa6621c3703f8a4
Jan Starzak <jan.starzak@gmail.com>
2023-07-17 16:16:02 +0200

    feat: Media Status WIP

---
+++ meteor/client/ui/MediaStatus/MediaStatus.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface IProps {
+	className?: string
+}
+
+export function MediaStatus({ className }: IProps): JSX.Element {
+	return <div className={className}></div>
+}
