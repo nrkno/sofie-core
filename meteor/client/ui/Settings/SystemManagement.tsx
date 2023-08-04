849ff11bfbe346fd6e8d3589a4b13067bd21f297
Jan Starzak <jan.starzak@gmail.com>
2023-08-04 15:53:08 +0200

    fix(Core System Settings): Cron Job Settings are duplicated

--- meteor/client/ui/Settings/SystemManagement.tsx
+++ meteor/client/ui/Settings/SystemManagement.tsx
@@ -240,31 +240,6 @@ export default translateWithTracker<IProps, {}, ITrackedProps>((_props: IProps) 
 								></EditAttribute>
 							</div>
 						</div>
-
-						<h2 className="mhn">{t('Cleanup')}</h2>
-						<div>
-							<button className="btn btn-default" onClick={() => this.cleanUpOldDatabaseIndexes()}>
-								{t('Cleanup old database indexes')}
-							</button>
-						</div>
-						<div>
-							<button className="btn btn-default" onClick={() => checkForOldDataAndCleanUp(t)}>
-								{t('Cleanup old data')}
-							</button>
-						</div>
-
-						<h2 className="mhn">{t('Cron jobs')}</h2>
-						<div className="field">
-							{t('Disable CasparCG restart job')}
-							<div className="mdi">
-								<EditAttribute
-									attribute="cron.casparCG.disabled"
-									obj={this.props.coreSystem}
-									type="checkbox"
-									collection={CoreSystem}
-								></EditAttribute>
-							</div>
-						</div>
 						<div className="field">
 							{t('Enable automatic storage of Rundown Playlist snapshots periodically')}
 							<div className="mdi">
@@ -292,6 +267,18 @@ export default translateWithTracker<IProps, {}, ITrackedProps>((_props: IProps) 
 								/>
 							</div>
 							<div>{t('(Comma separated list. Empty - will store snapshots of all Rundown Playlists)')}</div>
+						</div>
+
+						<h2 className="mhn">{t('Cleanup')}</h2>
+						<div>
+							<button className="btn btn-default" onClick={() => this.cleanUpOldDatabaseIndexes()}>
+								{t('Cleanup old database indexes')}
+							</button>
+						</div>
+						<div>
+							<button className="btn btn-default" onClick={() => checkForOldDataAndCleanUp(t)}>
+								{t('Cleanup old data')}
+							</button>
 						</div>
 					</div>
 				</div>
