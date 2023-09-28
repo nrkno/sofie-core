9a63519fac85bbcd6724cf00681a308d66aecdaf
ianshade <ianshade@users.noreply.github.com>
2023-09-28 10:24:37 +0200

    feat(live-status-gw): add segments handler throttling
    adds throttling with leading and trailing edge, and wait period of 200ms
    note: leading edge seems required because of the throttled function being async

--- packages/live-status-gateway/src/collections/segmentsHandler.ts
+++ packages/live-status-gateway/src/collections/segmentsHandler.ts
@@ -2,19 +2,24 @@ import { Logger } from 'winston'
 import { CoreHandler } from '../coreHandler'
 import { CollectionBase, Collection } from '../wsHandler'
 import { DBSegment } from '@sofie-automation/corelib/dist/dataModel/Segment'
+import * as _ from 'underscore'
+
+const THROTTLE_PERIOD_MS = 200
 
 export class SegmentsHandler extends CollectionBase<DBSegment[]> implements Collection<DBSegment[]> {
 	public observerName: string
+	private throttledNotify: (data: DBSegment[]) => Promise<void>
 
 	constructor(logger: Logger, coreHandler: CoreHandler) {
 		super(SegmentsHandler.name, undefined, undefined, logger, coreHandler)
 		this.observerName = this._name
+		this.throttledNotify = _.throttle(this.notify.bind(this), THROTTLE_PERIOD_MS, { leading: true, trailing: true })
 	}
 
 	async setSegments(rundowns: DBSegment[]): Promise<void> {
 		this._logger.info(`'${this._name}' handler received segments update with ${rundowns.length} segments`)
 		this._collectionData = rundowns
-		await this.notify(this._collectionData)
+		await this.throttledNotify(this._collectionData)
 	}
 
 	// override notify to implement empty array handling
