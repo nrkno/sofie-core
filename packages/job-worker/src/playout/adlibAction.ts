e2ff3545d097c74429bc81070f503795d5f1008f
Julian Waller <julian@superfly.tv>
2023-10-16 12:57:57 +0100

    fix: allow BucketAdlibAction to access PackageInfos SOFIE-2655 (#1052)

--- packages/job-worker/src/playout/adlibAction.ts
+++ packages/job-worker/src/playout/adlibAction.ts
@@ -54,7 +54,11 @@ export async function handleExecuteAdlibAction(
 		const watchedPackages = await WatchedPackagesHelper.create(context, context.studio._id, {
 			pieceId: data.actionDocId,
 			fromPieceType: {
-				$in: [ExpectedPackageDBType.ADLIB_ACTION, ExpectedPackageDBType.BASELINE_ADLIB_ACTION],
+				$in: [
+					ExpectedPackageDBType.ADLIB_ACTION,
+					ExpectedPackageDBType.BASELINE_ADLIB_ACTION,
+					ExpectedPackageDBType.BUCKET_ADLIB_ACTION,
+				],
 			},
 		})
 
