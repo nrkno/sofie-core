033b5b06a24f8bcbad21b5249408228ef5e62133
Julian Waller <git@julusian.co.uk>
2024-05-20 12:14:22 +0100

    fix: object schema table update issues

--- meteor/client/lib/forms/SchemaFormTable/ObjectTableOpHelper.tsx
+++ meteor/client/lib/forms/SchemaFormTable/ObjectTableOpHelper.tsx
@@ -7,34 +7,36 @@ import { OverrideOpHelperForItemContents, WrappedOverridableItem } from '../../.
  */
 export class OverrideOpHelperObjectTable implements OverrideOpHelperForItemContents {
 	readonly #baseHelper: OverrideOpHelperForItemContents
-	readonly #itemId: string
+	readonly #parentItem: WrappedOverridableItem<object>
 	readonly #currentRows: WrappedOverridableItem<object>[]
 	readonly #path: string
 
 	constructor(
 		baseHelper: OverrideOpHelperForItemContents,
-		itemId: string,
+		parentItem: WrappedOverridableItem<object>,
 		currentRows: WrappedOverridableItem<object>[],
 		path: string
 	) {
 		this.#baseHelper = baseHelper
-		this.#itemId = itemId
+		this.#parentItem = parentItem
 		this.#currentRows = currentRows
 		this.#path = path
 	}
 
 	clearItemOverrides(rowId: string, subPath: string): void {
-		this.#baseHelper.clearItemOverrides(this.#itemId, joinObjectPathFragments(this.#path, rowId, subPath))
+		this.#baseHelper.clearItemOverrides(this.#parentItem.id, joinObjectPathFragments(this.#path, rowId, subPath))
 	}
 	deleteRow(rowId: string): void {
-		// Clear any existing overrides
-		this.#baseHelper.clearItemOverrides(this.#itemId, joinObjectPathFragments(this.#path, rowId))
+		const rowPath = joinObjectPathFragments(this.#path, rowId)
 
 		// If row was not user created (it has defaults), then don't store `undefined`
-		const row = this.#currentRows.find((r) => r.id === rowId)
-		if (row && row.defaults) {
-			// This is a bit of a hack, but it isn't possible to create a delete op from here
-			this.#baseHelper.setItemValue(this.#itemId, joinObjectPathFragments(this.#path, rowId), undefined)
+		const currentRow = this.#currentRows.find((r) => r.id === rowId)
+		if (!this.#parentItem.defaults || currentRow?.defaults) {
+			// Mark the row as undefined. This ensures it gets deleted even if the parent is an override
+			this.#baseHelper.setItemValue(this.#parentItem.id, rowPath, undefined)
+		} else {
+			// Clear any existing overrides
+			this.#baseHelper.clearItemOverrides(this.#parentItem.id, rowPath)
 		}
 	}
 	setItemValue(rowId: string, subPath: string, value: unknown): void {
@@ -43,16 +45,14 @@ export class OverrideOpHelperObjectTable implements OverrideOpHelperForItemConte
 
 		if (currentRow.defaults) {
 			// has defaults, so override the single value
-			this.#baseHelper.setItemValue(this.#itemId, joinObjectPathFragments(this.#path, rowId, subPath), value)
+			this.#baseHelper.setItemValue(this.#parentItem.id, joinObjectPathFragments(this.#path, rowId, subPath), value)
 		} else {
 			// no defaults, replace the whole object
-			// Ensure there arent existing overrides
-			this.#baseHelper.clearItemOverrides(this.#itemId, joinObjectPathFragments(this.#path, rowId))
 
 			// replace with a new override
 			const newObj = clone(currentRow.computed)
 			objectPathSet(newObj, subPath, value)
-			this.#baseHelper.setItemValue(this.#itemId, joinObjectPathFragments(this.#path, rowId), newObj)
+			this.#baseHelper.setItemValue(this.#parentItem.id, joinObjectPathFragments(this.#path, rowId), newObj)
 		}
 	}
 }
