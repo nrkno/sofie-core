45c25b969a6376bb390ece7fde638a5fb098baea
Johan Nyman <johan@nytamin.se>
2024-07-11 14:42:49 +0200

    fix: add Package Manager types for HTML_template rendering

--- packages/shared-lib/src/package-manager/package.ts
+++ packages/shared-lib/src/package-manager/package.ts
@@ -9,12 +9,17 @@ import { StatusCode } from '../lib/status'
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace ExpectedPackage {
-	export type Any = ExpectedPackageMediaFile | ExpectedPackageQuantelClip | ExpectedPackageJSONData
+	export type Any =
+		| ExpectedPackageMediaFile
+		| ExpectedPackageQuantelClip
+		| ExpectedPackageJSONData
+		| ExpectedPackageHtmlTemplate
 
 	export enum PackageType {
 		MEDIA_FILE = 'media_file',
 		QUANTEL_CLIP = 'quantel_clip',
 		JSON_DATA = 'json_data',
+		HTML_TEMPLATE = 'html_template',
 
 		// TALLY_LABEL = 'tally_label'
 
@@ -169,6 +174,78 @@ export namespace ExpectedPackage {
 					| AccessorOnPackage.HTTPProxy
 					| AccessorOnPackage.LocalFolder
 					| AccessorOnPackage.FileShare
+			}
+		}[]
+	}
+	export interface ExpectedPackageHtmlTemplate extends Base {
+		type: PackageType.HTML_TEMPLATE
+		content: {
+			/** path to the HTML template */
+			path: string
+			/** Add prefix to output artifacts */
+			outputPrefix: string
+		}
+		version: {
+			renderer?: {
+				/** Renderer width, defaults to 1920 */
+				width?: number
+				/** Renderer width, defaults to 1080 */
+				height?: number
+				/** Zoom level, defaults to 1 */
+				zoom?: number
+				/** (defaults to black) */
+				backgroundColor?: string
+				userAgent?: string
+			}
+
+			/**
+			 * Convenience settings for a template that follows the typical CasparCG steps;
+			 * update(data); play(); stop();
+			 * If this is set, steps are overridden */
+			casparCG?: {
+				/**
+				 * Data to send into the update() function of a CasparCG Template.
+				 * Strings will be piped through as-is, objects will be JSON.stringified.
+				 */
+				data: { [key: string]: any } | null | string
+
+				/** How long to wait between each action in a CasparCG template, (default: 1000ms) */
+				delay?: number
+			}
+
+			steps?: (
+				| { do: 'waitForLoad' }
+				| { do: 'sleep'; duration: number }
+				| {
+						do: 'sendHTTPCommand'
+						url: string
+						/** GET, POST, PUT etc.. */
+						method: string
+						body?: ArrayBuffer | ArrayBufferView | NodeJS.ReadableStream | string | URLSearchParams
+
+						headers?: Record<string, string>
+				  }
+				| { do: 'takeScreenshot'; fileName: string }
+				| { do: 'startRecording'; fileName: string }
+				| { do: 'stopRecording' }
+				| { do: 'cropRecording'; fileName: string }
+				| { do: 'executeJs'; js: string }
+				// Store an object in memory
+				| { do: 'storeObject'; key: string; value: Record<string, any> }
+				// Modify an object in memory. Path is a dot-separated string
+				| { do: 'modifyObject'; key: string; path: string; value: any }
+				// Send an object to the renderer as a postMessage
+				| { do: 'injectObject'; key: string }
+			)[]
+		}
+		sources: {
+			containerId: string
+			accessors: {
+				[accessorId: string]:
+					| AccessorOnPackage.LocalFolder
+					| AccessorOnPackage.FileShare
+					| AccessorOnPackage.HTTP
+					| AccessorOnPackage.HTTPProxy
 			}
 		}[]
 	}
