569f2fbbe790c9a917748cfb3e9188c309fd0121
Julian Waller <git@julusian.co.uk>
2024-07-30 17:02:55 +0100

    wip: simplify

--- meteor/lib/collections/lib.ts
+++ meteor/lib/collections/lib.ts
@@ -1,18 +1,14 @@
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
-import { ProtectedString, protectString } from '../lib'
+import { ProtectedString } from '../lib'
 import { stringifyError } from '@sofie-automation/shared-lib/dist/lib/stringifyError'
-import type { Collection as RawCollection, Db as RawDb } from 'mongodb'
 import { CollectionName } from '@sofie-automation/corelib/dist/dataModel/Collections'
-import { MongoModifier, MongoQuery } from '@sofie-automation/corelib/dist/mongo'
+import { MongoQuery } from '@sofie-automation/corelib/dist/mongo'
 import {
-	MongoCollection,
 	MongoReadOnlyCollection,
 	MongoCursor,
 	FindOptions,
 	FindOneOptions,
-	UpdateOptions,
-	UpsertOptions,
 } from '@sofie-automation/meteor-lib/dist/collections/lib'
 
 export * from '@sofie-automation/meteor-lib/dist/collections/lib'
@@ -31,21 +27,6 @@ export function getOrCreateMongoCollection(name: string): Mongo.Collection<any> 
 	const newCollection = new Mongo.Collection(name)
 	collectionsCache.set(name, newCollection)
 	return newCollection
-}
-
-/**
- * Create a Mongo Collection for use in the client (has sync apis)
- * @param name Name of the collection
- */
-export function createSyncMongoCollection<DBInterface extends { _id: ProtectedString<any> }>(
-	name: CollectionName
-): MongoCollection<DBInterface> {
-	const collection = getOrCreateMongoCollection(name)
-	const wrapped = new WrappedMongoCollection<DBInterface>(collection, name)
-
-	// registerClientCollection(name, wrapped)
-
-	return wrapped
 }
 
 /**
@@ -105,61 +86,6 @@ class WrappedMongoReadOnlyCollection<DBInterface extends { _id: ProtectedString<
 	): DBInterface | undefined {
 		try {
 			return this._collection.findOne((selector ?? {}) as any, options as any)
-		} catch (e) {
-			this.wrapMongoError(e)
-		}
-	}
-}
-export class WrappedMongoCollection<DBInterface extends { _id: ProtectedString<any> }>
-	extends WrappedMongoReadOnlyCollection<DBInterface>
-	implements MongoCollection<DBInterface>
-{
-	insert(doc: DBInterface): DBInterface['_id'] {
-		try {
-			const resultId = this._collection.insert(doc as unknown as Mongo.OptionalId<DBInterface>)
-			return protectString(resultId)
-		} catch (e) {
-			this.wrapMongoError(e)
-		}
-	}
-	rawCollection(): RawCollection<DBInterface> {
-		return this._collection.rawCollection() as any
-	}
-	rawDatabase(): RawDb {
-		return this._collection.rawDatabase() as any
-	}
-	remove(selector: MongoQuery<DBInterface> | DBInterface['_id']): number {
-		try {
-			return this._collection.remove(selector as any)
-		} catch (e) {
-			this.wrapMongoError(e)
-		}
-	}
-	update(
-		selector: MongoQuery<DBInterface> | DBInterface['_id'] | { _id: DBInterface['_id'] },
-		modifier: MongoModifier<DBInterface>,
-		options?: UpdateOptions
-	): number {
-		try {
-			return this._collection.update(selector as any, modifier as any, options)
-		} catch (e) {
-			this.wrapMongoError(e)
-		}
-	}
-	upsert(
-		selector: MongoQuery<DBInterface> | DBInterface['_id'],
-		modifier: MongoModifier<DBInterface>,
-		options?: UpsertOptions
-	): {
-		numberAffected?: number
-		insertedId?: DBInterface['_id']
-	} {
-		try {
-			const result = this._collection.upsert(selector as any, modifier as any, options)
-			return {
-				numberAffected: result.numberAffected,
-				insertedId: protectString(result.insertedId),
-			}
 		} catch (e) {
 			this.wrapMongoError(e)
 		}
--- meteor/lib/collections/libCollections.ts
+++ meteor/lib/collections/libCollections.ts
@@ -6,12 +6,9 @@
 import { CollectionName } from '@sofie-automation/corelib/dist/dataModel/Collections'
 import { AdLibAction } from '@sofie-automation/corelib/dist/dataModel/AdlibAction'
 import { AdLibPiece } from '@sofie-automation/corelib/dist/dataModel/AdLibPiece'
-import { createSyncMongoCollection, createSyncReadOnlyMongoCollection } from './lib'
-import { DBOrganization } from '@sofie-automation/meteor-lib/dist/collections/Organization'
+import { createSyncReadOnlyMongoCollection } from './lib'
 import { PartInstance } from '@sofie-automation/meteor-lib/dist/collections/PartInstances'
 import { DBPart } from '@sofie-automation/corelib/dist/dataModel/Part'
-import { PieceInstance } from '@sofie-automation/corelib/dist/dataModel/PieceInstance'
-import { Piece } from '@sofie-automation/corelib/dist/dataModel/Piece'
 import { RundownBaselineAdLibAction } from '@sofie-automation/corelib/dist/dataModel/RundownBaselineAdLibAction'
 import { RundownBaselineAdLibItem } from '@sofie-automation/corelib/dist/dataModel/RundownBaselineAdLibPiece'
 import { DBRundownPlaylist } from '@sofie-automation/corelib/dist/dataModel/RundownPlaylist'
@@ -21,12 +18,6 @@ import { DBSegment } from '@sofie-automation/corelib/dist/dataModel/Segment'
 export const AdLibActions = createSyncReadOnlyMongoCollection<AdLibAction>(CollectionName.AdLibActions)
 
 export const AdLibPieces = createSyncReadOnlyMongoCollection<AdLibPiece>(CollectionName.AdLibPieces)
-
-export const Organizations = createSyncMongoCollection<DBOrganization>(CollectionName.Organizations)
-
-export const PieceInstances = createSyncReadOnlyMongoCollection<PieceInstance>(CollectionName.PieceInstances)
-
-export const Pieces = createSyncReadOnlyMongoCollection<Piece>(CollectionName.Pieces)
 
 export const PartInstances = createSyncReadOnlyMongoCollection<PartInstance>(CollectionName.PartInstances)
 
