0a4bd7f73b7cc6326b509c3dbfd2947d40b9f793
Johan Nyman <johan@nytamin.se>
2024-08-21 10:26:30 +0200

    fix: add ExpectedPackageHtmlTemplate types from Package Manager

--- packages/shared-lib/src/package-manager/package.ts
+++ packages/shared-lib/src/package-manager/package.ts
@@ -13,7 +13,11 @@ type PackageContainerId = string
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace ExpectedPackage {
-	export type Any = ExpectedPackageMediaFile | ExpectedPackageQuantelClip | ExpectedPackageJSONData
+	export type Any =
+		| ExpectedPackageMediaFile
+		| ExpectedPackageQuantelClip
+		| ExpectedPackageJSONData
+		| ExpectedPackageHtmlTemplate
 
 	export enum PackageType {
 		MEDIA_FILE = 'media_file',
@@ -175,6 +179,67 @@ export namespace ExpectedPackage {
 					| AccessorOnPackage.HTTPProxy
 					| AccessorOnPackage.LocalFolder
 					| AccessorOnPackage.FileShare
+			}
+		}[]
+	}
+	export interface ExpectedPackageHtmlTemplate extends Base {
+		type: PackageType.HTML_TEMPLATE
+		content: {
+			/** path to the HTML template */
+			path: string
+			/** Add prefix to output artifacts */
+			outputPrefix: string
+		}
+		version: {
+			renderer?: {
+				/** Renderer width, defaults to 1920 */
+				width?: number
+				/** Renderer width, defaults to 1080 */
+				height?: number
+				/**
+				 * Scale the rendered width and height with this value, and also zoom the content accordingly.
+				 * For example, if the width is 1920 and scale is 0.5, the width will be scaled to 960.
+				 * (Defaults to 1)
+				 */
+				scale?: number
+				/** Background color, #RRGGBB, CSS-string, "transparent" or "default" (defaults to "default") */
+				background?: string
+				userAgent?: string
+			}
+
+			/**
+			 * Convenience settings for a template that follows the typical CasparCG steps;
+			 * update(data); play(); stop();
+			 * If this is set, steps are overridden */
+			casparCG?: {
+				/**
+				 * Data to send into the update() function of a CasparCG Template.
+				 * Strings will be piped through as-is, objects will be JSON.stringified.
+				 */
+				data: { [key: string]: any } | null | string
+
+				/** How long to wait between each action in a CasparCG template, (default: 1000ms) */
+				delay?: number
+			}
+
+			steps?: (
+				| { do: 'waitForLoad' }
+				| { do: 'sleep'; duration: number }
+				| { do: 'takeScreenshot'; fileName: string }
+				| { do: 'startRecording'; fileName: string }
+				| { do: 'stopRecording' }
+				| { do: 'cropRecording'; fileName: string }
+				| { do: 'executeJs'; js: string }
+			)[]
+		}
+		sources: {
+			containerId: PackageContainerId
+			accessors: {
+				[accessorId: AccessorId]:
+					| AccessorOnPackage.LocalFolder
+					| AccessorOnPackage.FileShare
+					| AccessorOnPackage.HTTP
+					| AccessorOnPackage.HTTPProxy
 			}
 		}[]
 	}
