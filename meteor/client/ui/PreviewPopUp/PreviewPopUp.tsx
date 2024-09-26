6999adc3e99ca460124c58cec55dce2ad17a5cd7
Jan Starzak <jan.starzak@gmail.com>
2024-09-26 13:13:54 +0200

    wip: Preview popup (next gen floating inspector)

---
+++ meteor/client/ui/PreviewPopUp/PreviewPopUp.tsx
@@ -0,0 +1,87 @@
+import React, { useMemo, useState } from 'react'
+import classNames from 'classnames'
+import { usePopper } from 'react-popper'
+import { Padding, Placement, VirtualElement } from '@popperjs/core'
+
+export function PreviewPopUp({
+	anchor,
+	padding,
+	placement,
+	size,
+	preview,
+	controls,
+	contentInfo,
+	warnings,
+}: {
+	anchor: HTMLElement | VirtualElement
+	padding: Padding
+	placement: Placement
+	size: 'small' | 'large'
+	preview?: React.ReactNode
+	controls?: React.ReactNode
+	contentInfo?: React.ReactNode
+	warnings?: React.ReactNode
+}): React.JSX.Element {
+	const warningsCount = React.Children.count(warnings)
+
+	const [popperEl, setPopperEl] = useState<HTMLDivElement | null>(null)
+	const popperOptions = useMemo(
+		() => ({
+			placement: placement ?? 'top',
+			strategy: 'fixed' as const,
+			modifiers: [
+				{
+					name: 'flip',
+					options: {
+						fallbackPlacements: [
+							'top-start',
+							'top-end',
+							'right',
+							'right-start',
+							'right-end',
+							'left',
+							'left-start',
+							'left-end',
+							'bottom',
+							'bottom-start',
+							'bottom-end',
+						],
+						rootBoundary: 'viewport',
+						padding: padding ?? 0,
+					},
+				},
+				{
+					name: 'preventOverflow',
+					options: {
+						padding: padding ?? 0, // additional padding placed here (like Header, etc.)
+					},
+				},
+			],
+		}),
+		[padding]
+	)
+	const { styles, attributes } = usePopper(anchor, popperEl, popperOptions)
+
+	return (
+		<div
+			ref={setPopperEl}
+			className={classNames('preview-popUp', {
+				'preview-popUp--large': size === 'large',
+				'preview-popUp--small': size === 'small',
+			})}
+			style={styles.popper}
+			{...attributes.popper}
+		>
+			{preview && <div className="preview-popUp__preview">{preview}</div>}
+			{controls && <div className="preview-popUp__controls">{controls}</div>}
+			{contentInfo && <div className="preview-popUp__content-info">{contentInfo}</div>}
+			{warnings && warningsCount > 0 && (
+				<div className="preview-popUp__warnings">
+					{React.Children.map(warnings, (child) => (
+						<div className="preview-popUp__warning-row">{child}</div>
+					))}
+				</div>
+			)}
+		</div>
+	)
+}
