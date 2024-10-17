3fd97164769f0cdd8e33d5ef1de76f9c5faa560f
Mint de Wit <mint@superfly.tv>
2024-10-17 15:11:07 +0000

    chore: revert countdown in top bar to part time

--- packages/webui/src/client/ui/RundownView.tsx
+++ packages/webui/src/client/ui/RundownView.tsx
@@ -313,6 +313,7 @@ const TimingDisplay = withTranslation()(
 								<CurrentPartOrSegmentRemaining
 									currentPartInstanceId={rundownPlaylist.currentPartInfo.partInstanceId}
 									heavyClassName="overtime"
+									forceToPartTimer={true}
 								/>
 								<AutoNextStatus />
 								{rundownPlaylist.holdState && rundownPlaylist.holdState !== RundownHoldState.COMPLETE ? (
--- packages/webui/src/client/ui/RundownView/RundownTiming/CurrentPartOrSegmentRemaining.tsx
+++ packages/webui/src/client/ui/RundownView/RundownTiming/CurrentPartOrSegmentRemaining.tsx
@@ -14,6 +14,8 @@ interface IPartRemainingProps {
 	heavyClassName?: string
 	speaking?: boolean
 	vibrating?: boolean
+	/** This component will show the remaining segment budget when that is available, but in some places in the UI we want it to force it to show remaining Part regardless */
+	forceToPartTimer?: boolean
 }
 
 // global variable for remembering last uttered displayTime
@@ -37,6 +39,7 @@ export const CurrentPartOrSegmentRemaining = withTiming<IPartRemainingProps, {}>
 			let displayTimecode =
 				this.props.timingDurations.remainingBudgetOnCurrentSegment ??
 				this.props.timingDurations.remainingTimeOnCurrentPart
+			if (this.props.forceToPartTimer) displayTimecode = this.props.timingDurations.remainingTimeOnCurrentPart
 			if (displayTimecode === undefined) return null
 			displayTimecode *= -1
 			return (
