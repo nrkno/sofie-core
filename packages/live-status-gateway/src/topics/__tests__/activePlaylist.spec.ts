474e0d3122b999314a5f76fee3359dd38a1cbc76
Julian Waller <git@julusian.co.uk>
2024-10-24 12:48:43 +0100

    fix: restore test

--- packages/live-status-gateway/src/topics/__tests__/activePlaylist.spec.ts
+++ packages/live-status-gateway/src/topics/__tests__/activePlaylist.spec.ts
@@ -85,6 +85,95 @@ describe('ActivePlaylistTopic', () => {
 		const part1: Partial<DBPart> = {
 			_id: protectString('PART_1'),
 			title: 'Test Part',
+			segmentId: segment1id,
+			expectedDurationWithTransition: 10000,
+			expectedDuration: 10000,
+			publicData: { b: 'c' },
+		}
+		const testPartInstances: PartialDeep<SelectedPartInstances> = {
+			current: {
+				_id: currentPartInstanceId,
+				part: part1,
+				timings: { plannedStartedPlayback: 1600000060000 },
+				segmentId: segment1id,
+			},
+			firstInSegmentPlayout: {},
+			inCurrentSegment: [
+				literal<PartialDeep<DBPartInstance>>({
+					_id: protectString(currentPartInstanceId),
+					part: part1,
+					timings: { plannedStartedPlayback: 1600000060000 },
+				}),
+			] as DBPartInstance[],
+		}
+		await topic.update(PartInstancesHandler.name, testPartInstances as SelectedPartInstances)
+
+		await topic.update(PartsHandler.name, [part1] as DBPart[])
+
+		await topic.update(SegmentHandler.name, {
+			_id: segment1id,
+		} as DBSegment)
+
+		topic.addSubscriber(mockSubscriber)
+
+		const expectedStatus: ActivePlaylistStatus = {
+			event: 'activePlaylist',
+			name: playlist.name,
+			id: unprotectString(playlist._id),
+			currentPart: {
+				id: 'PART_1',
+				name: 'Test Part',
+				segmentId: 'SEGMENT_1',
+				timing: { startTime: 1600000060000, expectedDurationMs: 10000, projectedEndTime: 1600000070000 },
+				pieces: [],
+				autoNext: undefined,
+				publicData: { b: 'c' },
+			},
+			nextPart: null,
+			currentSegment: {
+				id: 'SEGMENT_1',
+				timing: {
+					expectedDurationMs: 10000,
+					projectedEndTime: 1600000070000,
+				},
+			},
+			rundownIds: unprotectStringArray(playlist.rundownIdsInOrder),
+			publicData: { a: 'b' },
+			timing: {
+				timingMode: PlaylistTimingType.None,
+			},
+		}
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockSubscriber.send).toHaveBeenCalledTimes(1)
+		expect(JSON.parse(mockSubscriber.send.mock.calls[0][0] as string)).toMatchObject(
+			JSON.parse(JSON.stringify(expectedStatus))
+		)
+	})
+
+	it('provides segment and part with segment timing', async () => {
+		const topic = new ActivePlaylistTopic(makeMockLogger())
+		const mockSubscriber = makeMockSubscriber()
+
+		const currentPartInstanceId = 'CURRENT_PART_INSTANCE_ID'
+
+		const playlist = makeTestPlaylist()
+		playlist.activationId = protectString('somethingRandom')
+		playlist.currentPartInfo = {
+			consumesQueuedSegmentId: false,
+			manuallySelected: false,
+			partInstanceId: protectString(currentPartInstanceId),
+			rundownId: playlist.rundownIdsInOrder[0],
+		}
+		await topic.update(PlaylistHandler.name, playlist)
+
+		const testShowStyleBase = makeTestShowStyleBase()
+		await topic.update(ShowStyleBaseHandler.name, testShowStyleBase as ShowStyleBaseExt)
+
+		const segment1id = protectString('SEGMENT_1')
+		const part1: Partial<DBPart> = {
+			_id: protectString('PART_1'),
+			title: 'Test Part',
 			segmentId: protectString('SEGMENT_1'),
 			expectedDurationWithTransition: 10000,
 			expectedDuration: 10000,
