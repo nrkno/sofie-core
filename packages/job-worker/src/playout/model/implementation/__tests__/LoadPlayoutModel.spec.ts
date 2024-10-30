32acf636126348e0bd2997500fe533752fd9ae74
Jan Starzak <jan.starzak@gmail.com>
2024-10-30 16:04:10 +0100

    fix(LoadPlayoutModel): rundowns are unsorted in createPlayoutModelFromIngestModel

--- packages/job-worker/src/playout/model/implementation/__tests__/LoadPlayoutModel.spec.ts
+++ packages/job-worker/src/playout/model/implementation/__tests__/LoadPlayoutModel.spec.ts
@@ -7,8 +7,11 @@ import { MockJobContext, setupDefaultJobEnvironment } from '../../../../__mocks_
 import { ProcessedShowStyleCompound } from '../../../../jobs'
 import { ReadonlyDeep } from 'type-fest'
 import { protectString } from '@sofie-automation/corelib/dist/protectedString'
-import { loadPlayoutModelPreInit } from '../LoadPlayoutModel'
+import { createPlayoutModelFromIngestModel, loadPlayoutModelPreInit } from '../LoadPlayoutModel'
 import { runWithPlaylistLock } from '../../../../playout/lock'
+import { loadIngestModelFromRundown } from '../../../../ingest/model/implementation/LoadIngestModel'
+import { runWithRundownLock } from '../../../../ingest/lock'
+import { IngestModelReadonly } from '../../../../ingest/model/IngestModel'
 
 describe('LoadPlayoutModel', () => {
 	let context: MockJobContext
@@ -91,6 +94,65 @@ describe('LoadPlayoutModel', () => {
 			await runWithPlaylistLock(context, playlistId0, async (lock) => {
 				const model = await loadPlayoutModelPreInit(context, lock, playlist0)
 				expect(model.rundowns.map((r) => r._id)).toMatchObject([rundownId01, rundownId00, rundownId02])
+			})
+		})
+	})
+
+	describe('createPlayoutModelFromIngestModel', () => {
+		afterEach(async () =>
+			Promise.all([
+				context.mockCollections.RundownBaselineAdLibPieces.remove({}),
+				context.mockCollections.RundownBaselineAdLibActions.remove({}),
+				context.mockCollections.RundownBaselineObjects.remove({}),
+				context.mockCollections.AdLibActions.remove({}),
+				context.mockCollections.AdLibPieces.remove({}),
+				context.mockCollections.Pieces.remove({}),
+				context.mockCollections.Parts.remove({}),
+				context.mockCollections.Segments.remove({}),
+				context.mockCollections.Rundowns.remove({}),
+				context.mockCollections.RundownPlaylists.remove({}),
+			])
+		)
+
+		test('Rundowns are in order specified in RundownPlaylist', async () => {
+			// Set up a playlist:
+			const { rundownId: rundownId00, playlistId: playlistId0 } = await setupDefaultRundownPlaylist(
+				context,
+				showStyleCompound,
+				protectString('rundown00')
+			)
+			const rundownId01 = protectString('rundown01')
+			await setupDefaultRundown(context, showStyleCompound, playlistId0, rundownId01)
+			const rundownId02 = protectString('rundown02')
+			await setupDefaultRundown(context, showStyleCompound, playlistId0, rundownId02)
+
+			const rundownIdsInOrder = [rundownId01, rundownId02, rundownId00]
+
+			await context.mockCollections.RundownPlaylists.update(playlistId0, {
+				rundownIdsInOrder,
+			})
+
+			const playlist0 = await context.mockCollections.RundownPlaylists.findOne(playlistId0)
+			expect(playlist0).toBeTruthy()
+
+			if (!playlist0) throw new Error(`Playlist "${playlistId0}" not found!`)
+
+			let ingestModel: IngestModelReadonly | undefined
+
+			await runWithRundownLock(context, rundownId01, async (rundown, lock) => {
+				if (!rundown) throw new Error(`Rundown "${rundownId01}" not found!`)
+
+				ingestModel = await loadIngestModelFromRundown(context, lock, rundown)
+			})
+
+			await runWithPlaylistLock(context, playlistId0, async (lock) => {
+				if (!ingestModel) throw new Error('Ingest model could not be created!')
+
+				const rundowns = await context.mockCollections.Rundowns.findFetch({})
+
+				const model = await createPlayoutModelFromIngestModel(context, lock, playlist0, rundowns, ingestModel)
+
+				expect(model.rundowns.map((r) => r.rundown._id)).toMatchObject([rundownId01, rundownId02, rundownId00])
 			})
 		})
 	})
