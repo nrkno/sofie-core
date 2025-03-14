0df6d542111cf44098722902810a080f7cd54282
olzzon <github@olzzon.dk>
2025-03-14 13:16:48 +0100

    wip: split boxes background

--- packages/webui/src/client/ui/ClockView/ClockViewPieceIcons/ClockViewRenderers/SplitInputIcon.tsx
+++ packages/webui/src/client/ui/ClockView/ClockViewPieceIcons/ClockViewRenderers/SplitInputIcon.tsx
@@ -2,17 +2,18 @@ import * as React from 'react'
 import { PieceGeneric } from '@sofie-automation/corelib/dist/dataModel/Piece'
 import { SplitsContent, SourceLayerType } from '@sofie-automation/blueprints-integration'
 import { RundownUtils } from '../../../../lib/rundown'
-import classNames from 'classnames'
 import { ReadonlyDeep } from 'type-fest'
 
 type SplitIconPieceType = ReadonlyDeep<Omit<PieceGeneric, 'timelineObjectsString'>>
 
-export default class SplitInputIcon extends React.Component<{
+interface SplitInputIconProps {
 	abbreviation: string | undefined
 	piece: SplitIconPieceType | undefined
 	hideLabel?: boolean
-}> {
-	private getCameraLabel(piece: SplitIconPieceType | undefined) {
+}
+
+export default function SplitInputIcon({ abbreviation, piece, hideLabel }: Readonly<SplitInputIconProps>): JSX.Element {
+	function getCameraLabel() {
 		if (piece && piece.content) {
 			const c = piece.content as SplitsContent
 			const camera = c.boxSourceConfiguration.find((i) => i.type === SourceLayerType.CAMERA)
@@ -21,18 +22,15 @@ export default class SplitInputIcon extends React.Component<{
 				return (
 					<React.Fragment>
 						{label && label[1] ? label[1].substr(0, 1).toUpperCase() + ' ' : ''}
-						<span className="camera">{label ? label[2] : ''}</span>
+						<span>{label ? label[2] : ''}</span>
 					</React.Fragment>
 				)
-			} else {
-				return this.props.abbreviation !== undefined ? this.props.abbreviation : 'Spl'
 			}
-		} else {
-			return this.props.abbreviation !== undefined ? this.props.abbreviation : 'Spl'
 		}
+		return abbreviation !== undefined ? abbreviation : 'Spl'
 	}
 
-	private getLeftSourceType(piece: SplitIconPieceType | undefined): string {
+	function getLeftSourceType(): string {
 		if (piece && piece.content) {
 			const c = piece.content as SplitsContent
 			const left = (c.boxSourceConfiguration && c.boxSourceConfiguration[0])?.type || SourceLayerType.CAMERA
@@ -41,28 +39,23 @@ export default class SplitInputIcon extends React.Component<{
 		return 'camera'
 	}
 
-	private getRightSourceType(piece: SplitIconPieceType | undefined): string {
+	function getRightSourceType(): string {
 		if (piece && piece.content) {
 			const c = piece.content as SplitsContent
 			const right = (c.boxSourceConfiguration && c.boxSourceConfiguration[1])?.type || SourceLayerType.REMOTE
 			const sourceType = RundownUtils.getSourceLayerClassName(right)
-			return sourceType + (this.getLeftSourceType(piece) === sourceType ? ' second' : '')
+			return sourceType + (getLeftSourceType() === sourceType ? ' second' : '')
 		}
 		return 'remote'
 	}
 
-	render(): JSX.Element {
-		return (
-			<div className="clock-view-piece-icon">
-				<rect width="126.5" height="44.5" className={classNames('upper', this.getLeftSourceType(this.props.piece))} />
-				<rect
-					width="126.5"
-					height="44.5"
-					y="44.5"
-					className={classNames('lower', this.getRightSourceType(this.props.piece))}
-				/>
-				{!this.props.hideLabel && <span className="piece-icon-text">{this.getCameraLabel(this.props.piece)}</span>}
+	return (
+		<div className="clock-view-piece-icon">
+			<div className="split-view">
+				<div className={getLeftSourceType()}></div>
+				<div className={getRightSourceType()}></div>
 			</div>
-		)
-	}
+			{!hideLabel && <span className="piece-icon-text">{getCameraLabel()}</span>}
+		</div>
+	)
 }
